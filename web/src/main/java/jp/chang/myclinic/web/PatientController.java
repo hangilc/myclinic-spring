289d8264e51f7fc5814918c1131520ff04a11fe1
hangilc <hangil@chang.jp>
2017-02-26 13:20:32 +0900

    added getPatient

--- web/src/main/java/jp/chang/myclinic/web/PatientController.java
+++ web/src/main/java/jp/chang/myclinic/web/PatientController.java
@@ -5,21 +5,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import jp.chang.myclinic.web.json.JsonPatient;
 
-class Patient {
-	int patientId;
-}
+import jp.chang.myclinic.model.Patient;
+import jp.chang.myclinic.model.PatientRepository;
 
 @RestController
 @RequestMapping(value="/service", params="_q")
 public class PatientController {
+	@Autowired PatientRepository patientRepository;
 
 	@RequestMapping(value="", method=RequestMethod.GET, params={"_q=get_patient", "patient_id"})
 	public JsonPatient getPatient(@RequestParam(value="patient_id") int patientId) {
-		JsonPatient patient = new JsonPatient();
-		patient.setPatientId(patientId);
-		return patient;
+		Patient patient = patientRepository.findOne(patientId);
+		JsonPatient json = JsonPatient.fromPatient(patient);
+		return json;
 	}
 
 }
--- web/src/main/java/jp/chang/myclinic/web/json/JsonPatient.java
+++ web/src/main/java/jp/chang/myclinic/web/json/JsonPatient.java
@@ -1,5 +1,8 @@
 package jp.chang.myclinic.web.json;
 
+import jp.chang.myclinic.model.Patient;
+import java.sql.Date;
+
 public class JsonPatient {
 	private int patientId;
 
@@ -10,4 +13,98 @@ public class JsonPatient {
 	public void setPatientId(int patientId){
 		this.patientId = patientId;
 	}
+
+	private String lastName;
+
+	public String getLastName(){
+		return lastName;
+	}
+
+	public void setLastName(String lastName){
+		this.lastName = lastName;
+	}
+
+	private String firstName;
+
+	public String getFirstName(){
+		return firstName;
+	}
+
+	public void setFirstName(String firstName){
+		this.firstName = firstName;
+	}
+
+	private String lastNameYomi;
+
+	public String getLastNameYomi(){
+		return lastNameYomi;
+	}
+
+	public void setLastNameYomi(String lastNameYomi){
+		this.lastNameYomi = lastNameYomi;
+	}
+
+	private String firstNameYomi;
+
+	public String getFirstNameYomi(){
+		return firstNameYomi;
+	}
+
+	public void setFirstNameYomi(String firstNameYomi){
+		this.firstNameYomi = firstNameYomi;
+	}
+
+	private String sex;
+
+	public String getSex(){
+		return sex;
+	}
+
+	public void setSex(String sex){
+		this.sex = sex;
+	}
+
+	private Date birthday;
+
+	public Date getBirthday(){
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday){
+		this.birthday = birthday;
+	}
+
+	private String address;
+
+	public String getAddress(){
+		return address;
+	}
+
+	public void setAddress(String address){
+		this.address = address;
+	}
+
+	private String phone;
+
+	public String getPhone(){
+		return phone;
+	}
+
+	public void setPhone(String phone){
+		this.phone = phone;
+	}
+
+	public static JsonPatient fromPatient(Patient patient){
+		JsonPatient json = new JsonPatient();
+		json.setPatientId(patient.getPatientId());
+		json.setLastName(patient.getLastName());
+		json.setFirstName(patient.getFirstName());
+		json.setLastNameYomi(patient.getLastNameYomi());
+		json.setFirstNameYomi(patient.getFirstNameYomi());
+		json.setBirthday(patient.getBirthday());
+		json.setAddress(patient.getAddress());
+		json.setPhone(patient.getPhone());
+		return json;
+	}
+
 }
