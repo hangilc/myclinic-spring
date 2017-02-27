3febf2e7c71a9043f848fbef4f4a13943a6b3a7f
hangilc <hangil@chang.jp>
2017-02-27 23:54:59 +0900

    cleanup

--- model/src/main/java/jp/chang/myclinic/model/GazouLabelRepository.java
+++ model/src/main/java/jp/chang/myclinic/model/GazouLabelRepository.java
@@ -1,7 +1,10 @@
 package jp.chang.myclinic.model;
 
 import org.springframework.data.repository.CrudRepository;
+import java.util.Optional;
 
 public interface GazouLabelRepository extends CrudRepository<GazouLabel, Integer> {
+
+	Optional<GazouLabel> findOneByConductId(Integer conductId);
 
 }
