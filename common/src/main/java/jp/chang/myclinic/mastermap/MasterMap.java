84b701ef1cb0e12c6f3e06da756e5d0dfc380707
Hangil Chang <hangil@chang.jp>
2017-03-04 15:22:33 +0900

    implemented CodeMapEntry

--- common/src/main/java/jp/chang/myclinic/mastermap/MasterMap.java
+++ common/src/main/java/jp/chang/myclinic/mastermap/MasterMap.java
@@ -1,4 +1,4 @@
-package jp.chang.myclinic.common;
+package jp.chang.myclinic.mastermap;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
