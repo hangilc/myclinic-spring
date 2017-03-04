37b3f8fe1de39ad24beac16e260230bbe3e269ae
Hangil Chang <hangil@chang.jp>
2017-03-04 17:48:05 +0900

    implemented MasterMap

--- common/src/main/java/jp/chang/myclinic/mastermap/MasterMap.java
+++ common/src/main/java/jp/chang/myclinic/mastermap/MasterMap.java
@@ -5,6 +5,7 @@ import org.springframework.core.io.Resource;
 import java.io.IOException;
 import java.io.InputStream;
 import java.time.LocalDate;
+import java.util.Optional;
 import java.util.Scanner;
 
 /**
@@ -15,11 +16,19 @@ public class MasterMap {
     private CodeMap iyakuhinCodeMap;
     private CodeMap shinryouCodeMap;
     private CodeMap kizaiCodeMap;
+    private NameMap shinryouNameMap;
+    private NameMap kizaiNameMap;
+    private NameMap diseaseNameMap;
+    private NameMap diseaseAdjNameMap;
 
     MasterMap() {
         this.iyakuhinCodeMap = new CodeMap();
         this.shinryouCodeMap = new CodeMap();
         this.kizaiCodeMap = new CodeMap();
+        shinryouNameMap = new NameMap();
+        kizaiNameMap = new NameMap();
+        diseaseNameMap = new NameMap();
+        diseaseAdjNameMap = new NameMap();
     }
 
     public int resolveIyakuhinCode(int code, LocalDate at){
@@ -34,9 +43,25 @@ public class MasterMap {
         return kizaiCodeMap.resolve(code, at);
     }
 
-    void loadResource(Resource resource) throws IOException {
+    public Optional<Integer> getShinryoucodeByName(String name){
+        return shinryouNameMap.get(name);
+    }
+
+    public Optional<Integer> getKizaicodeByName(String name){
+        return kizaiNameMap.get(name);
+    }
+
+    public Optional<Integer> getShoubyoumeicodeByName(String name){
+        return diseaseNameMap.get(name);
+    }
+
+    public Optional<Integer> getShuushokugocodeByName(String name){
+        return diseaseAdjNameMap.get(name);
+    }
+
+    void loadCodeMapResource(Resource resource) throws IOException {
         try (InputStream in = resource.getInputStream()) {
-            Scanner scanner = new Scanner(in);
+            Scanner scanner = new Scanner(in, "UTF-8");
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 if (line.isEmpty()) continue;
@@ -50,13 +75,50 @@ public class MasterMap {
                     codeMap = kizaiCodeMap;
                 }
                 if (codeMap == null) {
-                    if (kind == ';') {
+                    line = line.trim();
+                    if( line.isEmpty() ) continue;
+                    if (line.charAt(0) == ';') {
                         continue;
                     }
-                    throw new RuntimeException("invalid master map entry: " + line);
+                    throw new RuntimeException("invalid code map entry: " + line);
                 } else {
                     CodeMapEntry entry = CodeMapEntry.parse(line);
                     codeMap.addEntry(entry);
+                }
+            }
+            scanner.close();
+            iyakuhinCodeMap.sortByDate();
+            shinryouCodeMap.sortByDate();
+            kizaiCodeMap.sortByDate();
+        }
+    }
+
+    void loadNameMapResource(Resource resource) throws IOException {
+        try (InputStream in = resource.getInputStream()) {
+            Scanner scanner = new Scanner(in, "UTF-8");
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.isEmpty()) continue;
+                char kind = line.charAt(0);
+                NameMap nameMap = null;
+                if (kind == 's') {
+                    nameMap = shinryouNameMap;
+                } else if (kind == 'k') {
+                    nameMap = kizaiNameMap;
+                } else if (kind == 'd') {
+                    nameMap = diseaseNameMap;
+                } else if( kind == 'a' ){
+                    nameMap = diseaseAdjNameMap;
+                }
+                if (nameMap == null) {
+                    line = line.trim();
+                    if( line.isEmpty() ) continue;
+                    if (line.charAt(0) == ';') {
+                        continue;
+                    }
+                    throw new RuntimeException("invalid name map entry: " + line);
+                } else {
+                    nameMap.parseAndEnter(line);
                 }
             }
             scanner.close();
--- common/src/test/java/jp/chang/myclinic/mastermap/MasterMapTest.java
+++ common/src/test/java/jp/chang/myclinic/mastermap/MasterMapTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -24,21 +25,43 @@ public class MasterMapTest {
     private ResourceLoader resourceLoader;
 
     @Test
-    public void testFindResource() throws IOException {
+    public void testFindCodeMapResource() {
         Resource resource = resourceLoader.getResource("classpath:master-map.txt");
         assertTrue("find resource", resource.exists());
     }
 
     @Test
-    public void testLoadResouce() throws IOException {
+    public void testLoadCodeMapResouce() throws IOException {
         MasterMap map = new MasterMap();
         Resource resource = resourceLoader.getResource("classpath:master-map.txt");
-        map.loadResource(resource);
+        map.loadCodeMapResource(resource);
         // Y,611140694,2012-04-01,620098801 ロキソニン錠６０ｍｇ
         assertEquals("resolve loxonin", 620098801, map.resolveIyakuhinCode(611140694, LocalDate.of(2014, 4, 1)));
         // S,112009210,2010-04-01,112007410 再診
         assertEquals("resolve saishin", 112007410, map.resolveShinryouCode(112009210, LocalDate.of(2014, 4, 1)));
         // S,111012370,2010-04-01,000000000 電子化加算（廃止）
         assertEquals("resolve denka", 0, map.resolveShinryouCode(111012370, LocalDate.of(2014, 4, 1)));
+    }
+
+    @Test
+    public void testFindNameMapResource(){
+        Resource resource = resourceLoader.getResource("classpath:name-map.txt");
+        assertTrue("find resource", resource.exists());
+    }
+
+    @Test
+    public void testLoadNameMapResource() throws IOException {
+        MasterMap map = new MasterMap();
+        Resource resource = resourceLoader.getResource("classpath:name-map.txt");
+        map.loadNameMapResource(resource);
+        // s,単純撮影,170001910
+        assertEquals("name map shinryou", Optional.of(170001910), map.getShinryoucodeByName("単純撮影"));
+        assertEquals("name map empty", Optional.empty(), map.getShinryoucodeByName("単純撮影XX"));
+        // k,大角,700030000
+        assertEquals("name map kizai", Optional.of(700030000), map.getKizaicodeByName("大角"));
+        // d,急性上気道炎,3041
+        assertEquals("name map disease", Optional.of(3041), map.getShoubyoumeicodeByName("急性上気道炎"));
+        // a,疑い,8002
+        assertEquals("name map disease adj", Optional.of(8002), map.getShuushokugocodeByName("疑い"));
     }
 }
