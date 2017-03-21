6b95c712e182da8b350ecc6fb30cf811c092d835
hangilc <hangil@chang.jp>
2017-03-22 00:31:10 +0900

    added master zip finder

---
+++ master-add/src/main/java/jp/chang/myclinic/master/MasterZipFinder.java
@@ -0,0 +1,29 @@
+package jp.chang.myclinic.master;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.ArrayList;
+
+public class MasterZipFinder {
+
+	public static Path findMasterZipFile(Path dir, String prefix){
+		List<Path> zipFiles = new ArrayList<>();
+		try(DirectoryStream<Path> stream = Files.newDirectoryStream(dir, prefix + "*.zip")){
+			for(Path path: stream){
+				zipFiles.add(path);
+			}
+		} catch(IOException ex){
+			throw new UncheckedIOException(ex);
+		}
+		zipFiles.sort((a, b) -> -a.getFileName().compareTo(b.getFileName()));
+		if( zipFiles.size() == 0 ){
+			return null;
+		}
+		return zipFiles.get(0);
+	}
+
+}
