190ee86e1add5b519b6dcd0bc91ef937d65d7be1
hangilc <hangil@chang.jp>
2017-03-22 09:23:05 +0900

    added find iyakuhin master zip

--- master-add/src/main/java/jp/chang/myclinic/IyakuhinAddMenu.java
+++ master-add/src/main/java/jp/chang/myclinic/IyakuhinAddMenu.java
@@ -1,9 +1,21 @@
 package jp.chang.myclinic;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.ArrayList;
+import jp.chang.myclinic.master.MasterZipFinder;
 
 class IyakuhinAddMenu extends Menu {
+
+	private Path zipFilePath;
+
+	IyakuhinAddMenu(){
+		Path masterDir = Paths.get(".");
+		zipFilePath = MasterZipFinder.findMasterZip(masterDir, MasterZipFinder.IYAKUHIN_PREFIX);
+		System.out.println(zipFilePath);
+	}
+
 	@Override
 	public String getPrompt(){
 		return "iyakuhin-add>";
--- master-add/src/main/java/jp/chang/myclinic/master/MasterZipFinder.java
+++ master-add/src/main/java/jp/chang/myclinic/master/MasterZipFinder.java
@@ -9,8 +9,13 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class MasterZipFinder {
+	public static final String SHINRYOU_PREFIX = "s";
+	public static final String IYAKUHIN_PREFIX = "y";
+	public static final String KIZAI_PREFIX = "t";
+	public static final String SHOUBYOUMEI_PREFIX = "b";
+	public static final String SHUUSHOKUGO_PREFIX = "z";
 
-	public static Path findMasterZipFile(Path dir, String prefix){
+	public static Path findMasterZip(Path dir, String prefix){
 		List<Path> zipFiles = new ArrayList<>();
 		try(DirectoryStream<Path> stream = Files.newDirectoryStream(dir, prefix + "*.zip")){
 			for(Path path: stream){
