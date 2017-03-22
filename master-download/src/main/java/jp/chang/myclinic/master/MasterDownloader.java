535441d439d67cfb54b4682d41c5a20f2adae18c
hangilc <hangil@chang.jp>
2017-03-22 23:18:41 +0900

    added test

---
+++ master-download/src/main/java/jp/chang/myclinic/master/MasterDownloader.java
@@ -0,0 +1,70 @@
+package jp.chang.myclinic.master;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebResponse;
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.LinkedHashMap;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class MasterDownloader {
+
+	private String startPageUrl = "http://www.iryohoken.go.jp/shinryohoshu/downloadMenu/";
+	public static final String DEFAULT_SHINRYOU_FILENAME = "s.zip"; 
+	public static final String DEFAULT_IYAKUHIN_FILENAME = "y.zip"; 
+	public static final String DEFAULT_KIZAI_FILENAME = "t.zip"; 
+	public static final String DEFAULT_SHOUBYOUMEI_FILENAME = "b.zip"; 
+	public static final String DEFAULT_SHUUSHOKUGO_FILENAME = "z.zip"; 
+
+	public void setStartPageUrl(String url){
+		this.startPageUrl = url;
+	}
+
+    public void downloadShinryou(Path filename) throws IOException {
+    	download("医科診療行為マスター", filename);
+    }
+
+    public void downloadIyakuhin(Path filename) throws IOException {
+    	download("医薬品マスター", filename);
+    }
+
+    public void downloadKizai(Path filename) throws IOException {
+    	download("特定器材マスター", filename);
+    }
+
+    public void downloadShoubyoumei(Path filename) throws IOException {
+    	download("傷病名マスター", filename);
+    }
+
+    public void downloadShuushokugo(Path filename) throws IOException {
+    	download("修飾語マスター", filename);
+    }
+
+    private void download(String anchorText, Path filepath) throws IOException {
+	    try(WebClient webClient = new WebClient()){
+	    	HtmlPage page = webClient.getPage(startPageUrl);
+	    	HtmlAnchor anchor = page.getAnchorByText(anchorText);
+	    	Page downloadPage = anchor.click();
+	    	WebResponse webResponse = downloadPage.getWebResponse();
+	    	try(InputStream in = webResponse.getContentAsStream();
+		    	OutputStream out = Files.newOutputStream(filepath)){
+		    	byte[] buffer = new byte[1024];
+		    	int len = in.read(buffer);
+		    	while( len >= 0 ){
+		    		out.write(buffer, 0, len);
+		    		len = in.read(buffer);
+		    	}
+		    }
+	    }
+    }
+
+}
--- master-download/src/test/java/jp/chang/myclinic/AppTest.java
+++ master-download/src/test/java/jp/chang/myclinic/AppTest.java
@@ -1,38 +1,102 @@
-package jp.chang.myclinic;
+package jp.chang.myclinic.master;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
+import static org.junit.Assert.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.FileSystems;
+import java.nio.file.FileSystem;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.util.Optional;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+
+    @Test
+    public void testIyakuhinDownload() throws IOException {
+        MasterDownloader downloader = new MasterDownloader();
+        Path path = Paths.get(tempFolder.getRoot().getAbsolutePath(), MasterDownloader.DEFAULT_IYAKUHIN_FILENAME);
+        downloader.downloadIyakuhin(path);
+        assertTrue( Files.exists(path) );
+        FileSystem fs = FileSystems.newFileSystem(path, null);
+        Path zipFilePath = fs.getPath("y.csv");
+        assertTrue( Files.exists(zipFilePath) );
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void testShinryouDownload() throws IOException {
+        MasterDownloader downloader = new MasterDownloader();
+        Path path = Paths.get(tempFolder.getRoot().getAbsolutePath(), MasterDownloader.DEFAULT_SHINRYOU_FILENAME);
+        downloader.downloadShinryou(path);
+        assertTrue( Files.exists(path) );
+        FileSystem fs = FileSystems.newFileSystem(path, null);
+        Path zipFilePath = fs.getPath("s.csv");
+        assertTrue( Files.exists(zipFilePath) );
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testKizaiDownload() throws IOException {
+        MasterDownloader downloader = new MasterDownloader();
+        Path path = Paths.get(tempFolder.getRoot().getAbsolutePath(), MasterDownloader.DEFAULT_KIZAI_FILENAME);
+        downloader.downloadKizai(path);
+        assertTrue( Files.exists(path) );
+        FileSystem fs = FileSystems.newFileSystem(path, null);
+        Path zipFilePath = fs.getPath("t.csv");
+        assertTrue( Files.exists(zipFilePath) );
     }
+
+    @Test
+    public void testShoubyoumeiDownload() throws IOException {
+        MasterDownloader downloader = new MasterDownloader();
+        Path path = Paths.get(tempFolder.getRoot().getAbsolutePath(), MasterDownloader.DEFAULT_SHOUBYOUMEI_FILENAME);
+        downloader.downloadShoubyoumei(path);
+        assertTrue( Files.exists(path) );
+        FileSystem fs = FileSystems.newFileSystem(path, null);
+        boolean found = false;
+        PathMatcher matcher = fs.getPathMatcher("glob:**/b_*.txt");
+        for(Path rootDir: fs.getRootDirectories()){
+            Optional<Path> p = Files.list(rootDir)
+                .filter(pp -> {
+                    //System.out.println(pp);
+                    return matcher.matches(pp);
+                })
+                .findFirst();
+            if( p.isPresent() ){
+                found = true;
+                break;
+            }
+        }
+        assertTrue( found );
+    }
+
+    @Test
+    public void testShuushokugoDownload() throws IOException {
+        MasterDownloader downloader = new MasterDownloader();
+        Path path = Paths.get(tempFolder.getRoot().getAbsolutePath(), MasterDownloader.DEFAULT_SHUUSHOKUGO_FILENAME);
+        downloader.downloadShuushokugo(path);
+        assertTrue( Files.exists(path) );
+        FileSystem fs = FileSystems.newFileSystem(path, null);
+        boolean found = false;
+        PathMatcher matcher = fs.getPathMatcher("glob:**/z_*.txt");
+        for(Path rootDir: fs.getRootDirectories()){
+            Optional<Path> p = Files.list(rootDir)
+                .filter(pp -> {
+                    //System.out.println(pp);
+                    return matcher.matches(pp);
+                })
+                .findFirst();
+            if( p.isPresent() ){
+                found = true;
+                break;
+            }
+        }
+        assertTrue( found );
+    }
+
 }
