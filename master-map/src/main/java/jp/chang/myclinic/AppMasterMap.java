56919a608015b835a1a83835c905d560b007391e
hangilc <hangil@chang.jp>
2017-03-22 15:45:54 +0900

    created master-map

---
+++ master-map/src/main/java/jp/chang/myclinic/AppMasterMap.java
@@ -0,0 +1,13 @@
+package jp.chang.myclinic;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppMasterMap
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
