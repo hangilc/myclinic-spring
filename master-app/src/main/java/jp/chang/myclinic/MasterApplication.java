c9cd13aa3e2c5659b340b71765c229d53d9b1ff2
Hangil Chang <hangil@chang.jp>
2017-03-24 19:01:48 +0900

    added Globals.masterFilesDirectory

--- master-app/src/main/java/jp/chang/myclinic/MasterApplication.java
+++ master-app/src/main/java/jp/chang/myclinic/MasterApplication.java
@@ -7,6 +7,7 @@ import jp.chang.myclinic.masterapp.MainMenu;
 public class MasterApplication
 {
     public static void main( String[] args ) throws IOException {
+    	System.out.println(jp.chang.myclinic.masterapp.Globals.getMasterFilesDirectory());
         Menu menu = new MainMenu();
         while( menu != null ){
         	menu = menu.exec();
