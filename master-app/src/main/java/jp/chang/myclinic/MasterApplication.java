0c080a1d29bd8bc388d09ad40a6ac1c87ecee08b
hangilc <hangil@chang.jp>
2017-03-25 23:53:05 +0900

    added spring

--- master-app/src/main/java/jp/chang/myclinic/MasterApplication.java
+++ master-app/src/main/java/jp/chang/myclinic/MasterApplication.java
@@ -4,10 +4,19 @@ import java.io.IOException;
 import jp.chang.myclinic.masterapp.Menu;
 import jp.chang.myclinic.masterapp.MainMenu;
 
-public class MasterApplication
-{
-    public static void main( String[] args ) throws IOException {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class MasterApplication implements CommandLineRunner {
+    public static void main( String[] args ) throws IOException {
+    	SpringApplication.run(MasterApplication.class, args);
+    }
+
+    @Override
+    public void run(String[] args) throws IOException {
         Menu menu = new MainMenu();
         while( menu != null ){
         	menu = menu.exec();
