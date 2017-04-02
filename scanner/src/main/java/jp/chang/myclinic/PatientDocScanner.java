28950adff71c9a5add68c6235a2769730ecd3465
hangilc <hangil@chang.jp>
2017-04-02 22:13:04 +0900

    adding patient doc scan

--- scanner/src/main/java/jp/chang/myclinic/PatientDocScanner.java
+++ scanner/src/main/java/jp/chang/myclinic/PatientDocScanner.java
@@ -5,17 +5,63 @@ import java.awt.event.ActionEvent;
 import javax.swing.*;
 import java.io.File;
 import java.nio.file.Path;
-import jp.chang.wia.Wia;
-import jp.chang.wia.WiaConsts;
-import jp.chang.wia.WiaDevMgr;
+import java.util.List;
 
-import com.sun.jna.platform.win32.COM.COMUtils;
-import com.sun.jna.platform.win32.Guid;
-import com.sun.jna.platform.win32.Kernel32;
-import com.sun.jna.platform.win32.WinDef.HWND;
-import com.sun.jna.platform.win32.WinDef.LONG;
+import com.sun.jna.Native;
+import com.sun.jna.Memory;
+import com.sun.jna.Pointer;
+import com.sun.jna.WString;
+import com.sun.jna.ptr.PointerByReference;
+import com.sun.jna.ptr.IntByReference;
+import com.sun.jna.platform.win32.Ole32;
+import com.sun.jna.platform.win32.Guid.CLSID;
+import com.sun.jna.platform.win32.Guid.IID;
+import com.sun.jna.platform.win32.Guid.REFIID;
+import static com.sun.jna.platform.win32.Guid.IID_NULL;
+import com.sun.jna.platform.win32.W32Errors;
 import com.sun.jna.platform.win32.WinNT.HRESULT;
 import com.sun.jna.platform.win32.WTypes.BSTR;
+import com.sun.jna.platform.win32.WTypes.LPOLESTR;
+import com.sun.jna.platform.win32.WinDef.ULONG;
+import com.sun.jna.platform.win32.WinDef.ULONGByReference;
+import com.sun.jna.platform.win32.WinDef.LONG;
+import com.sun.jna.platform.win32.WinDef.LONGByReference;
+import com.sun.jna.platform.win32.WinDef.DWORD;
+import com.sun.jna.platform.win32.WinDef.HWND;
+import com.sun.jna.platform.win32.WTypes.VARTYPE;
+import com.sun.jna.platform.win32.Variant;
+import com.sun.jna.platform.win32.Kernel32;
+import com.sun.jna.platform.win32.COM.COMUtils;
+import com.sun.jna.platform.win32.Wincon;
+import com.sun.jna.platform.win32.WinError;
+
+import jp.chang.wia.*;
+// import jp.chang.wia.IWiaDevMgr;
+// import jp.chang.wia.WiaDevMgr;
+// import jp.chang.wia.IWiaDevMgr2;
+// import jp.chang.wia.WiaDevMgr2;
+// import jp.chang.wia.EnumWIA_DEV_INFO;
+// import jp.chang.wia.WiaPropertyStorage;
+// import jp.chang.wia.IWiaPropertyStorage;
+// import jp.chang.wia.WiaConsts;
+// import jp.chang.wia.PROPSPEC;
+// import jp.chang.wia.PROPVARIANT;
+// import jp.chang.wia.MyOle32;
+// import jp.chang.wia.WiaItem;
+// import jp.chang.wia.WiaItem2;
+// import jp.chang.wia.EnumWiaItem;
+// import jp.chang.wia.EnumWiaItem2;
+// import jp.chang.wia.IWiaDataTransfer;
+// import jp.chang.wia.WiaDataTransfer;
+// import jp.chang.wia.STGMEDIUM;
+// import jp.chang.wia.Wia;
+// import jp.chang.wia.WiaTypes.PROPID;
+// import jp.chang.wia.WiaDataCallback;
+// import jp.chang.wia.WiaDataCallbackImpl;
+// import jp.chang.wia.WiaDevMgr;
+// import jp.chang.wia.PropValue;
+// import jp.chang.wia.EnumSTATPROPSTG;
+// import jp.chang.wia.STATPROPSTG;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -105,14 +151,109 @@ public class PatientDocScanner extends JDialog {
 	}
 
 	private void doStart(ActionEvent event){
-		numPages += 1;
-		numPagesLabel.setText(String.valueOf(numPages));
-		pack();
+		List<Wia.Device> devices = Wia.listDevices();
+		WiaItem deviceItem = null;
+		if( devices.size() == 0 ){
+			System.out.println("No scanner");
+			return;
+		} else if( devices.size() == 1 ){
+			deviceItem = getDeviceWiaItem(devices.get(0).deviceId);
+		} else {
+			String deviceName = pickDevice();
+			if( deviceName == null ){
+				return;
+			}
+			deviceItem = getDeviceWiaItem(deviceName);
+		}
+		WiaItem wiaItem = findScannerFile(deviceItem);
+		if( wiaItem != null ){
+			String saveFileName = String.format("%d-%s-%02d.bmp", patientId, timeStamp, numPages+1);
+			Path savePath = saveDir.resolve(saveFileName);
+            PointerByReference pWiaDataTransfer = new PointerByReference();
+            HRESULT hr = wiaItem.QueryInterface(new REFIID(IWiaDataTransfer.IID_IWiaDataTransfer), pWiaDataTransfer);
+            COMUtils.checkRC(hr);
+            WiaDataTransfer transfer = new WiaDataTransfer(pWiaDataTransfer.getValue());
+            STGMEDIUM stgmedium = new STGMEDIUM();
+            stgmedium.tymed = new DWORD(WiaConsts.TYMED_FILE);
+            stgmedium.unionValue.setType(Pointer.class);
+            String fileName = savePath.toString();
+            stgmedium.unionValue.pointer = new LPOLESTR(fileName).getPointer();
+            WiaDataCallback dataCallback = WiaDataCallbackImpl.create(new WiaDataCallbackImpl.BandedDataCallbackCallback(){
+                @Override
+                public HRESULT invoke(Pointer thisPointer, LONG lMessage, LONG lStatus, LONG lPercentComplete,
+                    LONG lOffset, LONG lLength, LONG lReserved, LONG lResLength, PointerByReference pbBuffer){
+                    System.out.printf("callback %d\n", lPercentComplete.intValue());
+                    return WinError.S_OK;
+                };
+            });
+            hr = transfer.idtGetData(stgmedium, dataCallback);
+            COMUtils.checkRC(hr);
+            transfer.Release();
+			wiaItem.Release();
+			numPages += 1;
+			numPagesLabel.setText(String.valueOf(numPages));
+			pack();
+		}
+		deviceItem.Release();
 	}
 
 	private String makeTimeStamp(){
 		LocalDateTime dt = LocalDateTime.now();
 		return dt.format(timeStampFormatter);
 	}
+
+    private static WiaItem getDeviceWiaItem(String deviceId){
+        WiaDevMgr devMgr = Wia.createWiaDevMgr();
+        PointerByReference pp = new PointerByReference();
+        HRESULT hr = devMgr.CreateDevice(new BSTR(deviceId), pp);
+        WiaItem item = new WiaItem(pp.getValue());
+        devMgr.Release();
+        return item;
+    }
+
+    private static String pickDevice(){
+        WiaDevMgr devMgr = Wia.createWiaDevMgr();
+        HWND hwnd = Kernel32.INSTANCE.GetConsoleWindow();
+        PointerByReference pp = new PointerByReference();
+        HRESULT hr = devMgr.SelectDeviceDlgID(hwnd, new LONG(WiaConsts.StiDeviceTypeScanner), 
+            new LONG(WiaConsts.WIA_SELECT_DEVICE_NODEFAULT), pp);
+        COMUtils.checkRC(hr);
+        if( hr.equals(COMUtils.S_FALSE) ){
+        	return null;
+        } else {
+	        BSTR bstr = new BSTR(pp.getValue());
+	        devMgr.Release();
+	        return bstr.toString();
+	    }
+    }
+
+    private static WiaItem findScannerFile(WiaItem deviceItem){
+    	WiaItem wiaItem = null;
+        PointerByReference pIEnumWiaItem = new PointerByReference();
+        HRESULT hr = deviceItem.EnumChildItems(pIEnumWiaItem);
+        COMUtils.checkRC(hr);
+        EnumWiaItem enumWiaItem = new EnumWiaItem(pIEnumWiaItem.getValue());
+        WiaItem.ByReference[] items = new WiaItem.ByReference[4];
+        ULONGByReference pFetched = new ULONGByReference();
+        hr = enumWiaItem.Next(new ULONG(4), items, pFetched);
+        int nFetched = pFetched.getValue().intValue();
+        for(int i=0;i<nFetched;i++){
+            WiaItem.ByReference item = items[i];
+            LONGByReference pItemType = new LONGByReference();
+            hr = item.GetItemType(pItemType);
+            COMUtils.checkRC(hr);
+            int itemType = pItemType.getValue().intValue();
+            System.out.printf("ItemType: 0x%x\n", itemType);
+            if( (itemType & WiaConsts.WiaItemTypeImage) != 0 && 
+                (itemType & WiaConsts.WiaItemTypeFile) != 0 ){
+                wiaItem = item;
+            } else {
+                item.Release();
+            }
+        }
+        COMUtils.checkRC(hr);
+        enumWiaItem.Release();
+    	return wiaItem;
+    }
 
 }
