be87222009484e7da8f0c99106c41dc2dd4580b3
hangilc <hangil@chang.jp>
2017-04-02 17:01:37 +0900

    temporary saving

--- scanner/src/main/java/jp/chang/myclinic/MainFrame.java
+++ scanner/src/main/java/jp/chang/myclinic/MainFrame.java
@@ -9,6 +9,28 @@ public class MainFrame extends JFrame {
 		JPanel panel = new JPanel();
 		JButton patient = new JButton("患者書類");
 		JButton general = new JButton("一般書類");
+		patient.addActionListener(event -> {
+			String patientIdInput = JOptionPane.showInputDialog(this, "患者番号を入力してください。");
+			Integer patientId = null;
+			while( true ){
+				if( patientIdInput == null ){
+					break;
+				}
+				try {
+					patientId = Integer.parseInt(patientIdInput);
+					break;
+				} catch(NumberFormatException ex){
+					patientIdInput = JOptionPane.showInputDialog(this, 
+						"患者番号を入力してください。\n（患者番号の値が不適切です。）", patientIdInput);
+				}
+			}
+			if( patientId == null ){
+				return;
+			}
+			PatientDocScanner docScanner = new PatientDocScanner(this, patientId);
+			docScanner.setLocationByPlatform(true);
+			docScanner.setVisible(true);
+		});
 		general.addActionListener(event -> {
 			GeneralDocScanner docScanner = new GeneralDocScanner(this);
 			docScanner.setLocationByPlatform(true);
---
+++ scanner/src/main/java/jp/chang/myclinic/PatientDocScanner.java
@@ -0,0 +1,100 @@
+package jp.chang.myclinic;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+import java.io.File;
+import java.nio.file.Path;
+import jp.chang.wia.Wia;
+import jp.chang.wia.WiaConsts;
+import jp.chang.wia.WiaDevMgr;
+
+import com.sun.jna.platform.win32.COM.COMUtils;
+import com.sun.jna.platform.win32.Guid;
+import com.sun.jna.platform.win32.Kernel32;
+import com.sun.jna.platform.win32.WinDef.HWND;
+import com.sun.jna.platform.win32.WinDef.LONG;
+import com.sun.jna.platform.win32.WinNT.HRESULT;
+import com.sun.jna.platform.win32.WTypes.BSTR;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+
+public class PatientDocScanner extends JDialog {
+
+	private int patientId;
+	private int numPages = 0;
+	private String timeStamp;
+	private Path saveDir;
+	private static DateTimeFormatter timeStampFormatter = DateTimeFormatter.ofPattern("uuuuMMdd-HHmmss");
+
+	public PatientDocScanner(Frame owner, int patientId){
+		super(owner, "患者書類のスキャン", true);
+		this.patientId = patientId;
+		this.timeStamp = makeTimeStamp();
+		this.saveDir = getSaveDir();
+		add(makeCenterPanel(), BorderLayout.CENTER);
+		add(makeCommandPanel(), BorderLayout.SOUTH);
+		pack();
+	}
+
+	private Component makeCenterPanel(){
+		JPanel panel = new JPanel();
+		panel.add(makeCenterPanelContent());
+		return panel;
+	}
+
+	private Component makeCenterPanelContent(){
+		JPanel panel = new JPanel();
+		BoxLayout boxLayout = new BoxLayout(panel, BoxLayout.PAGE_AXIS);
+		panel.setLayout(boxLayout);
+		JLabel patientIdLabel = new JLabel("患者番号：" + patientId);
+		panel.add(patientIdLabel);
+		return panel;
+	}
+
+	private Component makeCommandPanel(){
+		JPanel panel = new JPanel();
+		JButton start = new JButton("スタート");
+		JButton finish = new JButton("終了");
+		int maxWidth = 0;
+		for(JButton btn: new JButton[]{start, finish}){
+			Dimension dim = btn.getPreferredSize();
+			if( dim.width > maxWidth ){
+				maxWidth = dim.width;
+			}
+		}
+		for(JButton btn: new JButton[]{start, finish}){
+			Dimension dim = btn.getPreferredSize();
+			dim.width = maxWidth;
+			btn.setPreferredSize(dim);
+		}
+		start.addActionListener(this::doStart);
+		finish.addActionListener(event -> {
+			dispose();
+		});
+		panel.add(start);
+		panel.add(finish);
+		return panel;
+	}
+
+	private Path getSaveDir(){
+		Path path = ScannerProperties.INSTANCE.getSaveDir();
+		if( path == null ){
+			path = Paths.get(System.getProperty("user.dir"));
+		}
+		return path;
+	}
+
+	private void doStart(ActionEvent event){
+
+	}
+
+	private String makeTimeStamp(){
+		LocalDateTime dt = LocalDateTime.now();
+		return dt.format(timeStampFormatter);
+	}
+
+}
