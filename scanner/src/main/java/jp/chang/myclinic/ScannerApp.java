18e99bfad94b98da63b853b9b8643c9de247c1e5
Hangil Chang <hangil@chang.jp>
2017-04-10 13:27:59 +0900

    added slf4j and logback

--- scanner/src/main/java/jp/chang/myclinic/ScannerApp.java
+++ scanner/src/main/java/jp/chang/myclinic/ScannerApp.java
@@ -4,15 +4,20 @@ import java.awt.*;
 import javax.swing.*;
 import jp.chang.wia.Wia;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ScannerApp 
 {
+    private Logger logger = LoggerFactory.getLogger(ScannerApp.class);
+
     public static void main( String[] args )
     {
     	new ScannerApp(args).run();
     }
 
     private ScannerApp(String[] args){
-    	
+    	logger.info("App started");
     }
 
     private void run(){
