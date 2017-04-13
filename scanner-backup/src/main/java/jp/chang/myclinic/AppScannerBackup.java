9d608645276128adca2e73180d77250c6b051596
Hangil Chang <hangil@chang.jp>
2017-04-13 13:40:08 +0900

    craeted scanner-backup

---
+++ scanner-backup/src/main/java/jp/chang/myclinic/AppScannerBackup.java
@@ -0,0 +1,13 @@
+package jp.chang.myclinic;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppScannerBackup
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
