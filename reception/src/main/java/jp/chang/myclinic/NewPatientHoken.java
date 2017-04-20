6f5fb7584b5c67c0e2603156f91d2567b8797dd6
Hangil Chang <hangil@chang.jp>
2017-04-20 09:30:41 +0900

    added new patient gui

--- reception/src/main/java/jp/chang/myclinic/NewPatientHoken.java
+++ reception/src/main/java/jp/chang/myclinic/NewPatientHoken.java
@@ -5,8 +5,59 @@ import javax.swing.*;
 
 class NewPatientHoken extends JPanel {
 
+	private JList hokenList;
+
 	NewPatientHoken(){
-		setPreferredSize(new Dimension(400, 300));
+		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+		add(makeUpperPanel());
+		add(Box.createVerticalStrut(5));
+		add(makeLowerPanel());
+	}
+
+	private JComponent makeUpperPanel(){
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.LINE_AXIS));
+		hokenList = new JList();
+		hokenList.setPreferredSize(new Dimension(400, 200));
+		panel.add(hokenList);
+		panel.add(Box.createHorizontalStrut(5));
+		JPanel commandBox = new JPanel();
+		{
+			commandBox.setLayout(new BoxLayout(commandBox, BoxLayout.PAGE_AXIS));
+			JButton editButton = new JButton("訂正");
+			JButton deleteButton = new JButton("削除");
+			commandBox.add(editButton);
+			commandBox.add(Box.createVerticalStrut(5));
+			commandBox.add(deleteButton);
+		}
+		panel.add(commandBox);
+		return panel;
+	}
+
+	private JComponent makeLowerPanel(){
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+		JPanel upperBox = new JPanel();
+		{
+			upperBox.setLayout(new FlowLayout());
+			JButton enterShahoButton = new JButton("新規社保国保");
+			JButton enterKoukiButton = new JButton("新規後期高齢");
+			JButton enterKouhiButton = new JButton("新規公費負担");
+			upperBox.add(enterShahoButton);
+			upperBox.add(enterKoukiButton);
+			upperBox.add(enterKouhiButton);
+		}
+		panel.add(upperBox);
+		panel.add(Box.createVerticalStrut(5));
+		JPanel lowerBox = new JPanel();
+		{
+			lowerBox.setLayout(new BoxLayout(lowerBox, BoxLayout.LINE_AXIS));
+			JCheckBox onlyCurrentBox = new JCheckBox("現在有効のみ");
+			onlyCurrentBox.setSelected(true);
+			lowerBox.add(onlyCurrentBox);
+		}
+		panel.add(lowerBox);
+		return panel;
 	}
 
 }
