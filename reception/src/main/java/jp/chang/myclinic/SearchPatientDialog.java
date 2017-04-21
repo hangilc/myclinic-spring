46d5f491bbb5f9e11ed7f9f4a730939dabb71073
Hangil Chang <hangil@chang.jp>
2017-04-21 13:36:23 +0900

    added search patient gui

--- reception/src/main/java/jp/chang/myclinic/SearchPatientDialog.java
+++ reception/src/main/java/jp/chang/myclinic/SearchPatientDialog.java
@@ -14,8 +14,76 @@ class SearchPatientDialog extends JDialog {
 
 	private void setupCenter(){
 		JPanel panel = new JPanel();
-
+		panel.setBorder(BorderFactory.createEmptyBorder(5, 5, 0, 5));
+		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+		panel.add(centerBox1());
 		add(panel, BorderLayout.CENTER);
+	}
+
+	private JComponent centerBox1(){
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		c.insets = new Insets(0, 0, 5, 5);
+		c.anchor = GridBagConstraints.LINE_START;
+		c.gridy = 0;
+		panel.add(new JLabel("名前"), c);
+		c.gridx = 1;
+		{
+			JPanel box = new JPanel();
+			box.setLayout(new BoxLayout(box, BoxLayout.LINE_AXIS));
+			JTextField lastNameTextField = new JTextField(6);
+			box.add(lastNameTextField);
+			box.add(Box.createHorizontalStrut(5));
+			JTextField firstNameTextField = new JTextField(6);
+			box.add(firstNameTextField);
+			box.add(Box.createHorizontalStrut(5));
+			JButton searchByNameButton = new JButton("検索");
+			box.add(searchByNameButton);
+			box.add(Box.createHorizontalGlue());
+			panel.add(box, c);
+		}
+		c.gridx = 0;
+		c.gridy = 1;
+		panel.add(new JLabel("よみ"), c);
+		c.gridx = 1;
+		{
+			JPanel box = new JPanel();
+			box.setLayout(new BoxLayout(box, BoxLayout.LINE_AXIS));
+			JTextField lastNameYomiTextField = new JTextField(6);
+			box.add(lastNameYomiTextField);
+			box.add(Box.createHorizontalStrut(5));
+			JTextField firstNameYomiTextField = new JTextField(6);
+			box.add(firstNameYomiTextField);
+			box.add(Box.createHorizontalStrut(5));
+			JButton searchByYomiButton = new JButton("検索");
+			box.add(searchByYomiButton);
+			box.add(Box.createHorizontalGlue());
+			panel.add(box, c);
+		}
+		c.gridx = 0;
+		c.gridy = 2;
+		c.gridwidth = 2;
+		{
+			JButton recentButton = new JButton("最近の登録");
+			panel.add(recentButton, c);
+		}
+		c.gridx = 0;
+		c.gridy = 3;
+		c.gridwidth = 2;
+		{
+			JList resultList = new JList();
+			resultList.setPreferredSize(new Dimension(400, 300));
+			panel.add(resultList, c);
+		}
+		c.gridx = 0;
+		c.gridy = 4;
+		c.gridwidth = 2;
+		{
+			JButton patientInfoButton = new JButton("患者情報");
+			panel.add(patientInfoButton, c);
+		}
+		return panel;
 	}
 
 	private void setupSouth(){
