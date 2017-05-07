2431acc165164fcb6c30816707a061683e074502
hangilc <hangil@chang.jp>
2017-05-07 16:52:34 +0900

    temporary saving

--- server/src/main/java/jp/chang/myclinic/rcpt/RcptUtil.java
+++ server/src/main/java/jp/chang/myclinic/rcpt/RcptUtil.java
@@ -10,4 +10,15 @@ class RcptUtil {
 		}
 	}
 
+	public static int shochiKingakuToTen(double kingaku){
+		if( kingaku <= 15 )
+			return 0;
+		else
+			return (int)Math.ceil((kingaku - 15)/10) + 1;
+	}
+
+	public static int kizaiKingakuToTen(double kingaku){
+		return (int)Math.round(kingaku/10.0);
+	}
+
 }
