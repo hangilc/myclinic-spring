7d89716e326b9e08df47221cd4aece76bb7d98e0
Hangil Chang <hangil@chang.jp>
2017-05-12 13:38:28 +0900

    temporary saving

--- reception/src/main/java/jp/chang/myclinic/Service.java
+++ reception/src/main/java/jp/chang/myclinic/Service.java
@@ -15,64 +15,64 @@ import retrofit2.converter.jackson.JacksonConverterFactory;
 import retrofit2.adapter.java8.Java8CallAdapterFactory;
 
 class Service {
-	public static interface ServerAPI {
-		@GET("list-wqueue-full")
-		CompletableFuture<List<WqueueFullDTO>> listWqueue();
+    public static interface ServerAPI {
+        @GET("list-wqueue-full")
+        CompletableFuture<List<WqueueFullDTO>> listWqueue();
 
-		@GET("get-patient")
-		CompletableFuture<PatientDTO> getPatient(@Query("patient-id") int patientId);
+        @GET("get-patient")
+        CompletableFuture<PatientDTO> getPatient(@Query("patient-id") int patientId);
 
-		@POST("enter-patient-with-hoken")
-		CompletableFuture<PatientHokenListDTO> enterPatientWithHoken(@Body PatientHokenListDTO patientHokenListDTO);
+        @POST("enter-patient-with-hoken")
+        CompletableFuture<PatientHokenListDTO> enterPatientWithHoken(@Body PatientHokenListDTO patientHokenListDTO);
 
-		@POST("enter-patient")
-		CompletableFuture<Integer> enterPatient(@Body PatientDTO patientDTO);
+        @POST("enter-patient")
+        CompletableFuture<Integer> enterPatient(@Body PatientDTO patientDTO);
 
-		@GET("list-hoken")
-		CompletableFuture<HokenListDTO> listHoken(@Query("patient-id") int patientId);
+        @GET("list-hoken")
+        CompletableFuture<HokenListDTO> listHoken(@Query("patient-id") int patientId);
 
-		@GET("search-patient-by-name")
-		CompletableFuture<List<PatientDTO>> searchPatientByName(@Query("last-name") String lastName, 
-			@Query("first-name") String firstName);
+        @GET("search-patient-by-name")
+        CompletableFuture<List<PatientDTO>> searchPatientByName(@Query("last-name") String lastName, 
+            @Query("first-name") String firstName);
 
-		@GET("search-patient-by-yomi")
-		CompletableFuture<List<PatientDTO>> searchPatientByYomi(@Query("last-name-yomi") String lastNameYomi, 
-			@Query("first-name-yomi") String firstNameYomi);
+        @GET("search-patient-by-yomi")
+        CompletableFuture<List<PatientDTO>> searchPatientByYomi(@Query("last-name-yomi") String lastNameYomi, 
+            @Query("first-name-yomi") String firstNameYomi);
 
-		@GET("list-recently-registered-patients")
-		CompletableFuture<List<PatientDTO>> listRecentlyRegisteredPatients();
+        @GET("list-recently-registered-patients")
+        CompletableFuture<List<PatientDTO>> listRecentlyRegisteredPatients();
 
-		@POST("enter-shahokokuho")
-		CompletableFuture<Integer> enterShahokokuho(@Body ShahokokuhoDTO shahokokuhoDTO);
+        @POST("enter-shahokokuho")
+        CompletableFuture<Integer> enterShahokokuho(@Body ShahokokuhoDTO shahokokuhoDTO);
 
-		@POST("delete-shahokokuho")
-		CompletableFuture<Boolean> deleteShahokokuho(@Body ShahokokuhoDTO shahokokuhoDTO);
+        @POST("delete-shahokokuho")
+        CompletableFuture<Boolean> deleteShahokokuho(@Body ShahokokuhoDTO shahokokuhoDTO);
 
-		@POST("enter-koukikourei")
-		CompletableFuture<Integer> enterKoukikourei(@Body KoukikoureiDTO koukikoureiDTO);
+        @POST("enter-koukikourei")
+        CompletableFuture<Integer> enterKoukikourei(@Body KoukikoureiDTO koukikoureiDTO);
 
-		@POST("delete-koukikourei")
-		CompletableFuture<Boolean> deleteKoukikourei(@Body KoukikoureiDTO koukikoureiDTO);
+        @POST("delete-koukikourei")
+        CompletableFuture<Boolean> deleteKoukikourei(@Body KoukikoureiDTO koukikoureiDTO);
 
-		@POST("enter-kouhi")
-		CompletableFuture<Integer> enterKouhi(@Body KouhiDTO kouhiDTO);
+        @POST("enter-kouhi")
+        CompletableFuture<Integer> enterKouhi(@Body KouhiDTO kouhiDTO);
 
-		@POST("delete-kouhi")
-		CompletableFuture<Boolean> deleteKouhi(@Body KouhiDTO kouhiDTO);
+        @POST("delete-kouhi")
+        CompletableFuture<Boolean> deleteKouhi(@Body KouhiDTO kouhiDTO);
 
-		@GET("get-visit-meisai")
-		CompletableFuture<MeisaiDTO> getVisitMeisai(@Query("visit-id") int visitId);
-	}
+        @GET("get-visit-meisai")
+        CompletableFuture<MeisaiDTO> getVisitMeisai(@Query("visit-id") int visitId);
+    }
 
-	public static ServerAPI api; 
+    public static ServerAPI api; 
 
-	public static void setServerUrl(String serverUrl){
-		Retrofit server = new Retrofit.Builder()
-			.baseUrl(serverUrl)
-			.addConverterFactory(JacksonConverterFactory.create())
-			.addCallAdapterFactory(Java8CallAdapterFactory.create())
-			.build();
-		api = server.create(ServerAPI.class);
-	}
+    public static void setServerUrl(String serverUrl){
+        Retrofit server = new Retrofit.Builder()
+            .baseUrl(serverUrl)
+            .addConverterFactory(JacksonConverterFactory.create())
+            .addCallAdapterFactory(Java8CallAdapterFactory.create())
+            .build();
+        api = server.create(ServerAPI.class);
+    }
 
 }
