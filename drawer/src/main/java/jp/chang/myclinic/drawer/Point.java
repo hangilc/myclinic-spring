6c106738032d205ad6f38ade198337d7990a1f01
hangilc <hangil@chang.jp>
2017-05-17 20:22:59 +0900

    temporary saving

---
+++ drawer/src/main/java/jp/chang/myclinic/drawer/Point.java
@@ -0,0 +1,22 @@
+package jp.chang.myclinic.drawer;
+
+/**
+ * Created by hangil on 2017/05/17.
+ */
+public class Point {
+    private final double x;
+    private final double y;
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+}
