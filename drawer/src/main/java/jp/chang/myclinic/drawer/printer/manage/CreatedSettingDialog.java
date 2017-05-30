3842f3e99fa64be40b8dff81a3ab5fb20b095bfd
Hangil Chang <hangil@chang.jp>
2017-05-30 15:00:47 +0900

    temporary saving

--- drawer/src/main/java/jp/chang/myclinic/drawer/printer/manage/CreatedSettingDialog.java
+++ drawer/src/main/java/jp/chang/myclinic/drawer/printer/manage/CreatedSettingDialog.java
@@ -3,6 +3,7 @@ package jp.chang.myclinic.drawer.printer.manage;
 import jp.chang.myclinic.drawer.printer.AuxSetting;
 import jp.chang.myclinic.drawer.printer.DevmodeInfo;
 import jp.chang.myclinic.drawer.printer.DevnamesInfo;
+import jp.chang.myclinic.drawer.printer.DrawerPrinter;
 import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
@@ -14,8 +15,8 @@ public class CreatedSettingDialog extends JDialog {
     private byte[] devnamesData;
     private byte[] devmodeData;
     private AuxSetting auxSetting;
-    private JButton printerDialogButton = new JButton("変更");
-    private JButton modifyAuxButton = new JButton("変更");
+    private JButton printerDialogButton;
+    private JButton modifyAuxButton;
     private boolean canceled = true;
 
     public CreatedSettingDialog(Window owner, String name, byte[] devnamesData, byte[] devmodeData, AuxSetting auxSetting){
@@ -24,15 +25,20 @@ public class CreatedSettingDialog extends JDialog {
         this.devnamesData = devnamesData;
         this.devmodeData = devmodeData;
         this.auxSetting = auxSetting;
-        setLayout(new MigLayout("fill", "[grow]", ""));
-        add(makeTitle(), "wrap");
-        add(makePrinterInfo(), "grow, wrap");
-        add(makeAuxInfo(), "grow");
-        pack();
+        setupUI();
     }
 
     public boolean isCanceled(){
         return canceled;
+    }
+
+    private void setupUI(){
+        setLayout(new MigLayout("fill", "[grow]", ""));
+        add(makeTitle(), "wrap");
+        add(makePrinterInfo(), "grow, wrap");
+        add(makeAuxInfo(), "grow");
+        bind();
+        pack();
     }
 
     private JComponent makeTitle(){
@@ -44,12 +50,12 @@ public class CreatedSettingDialog extends JDialog {
         panel.setBorder(BorderFactory.createTitledBorder("プリンター設定"));
         DevnamesInfo devnamesInfo = new DevnamesInfo(devnamesData);
         DevmodeInfo devmodeInfo = new DevmodeInfo(devmodeData);
+        printerDialogButton = new JButton("変更");
         panel.add(new JLabel(String.format("%s: %s", "プリンター", devnamesInfo.getDevice())), "gaptop 5, wrap");
         panel.add(new JLabel(String.format("%s: %s", "用紙", devmodeInfo.getPaperSizeLabel())), "wrap");
         panel.add(new JLabel(String.format("%s: %s", "向き", devmodeInfo.getOrientationLabel())), "wrap");
         panel.add(new JLabel(String.format("%s: %s", "印刷品質", devmodeInfo.getPrintQualityLabel())), "wrap");
         panel.add(new JLabel(String.format("%s: %s", "給紙", devmodeInfo.getDefaultSourceLabel())), "wrap");
-        panel.add(new JLabel(String.format("%s: %d", "印刷枚数", devmodeInfo.getCopies())), "wrap");
         panel.add(new JLabel(String.format("%s: %s", "接続", devnamesInfo.getOutput())), "gapbottom 5, wrap");
         panel.add(printerDialogButton, "right");
         return panel;
@@ -57,11 +63,25 @@ public class CreatedSettingDialog extends JDialog {
 
     private JComponent makeAuxInfo(){
         JPanel panel = new JPanel(new MigLayout("inset 0, gapy 0, fill", "[grow]", ""));
+        modifyAuxButton = new JButton("変更");
         panel.setBorder(BorderFactory.createTitledBorder("その他"));
         panel.add(new JLabel(String.format("%s: %s", "dx", auxSetting.getDx())), "gaptop 5, wrap");
         panel.add(new JLabel(String.format("%s: %s", "dx", auxSetting.getDy())), "gapbottom 5, wrap");
         panel.add(modifyAuxButton, "right");
         return panel;
+    }
+
+    private void bind(){
+        printerDialogButton.addActionListener(event -> {
+            DrawerPrinter printer = new DrawerPrinter();
+            DrawerPrinter.DialogResult result = printer.printDialog(null, null);
+            if( result.ok ){
+                devnamesData = result.devnamesData;
+                devmodeData = result.devmodeData;
+                getContentPane().removeAll();
+                setupUI();
+            }
+        });
     }
 
 }
