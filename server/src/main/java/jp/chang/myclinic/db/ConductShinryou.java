84028ce61c32d554b09ffc1a7efedbbad38383e3
hangilc <hangil@chang.jp>
2017-06-02 23:16:39 +0900

    removed master_valid_from visit_conduct_shinryou;

--- server/src/main/java/jp/chang/myclinic/db/ConductShinryou.java
+++ server/src/main/java/jp/chang/myclinic/db/ConductShinryou.java
@@ -1,24 +1,6 @@
 package jp.chang.myclinic.db;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Column;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinColumns;
-import javax.persistence.FetchType;
-import javax.persistence.Transient;
-import java.sql.Date;
-
-import org.hibernate.annotations.NotFound;
-import org.hibernate.annotations.NotFoundAction;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.math.BigDecimal;
+import javax.persistence.*;
 
 @Entity
 @Table(name="visit_conduct_shinryou")
@@ -70,39 +52,39 @@ public class ConductShinryou {
 		this.shinryoucode = shinryoucode;
 	}
 
-	@Column(name="master_valid_from")
-	private Date masterValidFrom;
-
-	public Date getMasterValidFrom(){
-		return masterValidFrom;
-	}
-
-	public void setMasterValidFrom(Date masterValidFrom){
-		this.masterValidFrom = masterValidFrom;
-	}
-
-	@ManyToOne(fetch=FetchType.LAZY)
-	@JoinColumns({
-		@JoinColumn(name="shinryoucode", referencedColumnName="shinryoucode", insertable=false, updatable=false),
-		@JoinColumn(name="master_valid_from", referencedColumnName="valid_from", insertable=false, updatable=false),
-	})
-	private ShinryouMaster master;
-
-	public ShinryouMaster getMaster(){
-		return master;
-	}
-
-	public void setMaster(ShinryouMaster master){
-		this.master = master;
-	}
+//	@Column(name="master_valid_from")
+//	private Date masterValidFrom;
+//
+//	public Date getMasterValidFrom(){
+//		return masterValidFrom;
+//	}
+//
+//	public void setMasterValidFrom(Date masterValidFrom){
+//		this.masterValidFrom = masterValidFrom;
+//	}
+//
+//	@ManyToOne(fetch=FetchType.LAZY)
+//	@JoinColumns({
+//		@JoinColumn(name="shinryoucode", referencedColumnName="shinryoucode", insertable=false, updatable=false),
+//		@JoinColumn(name="master_valid_from", referencedColumnName="valid_from", insertable=false, updatable=false),
+//	})
+//	private ShinryouMaster master;
+//
+//	public ShinryouMaster getMaster(){
+//		return master;
+//	}
+//
+//	public void setMaster(ShinryouMaster master){
+//		this.master = master;
+//	}
 
 	@Override
 	public String toString(){
 		return "ConductShinryou[" +
 			"conductShinryouId=" + conductShinryouId + ", " +
 			"conductId=" + conductId + ", " +
-			"shinryoucode=" + shinryoucode + ", " +
-			"masterValidFrom=" + masterValidFrom +
+			"shinryoucode=" + shinryoucode + //", " +
+			//"masterValidFrom=" + masterValidFrom +
 		"]";
 	}
 }
