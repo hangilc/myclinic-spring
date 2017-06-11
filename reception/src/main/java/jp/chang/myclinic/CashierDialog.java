fc722ad1d802b6be016f2d1fd9d65191d3f46370
hangilc <hangil@chang.jp>
2017-06-11 16:40:11 +0900

    revised cashier dialog

--- reception/src/main/java/jp/chang/myclinic/CashierDialog.java
+++ reception/src/main/java/jp/chang/myclinic/CashierDialog.java
@@ -13,6 +13,8 @@ import java.awt.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static java.awt.Font.BOLD;
+
 class CashierDialog extends JDialog {
 
 	private MeisaiDTO meisai;
@@ -46,14 +48,6 @@ class CashierDialog extends JDialog {
 
 	private JComponent makeCenter(){
 		JPanel panel = new JPanel(new MigLayout("insets 0 0 0 0", "[]", "[]"));
-//		{
-//			JLabel label = new JLabel(makeLabelContent());
-//			label.setHorizontalAlignment(SwingConstants.LEFT);
-//			label.setVerticalAlignment(SwingConstants.TOP);
-//			label.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-//			JScrollPane scroll = new JScrollPane(label);
-//			panel.add(scroll, "width :400:, height :300:, top, grow");
-//		}
 		panel.add(makeMeisaiDetail(), "");
 		{
 			JPanel box = new JPanel(new MigLayout("insets 0 0 0 0", "", ""));
@@ -73,6 +67,33 @@ class CashierDialog extends JDialog {
 		panel.add(new JLabel("総点：" + NumberUtil.formatNumber(meisai.totalTen) + "点"), "gaptop 5, wrap");
 		panel.add(new JLabel("負担割：" + meisai.futanWari + "割"), "wrap");
 		panel.add(new JLabel("請求額：" + NumberUtil.formatNumber(meisai.charge) + "円"), "wrap");
+		if( payments.size() > 0 ){
+			PaymentDTO payment = payments.get(0);
+			panel.add(new JLabel("前回徴収額：" + NumberUtil.formatNumber(payment.amount) + "円"), "wrap");
+		}
+		panel.add(makeChargePane(), "gaptop 5");
+		return panel;
+	}
+
+	private JComponent makeChargePane(){
+		JPanel panel = new JPanel(new MigLayout("insets 3, gapy 0", "", ""));
+		int diff = 0;
+		if( payments.size() > 0 ){
+			PaymentDTO payment = payments.get(0);
+			diff = charge.charge - payment.amount;
+		}
+		panel.setBorder(BorderFactory.createLineBorder(diff >= 0 ? Color.BLACK : Color.RED, 2));
+		Font curFont = getFont();
+		Font font = curFont.deriveFont(BOLD, curFont.getSize() * 1.5f);
+		String labelString = "請求額：" + NumberUtil.formatNumber(meisai.charge) + "円";
+		if( diff > 0 ){
+			labelString += "（" + NumberUtil.formatNumber(diff) + "円 追加徴収）";
+		} else if( diff < 0 ){
+			labelString += "（" + NumberUtil.formatNumber(-diff) + "円 返金）";
+		}
+		JLabel label = new JLabel(labelString);
+		label.setFont(font);
+		panel.add(label);
 		return panel;
 	}
 
@@ -81,74 +102,6 @@ class CashierDialog extends JDialog {
 		panel.add(doneButton, "sizegroup btn");
 		panel.add(cancelButton, "sizegroup btn");
 		return panel;
-	}
-
-	private String makeLabelContent(){
-		StringBuilder sb = new StringBuilder();
-		sb.append("<html>");
-		addTopline(sb);
-		addSections(sb);
-		addSummary(sb);
-		sb.append("</html>");
-		return sb.toString();
-	}
-
-	private void addTopline(StringBuilder sb){
-		sb.append(String.format("<div>%s %s (%s %s) 様 患者番号：%s</div>",
-				patient.lastName, patient.firstName, patient.lastNameYomi, patient.firstNameYomi,
-				patient.patientId));
-	}
-
-	private void addSections(StringBuilder sb){
-		sb.append("<table>");
-		for(MeisaiSectionDTO section: meisai.sections){
-			addSection(sb, section);
-		}
-		sb.append("</table>");
-	}
-
-	private void addSection(StringBuilder sb, MeisaiSectionDTO section){
-		sb.append("<tr>");
-		sb.append("<td span='2'>");
-		sb.append(String.format("[%s] (%d)", section.label, section.sectionTotalTen));
-		sb.append("</td>");
-		sb.append("</tr>");
-		for(SectionItemDTO item: section.items){
-			sb.append("<tr>");
-			sb.append("<td>");
-			sb.append(item.label);
-			sb.append("</td>");
-			sb.append("<td align='right'>");
-			sb.append(String.format("%dx%d=%d", item.tanka, item.count, item.tanka * item.count));
-			sb.append("</td>");
-			sb.append("</tr>");
-		}
-	}
-
-	private void addSummary(StringBuilder sb){
-		sb.append("<div>");
-		sb.append("<div>");
-		sb.append(String.format("総点 %d点", meisai.totalTen));
-		sb.append("</div>");
-		sb.append("<div>");
-		sb.append(String.format("負担割 %d割", meisai.futanWari));
-		sb.append("</div>");
-		sb.append("<div>");
-		if( payments.size() > 0 ){
-			int chargeValue = charge.charge;
-			int prevPay = payments.get(0).amount;
-			int diff = chargeValue - prevPay;
-			if( diff > 0 ){
-				sb.append(String.format("追加請求額 %d円 （総請求額 %d円、以前の徴収額 %d円）", diff, charge.charge,  prevPay));
-			}
-		} else {
-			sb.append(String.format("請求額 %d円", charge.charge));
-		}
-		if (meisai.charge != charge.charge) {
-			sb.append(String.format("??? 請求額(%d)が計算値（%d）と異なります。", charge.charge, meisai.charge));
-		}
-		sb.append("</div>");
-		sb.append("</div>");
 	}
 
 	private String patientLabel(PatientDTO patient){
