944d44b79663769fd5045fe3571740af404fa975
Hangil Chang <hangil@chang.jp>
2017-06-12 16:40:05 +0900

    cleanup

--- server/src/main/java/jp/chang/myclinic/db/PharmaQueueRepository.java
+++ server/src/main/java/jp/chang/myclinic/db/PharmaQueueRepository.java
@@ -14,16 +14,6 @@ public interface PharmaQueueRepository extends CrudRepository<PharmaQueue, Integ
     Optional<PharmaQueue> findByVisitId(int visitId);
 
     @Query("select queue, patient from PharmaQueue queue, Visit visit, Patient patient " +
-            " where queue.visitId = visit.visitId and visit.patientId = patient.patientId " +
-            " and queue.pharmaState = 0 ")
-    List<Object[]> findFullForPrescription();
-
-    @Query("select queue, patient from PharmaQueue queue, Visit visit, Patient patient " +
-            " where queue.visitId = visit.visitId and visit.patientId = patient.patientId " +
-            " and date(visit.visitedAt) = date(now()) ")
-    List<Object[]> findFullForToday();
-
-    @Query("select queue, patient from PharmaQueue queue, Visit visit, Patient patient " +
             " where queue.visitId = visit.visitId and visit.patientId = patient.patientId ")
     List<Object[]> findFull();
 
