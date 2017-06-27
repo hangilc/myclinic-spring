c64fee29c708a2d0ca990f9c3068f49c5ae71f7c
Hangil Chang <hangil@chang.jp>
2017-06-27 19:23:33 +0900

    temporary saving

--- pharma/src/main/java/jp/chang/myclinic/pharma/WrappedText.java
+++ pharma/src/main/java/jp/chang/myclinic/pharma/WrappedText.java
@@ -2,19 +2,21 @@ package jp.chang.myclinic.pharma;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 class WrappedText extends JPanel {
 
     private int width;
     private int ascent;
     private int fontSize;
-    private List<StringItem> stringItems = new ArrayList<>();
+    private List<Item> items = new ArrayList<>();
     private int posX;
     private int posY;
     private int botY;
-    private Font defaultFont;
 
     public WrappedText(int width){
         setLayout(null);
@@ -23,7 +25,14 @@ class WrappedText extends JPanel {
         this.ascent = getFontMetrics(font).getAscent();
         this.fontSize = font.getSize();
         this.botY = this.fontSize;
-        defaultFont = getFont();
+        addMouseListener(new MouseAdapter(){
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                Point p = e.getPoint();
+                System.out.println(p);
+            }
+        });
     }
 
     public WrappedText(String text, int width){
@@ -31,13 +40,14 @@ class WrappedText extends JPanel {
         appendString(text);
     }
 
-    public void appendString(String text){
+    private List<StringItem> breakToLines(String text){
+        List<StringItem> result = new ArrayList<>();
         FontMetrics fm = getFontMetrics(getFont());
         StringBuilder sb = new StringBuilder();
         int x = posX;
         for(char ch: text.toCharArray()){
             if( ch == '\n' ){
-                stringItems.add(new StringItem(posX, posY, sb.toString()));
+                result.add(new StringItem(posX, posY + ascent, sb.toString()));
                 sb.setLength(0);
                 newline();
                 x = 0;
@@ -45,7 +55,7 @@ class WrappedText extends JPanel {
             }
             int cw = fm.charWidth(ch);
             if( x > 0 && x + cw > width ){
-                stringItems.add(new StringItem(posX, posY, sb.toString()));
+                result.add(new StringItem(posX, posY + ascent, sb.toString()));
                 sb.setLength(0);
                 newline();
                 x = 0;
@@ -54,9 +64,14 @@ class WrappedText extends JPanel {
             x += cw;
         }
         if( sb.length() > 0 ){
-            stringItems.add(new StringItem(posX, posY, sb.toString()));
+            result.add(new StringItem(posX, posY + ascent, sb.toString()));
             posX = x;
         }
+        return result;
+    }
+
+    private void appendString(String text){
+        items.addAll(breakToLines(text));
         setAllSizes();
     }
 
@@ -73,6 +88,13 @@ class WrappedText extends JPanel {
         }
         add(component);
         setAllSizes();
+    }
+
+    public void appendLink(String text, Runnable action){
+        List<LinkItem> linkItems = breakToLines(text).stream()
+                .map(stringItem -> new LinkItem(stringItem, action))
+                .collect(Collectors.toList());
+        items.addAll(linkItems);
     }
 
     private void setAllSizes(){
@@ -95,13 +117,17 @@ class WrappedText extends JPanel {
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        for(StringItem item: stringItems){
-            g.drawString(item.text, item.x, item.y + ascent);
+        for(Item item: items) {
+            item.render(g);
         }
         paintComponents(g);
     }
 
-    private static class StringItem {
+    private interface Item {
+        void render(Graphics g);
+    }
+
+    private static class StringItem implements Item {
         int x;
         int y;
         String text;
@@ -110,6 +136,11 @@ class WrappedText extends JPanel {
             this.x = x;
             this.y = y;
             this.text = text;
+        }
+
+        @Override
+        public void render(Graphics g){
+            g.drawString(this.text, this.x, this.y);
         }
 
         @Override
@@ -122,66 +153,30 @@ class WrappedText extends JPanel {
         }
     }
 
-//    private interface Src {
-//        void append();
-//    }
-//
-//    private class SrcString implements Src {
-//        private String text;
-//
-//        public SrcString(String src){
-//            this.text = src;
-//        }
-//
-//        public void append(){
-//            FontMetrics fm = getFontMetrics(getFont());
-//            StringBuilder sb = new StringBuilder();
-//            int x = posX;
-//            for(char ch: text.toCharArray()){
-//                if( ch == '\n' ){
-//                    stringItems.add(new StringItem(posX, posY, sb.toString()));
-//                    sb.setLength(0);
-//                    newline();
-//                    x = 0;
-//                    continue;
-//                }
-//                int cw = fm.charWidth(ch);
-//                if( x > 0 && x + cw > width ){
-//                    stringItems.add(new StringItem(posX, posY, sb.toString()));
-//                    sb.setLength(0);
-//                    newline();
-//                    x = 0;
-//                }
-//                sb.append(ch);
-//                x += cw;
-//            }
-//            if( sb.length() > 0 ){
-//                stringItems.add(new StringItem(posX, posY, sb.toString()));
-//                posX = x;
-//            }
-//        }
-//    }
-//
-//    private class SrcComponent implements Src {
-//        private JComponent component;
-//
-//        public SrcComponent(JComponent component){
-//            this.component = component;
-//            add(component);
-//        }
-//
-//        public void append(){
-//            Dimension dim = component.getPreferredSize();
-//            if( posX > 0 && dim.getWidth() + posX > width ){
-//                newline();
-//            }
-//            component.setBounds(posX, posY, (int)dim.getWidth(), (int)dim.getHeight());
-//            posX += (int)dim.getWidth();
-//            int y = posY + (int)dim.getHeight();
-//            if( y > botY ){
-//                botY = y;
-//            }
-//        }
-//    }
+    private static class LinkItem implements Item {
+        StringItem stringItem;
+        Runnable runnable;
+
+        LinkItem(StringItem stringItem, Runnable runnable){
+            this.stringItem = stringItem;
+            this.runnable = runnable;
+        }
+
+        @Override
+        public void render(Graphics g){
+            Color colorSave = g.getColor();
+            g.setColor(Color.BLUE);
+            stringItem.render(g);
+            g.setColor(colorSave);
+        }
+
+        @Override
+        public String toString() {
+            return "LinkItem{" +
+                    "stringItem=" + stringItem +
+                    ", runnable=" + runnable +
+                    '}';
+        }
+    }
 
 }
