82c2c3f43181974ff325c6169aaa160f4ab53867
hangilc <hangil@chang.jp>
2017-07-19 00:23:36 +0900

    added multiple databases

--- server/src/main/java/jp/chang/myclinic/MyclinicConfig.java
+++ server/src/main/java/jp/chang/myclinic/MyclinicConfig.java
@@ -1,19 +1,22 @@
 package jp.chang.myclinic;
 
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 import java.util.HashMap;
 
 @Configuration
-@PropertySource({ "classpath:persistence-myclinic-db.properties"})
+@EnableTransactionManagement
 @EnableJpaRepositories(
         basePackages = "jp.chang.myclinic.db.myclinic",
         entityManagerFactoryRef = "myclinicEntityManager",
@@ -37,7 +40,16 @@ public class MyclinicConfig {
 
     @Primary
     @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource myclinicDataSource(){
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        return DataSourceBuilder.create().build();
+    }
+
+    @Primary
+    @Bean
+    public PlatformTransactionManager myclinicTransactionManager(){
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(myclinicEntityManager().getObject());
+        return transactionManager;
     }
 }
