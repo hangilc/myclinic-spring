1b8f9d426933bb717a433770f4bfc543e440edec
hangilc <hangil@chang.jp>
2017-08-20 22:28:21 +0900

    reworked hoken disp

--- practice/src/main/java/jp/chang/myclinic/practice/leftpane/Record.java
+++ practice/src/main/java/jp/chang/myclinic/practice/leftpane/Record.java
@@ -2,6 +2,7 @@ package jp.chang.myclinic.practice.leftpane;
 
 import jp.chang.myclinic.dto.VisitFull2DTO;
 import jp.chang.myclinic.practice.MainExecContext;
+import jp.chang.myclinic.practice.leftpane.hoken.HokenDisp;
 import jp.chang.myclinic.practice.leftpane.text.TextArea;
 import jp.chang.myclinic.practice.leftpane.title.Title;
 import net.miginfocom.swing.MigLayout;
@@ -10,11 +11,16 @@ import javax.swing.*;
 
 class Record extends JPanel {
 
+    private VisitFull2DTO fullVisit;
+    private MainExecContext mainExecContext;
+    private int colWidth;
     private Title title;
     private TextArea textArea;
 
     Record(VisitFull2DTO fullVisit, int width, MainExecContext mainExecContext){
-        int colWidth = (width - 4) / 2;
+        this.fullVisit = fullVisit;
+        this.mainExecContext = mainExecContext;
+        colWidth = (width - 4) / 2;
         int gap = width - colWidth * 2;
         setLayout(new MigLayout("insets 0, debug",
                 String.format("[%dpx!]%d[%dpx!]", colWidth, gap, colWidth),
@@ -22,7 +28,13 @@ class Record extends JPanel {
         title = new Title(fullVisit.visit, mainExecContext);
         textArea = new TextArea(fullVisit, colWidth);
         add(title, "span, growx, wrap");
-        add(textArea);
-        add(new JLabel("right"));
+        add(textArea, "top");
+        add(makeRightColumn(), "top");
+    }
+
+    private JComponent makeRightColumn(){
+        JPanel panel = new JPanel(new MigLayout("insets 0", String.format("[%dpx!]", colWidth), ""));
+        panel.add(new HokenDisp(fullVisit.hoken, fullVisit.visit));
+        return panel;
     }
 }
