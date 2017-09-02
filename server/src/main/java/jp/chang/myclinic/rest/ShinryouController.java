f22e6133f0ee7a0b30d8ad4e17789265fada76ef
Hangil Chang <hangil@chang.jp>
2017-09-02 16:54:47 +0900

    temporary saving

--- server/src/main/java/jp/chang/myclinic/rest/ShinryouController.java
+++ server/src/main/java/jp/chang/myclinic/rest/ShinryouController.java
@@ -1,15 +1,21 @@
 package jp.chang.myclinic.db;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.transaction.annotation.Transactional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
 import jp.chang.myclinic.db.myclinic.DbGateway;
-import jp.chang.myclinic.dto.*;
+import jp.chang.myclinic.db.myclinic.ShinryouMaster;
+import jp.chang.myclinic.dto.ShinryouFullDTO;
+import jp.chang.myclinic.dto.VisitDTO;
+import jp.chang.myclinic.mastermap.MasterMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/json")
@@ -18,10 +24,31 @@ public class ShinryouController {
 
 	@Autowired
 	private DbGateway dbGateway;
+	@Autowired
+	private MasterMap masterMap;
 
 	@RequestMapping(value="/get-shinryou-full", method=RequestMethod.GET)
 	public ShinryouFullDTO getShinryouFull(@RequestParam("shinryou-id") int shinryouId){
 		return dbGateway.getShinryouFull(shinryouId);
+	}
+
+	@RequestMapping(value="/batch-enter-shinryou-by-name", method=RequestMethod.POST)
+	public List<Integer> batchEnterShinryou(@RequestParam("visit-id") int visitId,
+											@RequestParam("name") List<String> names){
+		VisitDTO visit = dbGateway.getVisit(visitId);
+		LocalDate at = LocalDate.parse(visit.visitedAt.substring(0, 10));
+	}
+
+	private Optional<Integer> mapShinryouName(String shinryouName){
+		return masterMap.getShinryoucodeByName(shinryouName);
+	}
+
+	private Integer resolveShinryoucode(int shinryoucode, LocalDate at){
+		return masterMap.resolveShinryouCode(shinryoucode, at);
+	}
+
+	private ShinryouMaster getMaster(int shinryoucode, LocalDate at){
+
 	}
 
 }
