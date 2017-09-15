0072927b16c246b66f87b3c0cffd36fb0e346ff6
Hangil Chang <hangil@chang.jp>
2017-09-15 18:56:49 +0900

    adding conduct editor

--- practice/src/main/java/jp/chang/myclinic/practice/leftpane/conduct/ConductEditor.java
+++ practice/src/main/java/jp/chang/myclinic/practice/leftpane/conduct/ConductEditor.java
@@ -1,8 +1,7 @@
 package jp.chang.myclinic.practice.leftpane.conduct;
 
 import jp.chang.myclinic.consts.ConductKind;
-import jp.chang.myclinic.dto.ConductFullDTO;
-import jp.chang.myclinic.dto.GazouLabelDTO;
+import jp.chang.myclinic.dto.*;
 import jp.chang.myclinic.practice.Link;
 import jp.chang.myclinic.practice.Service;
 import jp.chang.myclinic.practice.WrappedText;
@@ -13,6 +12,7 @@ import net.miginfocom.swing.MigLayout;
 import javax.swing.*;
 import java.awt.*;
 import java.util.Arrays;
+import java.util.List;
 
 class ConductEditor extends JPanel {
 
@@ -29,7 +29,7 @@ class ConductEditor extends JPanel {
     ConductEditor(int width, ConductFullDTO conductFull){
         this.width = width;
         this.conductFull = conductFull;
-        setLayout(new MigLayout("insets 0", String.format("[%dpx!]", width), ""));
+        setLayout(new MigLayout("insets 0, hidemode 3", String.format("[%dpx!]", width), ""));
         add(makeSubmenu(), "wrap");
         add(makeKindArea(), "wrap");
         add(makeGazouLabelArea(), "wrap");
@@ -101,42 +101,60 @@ class ConductEditor extends JPanel {
 
     private Component makeShinryouArea(){
         JPanel panel = new JPanel(new MigLayout("insets 0, gapx 0", "", ""));
-        conductFull.conductShinryouList.forEach(shinryou -> {
-            Link deleteLink = new Link("削除");
-            int rightWidth = deleteLink.getPreferredSize().width;
-            int leftWidth = width - rightWidth - 6;
-            WrappedText text = new WrappedText(leftWidth, shinryou.master.name);
-            panel.add(text, "gapright 4");
-            panel.add(deleteLink, "wrap");
-        });
+        List<ConductShinryouFullDTO> shinryouList = conductFull.conductShinryouList;
+        if( shinryouList.size() == 0 ){
+            panel.setVisible(false);
+        } else {
+            for(int i=0;i<shinryouList.size();i++){
+                ConductShinryouFullDTO shinryou = shinryouList.get(i);
+                Link deleteLink = new Link("削除");
+                int rightWidth = deleteLink.getPreferredSize().width;
+                int leftWidth = width - rightWidth - 6;
+                WrappedText text = new WrappedText(leftWidth, shinryou.master.name);
+                panel.add(text, "gapright 4");
+                panel.add(deleteLink, i == shinryouList.size() - 1 ? "" : "wrap");
+            };
+        }
         return panel;
     }
 
     private Component makeDrugArea(){
         JPanel panel = new JPanel(new MigLayout("insets 0, gapx 0", "", ""));
-        conductFull.conductDrugs.forEach(drug -> {
-            Link deleteLink = new Link("削除");
-            int rightWidth = deleteLink.getPreferredSize().width;
-            int leftWidth = width - rightWidth - 6;
-            String rep = DrugUtil.conductDrugRep(drug);
-            WrappedText text = new WrappedText(leftWidth, rep);
-            panel.add(text, "gapright 4");
-            panel.add(deleteLink, "wrap");
-        });
+        List<ConductDrugFullDTO> drugs = conductFull.conductDrugs;
+        if( drugs.size() == 0 ){
+            panel.setVisible(false);
+        } else {
+            for(int i=0;i<drugs.size();i++) {
+                ConductDrugFullDTO drug = drugs.get(i);
+                Link deleteLink = new Link("削除");
+                int rightWidth = deleteLink.getPreferredSize().width;
+                int leftWidth = width - rightWidth - 6;
+                String rep = DrugUtil.conductDrugRep(drug);
+                WrappedText text = new WrappedText(leftWidth, rep);
+                panel.add(text, "gapright 4");
+                panel.add(deleteLink, i == drugs.size() - 1 ? "" : "wrap");
+            };
+        }
         return panel;
     }
 
     private Component makeKizaiArea(){
         JPanel panel = new JPanel(new MigLayout("insets 0, gapx 0", "", ""));
-        conductFull.conductKizaiList.forEach(kizai -> {
-            Link deleteLink = new Link("削除");
-            int rightWidth = deleteLink.getPreferredSize().width;
-            int leftWidth = width - rightWidth - 6;
-            String rep = KizaiUtil.kizaiRep(kizai);
-            WrappedText text = new WrappedText(leftWidth, rep);
-            panel.add(text, "gapright 4");
-            panel.add(deleteLink, "wrap");
-        });
+        List<ConductKizaiFullDTO> kizaiList = conductFull.conductKizaiList;
+        if( kizaiList.size() == 0 ){
+            panel.setVisible(false);
+        } else {
+            for(int i = 0;i<kizaiList.size();i++) {
+                ConductKizaiFullDTO kizai = kizaiList.get(i);
+                Link deleteLink = new Link("削除");
+                int rightWidth = deleteLink.getPreferredSize().width;
+                int leftWidth = width - rightWidth - 6;
+                String rep = KizaiUtil.kizaiRep(kizai);
+                WrappedText text = new WrappedText(leftWidth, rep);
+                panel.add(text, "gapright 4");
+                panel.add(deleteLink, "wrap");
+            };
+        }
         return panel;
     }
 
