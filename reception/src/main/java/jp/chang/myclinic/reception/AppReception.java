f6a4b22f5a8825b53a21b9204fc9d7c0bbea1c29
hangilc <hangil@chang.jp>
2017-11-05 23:21:38 +0900

    cleanup

--- reception/src/main/java/jp/chang/myclinic/reception/AppReception.java
+++ reception/src/main/java/jp/chang/myclinic/reception/AppReception.java
@@ -34,59 +34,6 @@ public class AppReception
 			}).start();
 		});
 
-//    	if( args.length == 0 ){
-//    		System.out.println("Usage: server-url");
-//    		System.exit(1);
-//    	}
-//    	{
-//    		String serverUrl = args[0];
-//    		if( !serverUrl.endsWith("/") ){
-//    			serverUrl = serverUrl + "/";
-//    		}
-//            Service.setServerUrl(serverUrl);
-//    	}
-//    	readConfig();
-//        EventQueue.invokeLater(() -> {
-//        	try{
-//	        	UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-//	        } catch(Exception ex){
-//	        	ex.printStackTrace();
-//	        }
-//        	MainFrame mainFrame = new MainFrame();
-//        	mainFrame.setLocationByPlatform(true);
-//        	mainFrame.setVisible(true);
-//        	mainFrame.doUpdateWqueue();
-//			new Timer(2000, event -> {
-//				mainFrame.doUpdateWqueue();
-//			}).start();
-//        });
     }
 
-    private static Path getDefaultConfigFile(){
-    	return Paths.get(System.getProperty("user.dir"), "myclinic-reception.properties");
-	}
-
-	private static Path getDefaultPrinterSettingsDir(){
-		return Paths.get(System.getProperty("user.dir"));
-	}
-
-    private static void readConfig() throws IOException {
-//    	Path configPath = Paths.get(System.getProperty("user.home"), "myclinic-reception.properties");
-//    	if( Files.exists(configPath) ){
-//    		try(BufferedReader reader = Files.newBufferedReader(configPath, StandardCharsets.UTF_8)){
-//    			Properties props = new Properties();
-//    			props.load(reader);
-//				String settingName = props.getProperty("printer-setting-name");
-//				if( settingName != null ){
-//					ReceptionConfigOld.INSTANCE.setCurrentSetting(settingName);
-//				}
-//			}
-//		} else {
-//    		try(BufferedWriter writer = Files.newBufferedWriter(configPath, StandardCharsets.UTF_8)){
-//				Properties props = new Properties();
-//				props.setProperty("printer-setting-name", "");
-//				props.store(writer, "");
-//			}
-//		}
-	}
 }
