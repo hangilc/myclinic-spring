b88f27eb7f4bc8ecf34189370d93b607a49b72d7
hangilc <hangil@chang.jp>
2017-12-31 11:54:43 +0900

    temporary saving

--- reception/src/main/java/jp/chang/myclinic/reception/javafx/EditPatientSceneController.java
+++ reception/src/main/java/jp/chang/myclinic/reception/javafx/EditPatientSceneController.java
@@ -1,8 +1,48 @@
 package jp.chang.myclinic.reception.javafx;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import jp.chang.myclinic.dto.PatientDTO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EditPatientSceneController {
+
+    @FXML
+    private TextField lastNameInput;
+    @FXML
+    private TextField firstNameInput;
+    @FXML
+    private TextField lastNameYomiInput;
+    @FXML
+    private TextField firstNameYomiInput;
+    @FXML
+    private ComboBox<String> birthdayGengouInput;
+    @FXML
+    private TextField birthdayNenInput;
+    @FXML
+    private TextField birthdayMonthInput;
+    @FXML
+    private TextField birthdayDayInput;
+    @FXML
+    private RadioButton sexMaleRadio;
+    @FXML
+    private RadioButton sexFemaleRadio;
+    @FXML
+    private TextField addressInput;
+    @FXML
+    private TextField phoneInput;
+
+    @FXML
+    public void initialize(){
+        birthdayGengouInput.getItems().addAll("平成", "昭和", "大正", "明治");
+        birthdayGengouInput.getSelectionModel().select("昭和");
+    }
+
     public void onNewShahokokuhoClick(ActionEvent actionEvent) {
         EditShahokokuhoStage stage = new EditShahokokuhoStage();
         stage.show();
@@ -16,5 +56,38 @@ public class EditPatientSceneController {
     public void onNewKouhiClick(ActionEvent actionEvent) {
         EditKouhiStage stage = new EditKouhiStage();
         stage.show();
+    }
+
+    public void onEnterPatientClick(ActionEvent actionEvent) {
+        System.out.println(lastNameInput.getText());
+    }
+
+    private List<String> save(PatientDTO patient){
+        List<String> errs = new ArrayList<>();
+        String lastName = lastNameInput.getText();
+        if( lastName.isEmpty() ){
+            errs.add("性が入力されていません。");
+        } else {
+            patient.lastName = lastName;
+        }
+        String firstName = firstNameInput.getText();
+        if( firstName.isEmpty() ){
+            errs.add("名が入力されていません。");
+        } else {
+            patient.firstName = firstName;
+        }
+        String lastNameYomi = lastNameYomiInput.getText();
+        if( lastNameYomi.isEmpty() ){
+            errs.add("性のよみが入力されていません。");
+        } else {
+            patient.lastNameYomi = lastNameYomi;
+        }
+        String firstNameYomi = firstNameYomiInput.getText();
+        if( firstNameYomi.isEmpty() ){
+            errs.add("名のよみが入力されていません。");
+        } else {
+            patient.firstNameYomi = firstNameYomi;
+        }
+        return errs;
     }
 }
---
+++ reception/src/main/java/jp/chang/myclinic/reception/lib/DateUtil.java
@@ -0,0 +1,57 @@
+package jp.chang.myclinic.reception.lib;
+
+import jp.chang.myclinic.consts.Gengou;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.chrono.JapaneseChronology;
+import java.time.chrono.JapaneseEra;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DateUtil {
+
+    public static class Result<T> {
+        public T value;
+        public List<String> errors = new ArrayList<>();
+
+        public boolean hasError(){
+            return errors.size() > 0;
+        }
+
+        public void addError(String err){
+            errors.add(err);
+        }
+    }
+
+    public Result<LocalDate> convertToLocalDate(String gengou, String nen, String month, String day){
+        Result<LocalDate> result = new Result<>();
+        JapaneseEra era = Gengou.fromKanji(gengou).getEra();
+        int nenValue = 0, monthValue = 0, dayValue = 9;
+        try {
+            nenValue = Integer.parseInt(nen);
+        } catch(NumberFormatException ex){
+            result.addError("年の値が適切でありません。");
+        }
+        try {
+            monthValue = Integer.parseInt(month);
+        } catch(NumberFormatException ex){
+            result.addError("月の値が適切でありません。");
+        }
+        try {
+            dayValue = Integer.parseInt(day);
+        } catch(NumberFormatException ex){
+            result.addError("日の値が適切でありません。");
+        }
+        if( !result.hasError() ){
+            int year = JapaneseChronology.INSTANCE.prolepticYear(era, nenValue);
+            try {
+                result.value = LocalDate.of(year, monthValue, dayValue);
+            } catch(DateTimeException ex){
+                result.addError("入力した日付が不適切です。");
+            }
+        }
+        return result;
+    }
+
+}
