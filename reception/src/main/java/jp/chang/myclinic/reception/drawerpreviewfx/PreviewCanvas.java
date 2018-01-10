bd4f3f96584dab4f868ca9ff62dd97a97f9f38dd
hangilc <hangil@chang.jp>
2018-01-10 15:12:07 +0900

    added drawer preview canvas

--- reception/src/main/java/jp/chang/myclinic/reception/drawerpreviewfx/PreviewCanvas.java
+++ reception/src/main/java/jp/chang/myclinic/reception/drawerpreviewfx/PreviewCanvas.java
@@ -3,6 +3,8 @@ package jp.chang.myclinic.reception.drawerpreviewfx;
 import javafx.geometry.VPos;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
@@ -17,14 +19,14 @@ import java.util.Map;
 
 public class PreviewCanvas extends Canvas {
 
-    private static Logger logger = LoggerFactory.getLogger(PreviewCanvas.class);
-    private static final double pixelPerMilliMeter;
-    static {
-        double inch = Units.mmToInch(1.0);
-        pixelPerMilliMeter = 96 * inch;
+    private enum StrokeMode {
+        NONE, TEXT, LINE
     }
-    private GraphicsContext gc;
-    private Map<String, Font> fontMap = new HashMap<>();
+
+    private static class StrokeEnv {
+        Paint paint;
+        double width;
+    }
 
     private static class PosList {
         private List<Double> list;
@@ -42,10 +44,24 @@ public class PreviewCanvas extends Canvas {
         }
     }
 
+    private static Logger logger = LoggerFactory.getLogger(PreviewCanvas.class);
+    private static final double pixelPerMilliMeter;
+    static {
+        double inch = Units.mmToInch(1.0);
+        pixelPerMilliMeter = 96 * inch;
+    }
+    private GraphicsContext gc;
+    private Map<String, Font> fontMap = new HashMap<>();
+    private Map<String, StrokeEnv> penMap = new HashMap<>();
+    private StrokeMode strokeMode = StrokeMode.NONE;
+    private StrokeEnv savedTextStrokeEnv;
+    private StrokeEnv savedLineStrokeEnv;
+
     public PreviewCanvas(List<Op> ops){
         super(600, 600);
         gc = getGraphicsContext2D();
         gc.setTextBaseline(VPos.TOP);
+        gc.setLineWidth(0.5);
         for(Op op: ops){
             switch(op.getOpCode()){
                 case MoveTo: { doMoveTo((OpMoveTo)op); break; }
@@ -53,9 +69,70 @@ public class PreviewCanvas extends Canvas {
                 case CreateFont: { doCreateFont((OpCreateFont)op); break; }
                 case SetFont: { doSetFont((OpSetFont)op); break; }
                 case DrawChars: { doDrawChars((OpDrawChars)op); break; }
+                case SetTextColor: { doSetTextColor((OpSetTextColor)op); break; }
+                case CreatePen: { doCreatePen((OpCreatePen)op); break; }
+                case SetPen: { doSetPen((OpSetPen)op); break; }
             }
         }
         gc.stroke();
+    }
+
+    private void doSetPen(OpSetPen op) {
+        String name = op.getName();
+        StrokeEnv env = penMap.get(name);
+        if( env == null ){
+            logger.error("Cannot find pen ({}).", name);
+        } else {
+            enterStrokeMode(StrokeMode.LINE);
+            restoreStrokeEnv(env);
+        }
+    }
+
+    private StrokeEnv getCurrentStrokeEnv(){
+        StrokeEnv currentEnv = new StrokeEnv();
+        currentEnv.paint = gc.getStroke();
+        currentEnv.width = gc.getLineWidth();
+        return currentEnv;
+    }
+
+    private void restoreStrokeEnv(StrokeEnv env){
+        if( env != null ) {
+            gc.setStroke(env.paint);
+            gc.setLineWidth(env.width);
+        }
+    }
+
+    private void enterStrokeMode(StrokeMode mode){
+        if( strokeMode != mode ){
+            if( mode == StrokeMode.TEXT ){
+                savedLineStrokeEnv = getCurrentStrokeEnv();
+                restoreStrokeEnv(savedTextStrokeEnv);
+            } else {
+                savedTextStrokeEnv = getCurrentStrokeEnv();
+                restoreStrokeEnv(savedLineStrokeEnv);
+            }
+        }
+    }
+
+    private void doCreatePen(OpCreatePen op) {
+        String name = op.getName();
+        int r = op.getR();
+        int g = op.getG();
+        int b = op.getB();
+        double width = scale(op.getWidth());
+        StrokeEnv env = new StrokeEnv();
+        env.paint = Color.color(r, g, b);
+        env.width = width;
+        penMap.put(name, env);
+    }
+
+    private void doSetTextColor(OpSetTextColor op) {
+        int r = op.getR();
+        int g = op.getG();
+        int b = op.getB();
+        Color color = Color.color(r, g, b);
+        enterStrokeMode(StrokeMode.TEXT);
+        gc.setStroke(color);
     }
 
     private double scale(double mm){
@@ -77,6 +154,7 @@ public class PreviewCanvas extends Canvas {
         int n = chars.length();
         PosList xPosList = new PosList(xs);
         PosList yPosList = new PosList(ys);
+        enterStrokeMode(StrokeMode.TEXT);
         for(int i=0;i<n;i++){
             String s = chars.substring(i, i+1);
             double x = scale(xPosList.get(i));
@@ -89,14 +167,13 @@ public class PreviewCanvas extends Canvas {
         double x = scale(op.getX());
         double y = scale(op.getY());
         gc.moveTo(x, y);
-        System.out.printf("moveto %.1f %.1f\n", x, y);
     }
 
     private void doLineTo(OpLineTo op){
         double x = scale(op.getX());
         double y = scale(op.getY());
+        enterStrokeMode(StrokeMode.LINE);
         gc.lineTo(x, y);
-        System.out.printf("lineto %.1f %.1f\n", x, y);
     }
 
     private void doCreateFont(OpCreateFont op){
