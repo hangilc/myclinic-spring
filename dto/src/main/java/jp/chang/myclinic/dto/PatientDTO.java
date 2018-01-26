8f2d1ad11979947a80184c2f80c23c20357b2bb2
Hangil Chang <hangil@chang.jp>
2018-01-26 13:35:15 +0900

    temporary saving

--- dto/src/main/java/jp/chang/myclinic/dto/PatientDTO.java
+++ dto/src/main/java/jp/chang/myclinic/dto/PatientDTO.java
@@ -1,5 +1,7 @@
 package jp.chang.myclinic.dto;
 
+import java.util.Objects;
+
 public class PatientDTO {
 	public int patientId;
 	public String lastName;
@@ -24,5 +26,27 @@ public class PatientDTO {
 			"address=" + address + ", " +
 			"phone=" + phone + 
 			"]";
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		PatientDTO that = (PatientDTO) o;
+		return patientId == that.patientId &&
+				Objects.equals(lastName, that.lastName) &&
+				Objects.equals(firstName, that.firstName) &&
+				Objects.equals(lastNameYomi, that.lastNameYomi) &&
+				Objects.equals(firstNameYomi, that.firstNameYomi) &&
+				Objects.equals(birthday, that.birthday) &&
+				Objects.equals(sex, that.sex) &&
+				Objects.equals(address, that.address) &&
+				Objects.equals(phone, that.phone);
+	}
+
+	@Override
+	public int hashCode() {
+
+		return Objects.hash(patientId, lastName, firstName, lastNameYomi, firstNameYomi, birthday, sex, address, phone);
 	}
 }
--- dto/src/main/java/jp/chang/myclinic/dto/VisitDTO.java
+++ dto/src/main/java/jp/chang/myclinic/dto/VisitDTO.java
@@ -1,5 +1,7 @@
 package jp.chang.myclinic.dto;
 
+import java.util.Objects;
+
 public class VisitDTO {
 	public int visitId;
 	public int patientId;
@@ -10,4 +12,41 @@ public class VisitDTO {
 	public int kouhi1Id;
 	public int kouhi2Id;
 	public int kouhi3Id;
+
+	@Override
+	public String toString() {
+		return "VisitDTO{" +
+				"visitId=" + visitId +
+				", patientId=" + patientId +
+				", visitedAt='" + visitedAt + '\'' +
+				", shahokokuhoId=" + shahokokuhoId +
+				", koukikoureiId=" + koukikoureiId +
+				", roujinId=" + roujinId +
+				", kouhi1Id=" + kouhi1Id +
+				", kouhi2Id=" + kouhi2Id +
+				", kouhi3Id=" + kouhi3Id +
+				'}';
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		VisitDTO visitDTO = (VisitDTO) o;
+		return visitId == visitDTO.visitId &&
+				patientId == visitDTO.patientId &&
+				shahokokuhoId == visitDTO.shahokokuhoId &&
+				koukikoureiId == visitDTO.koukikoureiId &&
+				roujinId == visitDTO.roujinId &&
+				kouhi1Id == visitDTO.kouhi1Id &&
+				kouhi2Id == visitDTO.kouhi2Id &&
+				kouhi3Id == visitDTO.kouhi3Id &&
+				Objects.equals(visitedAt, visitDTO.visitedAt);
+	}
+
+	@Override
+	public int hashCode() {
+
+		return Objects.hash(visitId, patientId, visitedAt, shahokokuhoId, koukikoureiId, roujinId, kouhi1Id, kouhi2Id, kouhi3Id);
+	}
 }
