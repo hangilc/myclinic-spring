3f661bff60b3a5f9b8f16af4c78e7dc2265a26c5
Hangil Chang <hangil@chang.jp>
2018-01-26 13:36:25 +0900

    temporary saving

--- dto/src/main/java/jp/chang/myclinic/dto/WqueueDTO.java
+++ dto/src/main/java/jp/chang/myclinic/dto/WqueueDTO.java
@@ -1,6 +1,31 @@
 package jp.chang.myclinic.dto;
 
+import java.util.Objects;
+
 public class WqueueDTO {
 	public int visitId;
 	public int waitState;
+
+	@Override
+	public String toString() {
+		return "WqueueDTO{" +
+				"visitId=" + visitId +
+				", waitState=" + waitState +
+				'}';
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		WqueueDTO wqueueDTO = (WqueueDTO) o;
+		return visitId == wqueueDTO.visitId &&
+				waitState == wqueueDTO.waitState;
+	}
+
+	@Override
+	public int hashCode() {
+
+		return Objects.hash(visitId, waitState);
+	}
 }
