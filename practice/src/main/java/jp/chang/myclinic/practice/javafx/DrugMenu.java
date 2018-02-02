c20e8debf0f7bc536141ed24fddd16a5e36239ea
Hangil Chang <hangil@chang.jp>
2018-02-02 18:37:46 +0900

    adding drug menu

---
+++ practice/src/main/java/jp/chang/myclinic/practice/javafx/DrugMenu.java
@@ -0,0 +1,47 @@
+package jp.chang.myclinic.practice.javafx;
+
+import javafx.scene.Node;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+
+public class DrugMenu extends VBox {
+
+    private StackPane workarea = new StackPane();
+
+    public DrugMenu(){
+        super(4);
+        workarea.setVisible(false);
+        workarea.setManaged(false);
+        getChildren().addAll(
+                createMenu(),
+                workarea
+        );
+    }
+
+    private Node createMenu(){
+        HBox hbox = new HBox(4);
+        Hyperlink mainMenu = new Hyperlink("[処方]");
+        Hyperlink auxMenu = new Hyperlink("[+]");
+        mainMenu.setOnAction(event -> {
+            showWorkarea(new Label("workarea"));
+        });
+        hbox.getChildren().addAll(mainMenu, auxMenu);
+        return hbox;
+    }
+
+    private void showWorkarea(Node content){
+        workarea.getChildren().add(content);
+        workarea.setManaged(true);
+        workarea.setVisible(true);
+    }
+
+    private void hideWorkarea(){
+        workarea.getChildren().clear();
+        workarea.setVisible(false);
+        workarea.setManaged(false);
+    }
+
+}
--- practice/src/main/java/jp/chang/myclinic/practice/javafx/RecordDrugsPane.java
+++ practice/src/main/java/jp/chang/myclinic/practice/javafx/RecordDrugsPane.java
@@ -1,5 +1,6 @@
 package jp.chang.myclinic.practice.javafx;
 
+import javafx.geometry.Pos;
 import javafx.scene.layout.VBox;
 import jp.chang.myclinic.dto.DrugFullDTO;
 
@@ -10,6 +11,9 @@ public class RecordDrugsPane extends VBox {
     private int index = 1;
 
     public RecordDrugsPane(List<DrugFullDTO> drugs){
+        setAlignment(Pos.TOP_LEFT);
+        DrugMenu menu = new DrugMenu();
+        getChildren().add(menu);
         drugs.forEach(this::addDrug);
     }
 
