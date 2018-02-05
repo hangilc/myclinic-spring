07de369a82c742cc9f39d45abeedfbd45d9d869c
hangilc <hangil@chang.jp>
2018-02-06 00:04:35 +0900

    added aux menu

--- practice/src/main/java/jp/chang/myclinic/practice/javafx/drug/DrugMenu.java
+++ practice/src/main/java/jp/chang/myclinic/practice/javafx/drug/DrugMenu.java
@@ -1,11 +1,12 @@
 package jp.chang.myclinic.practice.javafx.drug;
 
 import javafx.scene.Node;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import jp.chang.myclinic.practice.javafx.events.EventTypes;
 
 public class DrugMenu extends VBox {
 
@@ -30,18 +31,61 @@ public class DrugMenu extends VBox {
     private Node createMenu(){
         HBox hbox = new HBox(4);
         Hyperlink mainMenu = new Hyperlink("[処方]");
-        Hyperlink auxMenu = new Hyperlink("[+]");
+        Hyperlink auxMenuLink = new Hyperlink("[+]");
         mainMenu.setOnAction(event -> {
-            DrugForm form = new DrugEnterForm(patientId, visitId, at){
-                @Override
-                protected void onClose(DrugForm form) {
-                    hideWorkarea();
-                }
-            };
-            showWorkarea(form);
+            if( isWorkareaEmpty() ) {
+                DrugForm form = new DrugEnterForm(patientId, visitId, at) {
+                    @Override
+                    protected void onClose(DrugForm form) {
+                        hideWorkarea();
+                    }
+                };
+                showWorkarea(form);
+            }
         });
-        hbox.getChildren().addAll(mainMenu, auxMenu);
+        auxMenuLink.setOnMouseClicked(event -> {
+            if( isWorkareaEmpty() ) {
+                ContextMenu contextMenu = createAuxMenu();
+                contextMenu.show(auxMenuLink, event.getScreenX(), event.getScreenY());
+            }
+        });
+        hbox.getChildren().addAll(mainMenu, auxMenuLink);
         return hbox;
+    }
+
+    private ContextMenu createAuxMenu(){
+        ContextMenu menu = new ContextMenu();
+        menu.getItems().addAll(
+            createCopyAllMenuItem(),
+                createCopySelectedMenuItem(),
+                createModifyDaysMenuItem(),
+                createDeleteSelectedMenuItem()
+        );
+        return menu;
+    }
+
+    private MenuItem createCopyAllMenuItem(){
+        MenuItem item = new MenuItem("全部コピー");
+        return item;
+    }
+
+    private MenuItem createCopySelectedMenuItem(){
+        MenuItem item = new MenuItem("部分コピー");
+        return item;
+    }
+
+    private MenuItem createModifyDaysMenuItem(){
+        MenuItem item = new MenuItem("日数変更");
+        return item;
+    }
+
+    private MenuItem createDeleteSelectedMenuItem(){
+        MenuItem item = new MenuItem("複数削除");
+        return item;
+    }
+
+    private boolean isWorkareaEmpty(){
+        return workarea.getChildren().size() == 0;
     }
 
     private void showWorkarea(Node content){
