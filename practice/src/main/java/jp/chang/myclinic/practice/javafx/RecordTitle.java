0e59f3924f057e8ed2506cb31e21f90d6584a6f0
Hangil Chang <hangil@chang.jp>
2018-02-06 18:59:18 +0900

    temporary saving

--- practice/src/main/java/jp/chang/myclinic/practice/javafx/RecordTitle.java
+++ practice/src/main/java/jp/chang/myclinic/practice/javafx/RecordTitle.java
@@ -1,31 +1,36 @@
 package jp.chang.myclinic.practice.javafx;
 
 import javafx.scene.Node;
-import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 import jp.chang.myclinic.dto.VisitDTO;
 import jp.chang.myclinic.practice.PracticeEnv;
 import jp.chang.myclinic.util.DateTimeUtil;
 
-public class RecordTitle extends VBox {
+public class RecordTitle extends TextFlow {
 
-    public RecordTitle(VisitDTO visit){
-        getChildren().addAll(createTitle(visit));
+    private int visitId;
+
+    public RecordTitle(VisitDTO visit) {
+        this.visitId = visit.visitId;
+        getStyleClass().add("record-title-text");
+        getChildren().addAll(new Text(createText(visit.visitedAt));
+        adaptToEnv();
+        PracticeEnv.INSTANCE.currentVisitIdProperty().addListener((obs, oldValue, newValue) -> adaptToEnv());
+        PracticeEnv.INSTANCE.tempVisitIdProperty().addListener((obs, oldValue, newValue) -> adaptToEnv());
     }
 
-    private Node createTitle(VisitDTO visit){
-        TextFlow textFlow = new TextFlow();
-        String text = DateTimeUtil.sqlDateTimeToKanji(visit.visitedAt,
-                DateTimeUtil.kanjiFormatter3, DateTimeUtil.kanjiFormatter4);
-        Text title = new Text(text);
-        textFlow.getChildren().add(title);
-        textFlow.getStyleClass().add("record-title-text");
-        if( PracticeEnv.INSTANCE.getCurrentVisitId() == visit.visitId ){
-            textFlow.getStyleClass().add("current-visit");
-        } else if( PracticeEnv.INSTANCE.getTempVisitId() == visit.visitId ){
-            textFlow.getStyleClass().add("temp-visit");
+    private void adaptToEnv(){
+        getStyleClass().removeAll("current-visit", "temp-visit");
+        if (PracticeEnv.INSTANCE.getCurrentVisitId() == visitId) {
+            getStyleClass().add("current-visit");
+        } else if (PracticeEnv.INSTANCE.getTempVisitId() == visitId) {
+            getStyleClass().add("temp-visit");
         }
-        return textFlow;
+    }
+
+    private String createText(String at) {
+        return DateTimeUtil.sqlDateTimeToKanji(at,
+                DateTimeUtil.kanjiFormatter3, DateTimeUtil.kanjiFormatter4);
     }
 }
