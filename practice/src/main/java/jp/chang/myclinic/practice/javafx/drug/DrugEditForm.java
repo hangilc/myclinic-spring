290a4fe7b378571ee464af6ade37e5369481c52c
hangilc <hangil@chang.jp>
2018-02-11 15:55:47 +0900

    added fix constraints to edit drug

--- practice/src/main/java/jp/chang/myclinic/practice/javafx/drug/DrugEditForm.java
+++ practice/src/main/java/jp/chang/myclinic/practice/javafx/drug/DrugEditForm.java
@@ -4,14 +4,31 @@ import javafx.scene.control.CheckBox;
 import jp.chang.myclinic.dto.DrugFullDTO;
 import jp.chang.myclinic.dto.VisitDTO;
 import jp.chang.myclinic.practice.lib.drug.DrugFormHelper;
+import jp.chang.myclinic.practice.lib.drug.DrugInputConstraints;
 
 public class DrugEditForm extends DrugForm {
 
     private CheckBox allFixedCheck = new CheckBox("用量・用法・日数をそのままに");
 
-    public DrugEditForm(VisitDTO visit, DrugFullDTO drugFull) {
+    protected DrugEditForm(VisitDTO visit, DrugFullDTO drugFull) {
         super(visit);
         DrugFormHelper.setDrug(getDrugFormSetter(), drugFull, getDrugFormGetter(), getConstraints());
+        setConstraints(new DrugInputConstraints() {
+            @Override
+            public boolean isAmountFixed() {
+                return allFixedCheck.isSelected();
+            }
+
+            @Override
+            public boolean isUsageFixed() {
+                return allFixedCheck.isSelected();
+            }
+
+            @Override
+            public boolean isDaysFixed() {
+                return allFixedCheck.isSelected();
+            }
+        });
         addDrugInputRow(allFixedCheck);
     }
 
