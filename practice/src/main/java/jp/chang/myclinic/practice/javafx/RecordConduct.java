f1adc172b9cd783e863d10360a0d9891a8c111b8
hangilc <hangil@chang.jp>
2018-02-14 18:39:10 +0900

    temporary saving

--- practice/src/main/java/jp/chang/myclinic/practice/javafx/RecordConduct.java
+++ practice/src/main/java/jp/chang/myclinic/practice/javafx/RecordConduct.java
@@ -1,40 +1,42 @@
 package jp.chang.myclinic.practice.javafx;
 
+import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import jp.chang.myclinic.consts.ConductKind;
 import jp.chang.myclinic.dto.*;
 
-class RecordConduct extends VBox {
+class RecordConduct extends StackPane {
 
     RecordConduct(ConductFullDTO conduct){
+        getChildren().add(createDisp(conduct));
+    }
+
+    private Node createDisp(ConductFullDTO conduct){
+        VBox disp = new VBox();
         String kindText = ConductKind.codeToKanjiRep(conduct.conduct.kind);
-        getChildren().add(new Label("<" + kindText + ">"));
+        disp.getChildren().add(new Label("<" + kindText + ">"));
         GazouLabelDTO gazouLabel = conduct.gazouLabel;
         if( gazouLabel != null && gazouLabel.label != null && !gazouLabel.label.isEmpty() ){
-            getChildren().add(new Label(gazouLabel.label));
+            disp.getChildren().add(new Label(gazouLabel.label));
         }
         if( conduct.conductShinryouList != null ){
-            conduct.conductShinryouList.forEach(this::addShinryou);
+            conduct.conductShinryouList.forEach(shinryou -> {
+                disp.getChildren().add(new RecordConductShinryou(shinryou));
+            });
         }
         if( conduct.conductDrugs != null ){
-            conduct.conductDrugs.forEach(this::addDrug);
+            conduct.conductDrugs.forEach(drug -> {
+                disp.getChildren().add(new RecordConductDrug(drug));
+            });
         }
         if( conduct.conductKizaiList != null ){
-            conduct.conductKizaiList.forEach(this::addKizai);
+            conduct.conductKizaiList.forEach(kizai -> {
+                disp.getChildren().add(new RecordConductKizai(kizai));
+            });
         }
-    }
-
-    private void addShinryou(ConductShinryouFullDTO shinryou){
-        getChildren().add(new RecordConductShinryou(shinryou));
-    }
-
-    private void addDrug(ConductDrugFullDTO drug){
-        getChildren().add(new RecordConductDrug(drug));
-    }
-
-    private void addKizai(ConductKizaiFullDTO kizai){
-        getChildren().add(new RecordConductKizai(kizai));
+        return disp;
     }
 
 }
---
+++ practice/src/main/java/jp/chang/myclinic/practice/javafx/conduct/ConductEditForm.java
@@ -0,0 +1,16 @@
+package jp.chang.myclinic.practice.javafx.conduct;
+
+import jp.chang.myclinic.practice.javafx.parts.WorkForm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConductEditForm extends WorkForm {
+
+    private static Logger logger = LoggerFactory.getLogger(ConductEditForm.class);
+
+    public ConductEditForm() {
+        super("処置の編集");
+
+    }
+
+}
