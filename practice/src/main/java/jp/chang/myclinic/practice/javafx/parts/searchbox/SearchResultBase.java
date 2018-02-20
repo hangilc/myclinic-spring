c8fcc263c382ce6cbe473dc36d986e218bc40300
Hangil Chang <hangil@chang.jp>
2018-02-20 16:08:18 +0900

    temporary saving

---
+++ practice/src/main/java/jp/chang/myclinic/practice/javafx/parts/searchbox/SearchResultBase.java
@@ -0,0 +1,56 @@
+package jp.chang.myclinic.practice.javafx.parts.searchbox;
+
+import javafx.collections.FXCollections;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+import jp.chang.myclinic.practice.javafx.HandlerFX;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+
+public abstract class SearchResultBase<M> extends ListView<M> implements SearchResult{
+
+    private Consumer<M> onSelectCallback = m -> {};
+
+    public SearchResultBase(){
+        getStyleClass().add("search-result");
+        setCellFactory(listView -> new ListCell<M>(){
+            @Override
+            protected void updateItem(M item, boolean empty) {
+                super.updateItem(item, empty);
+                setText(empty ? "" : convert(item));
+            }
+        });
+        setOnMouseClicked(this::onMouseClick);
+    }
+
+    @Override
+    public void search(String text) {
+        doSearch(text)
+                .thenAccept(this::setList)
+                .exceptionally(HandlerFX::exceptionally);
+    }
+
+    @Override
+    public void setOnSelectCallback(Consumer cb) {
+        this.onSelectCallback = cb;
+    }
+
+    protected abstract CompletableFuture<List<M>> doSearch(String text);
+
+    protected abstract String convert(M model);
+
+    private void setList(List<M> list){
+        itemsProperty().setValue(FXCollections.observableArrayList(list));
+    }
+
+    private void onMouseClick(MouseEvent event){
+        M selected = getSelectionModel().getSelectedItem();
+        if( selected != null ){
+            onSelectCallback.accept(selected);
+        }
+    }
+
+}
