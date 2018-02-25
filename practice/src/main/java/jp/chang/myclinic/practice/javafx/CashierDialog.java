de7430c5aafab0975a2d25191f8c85d46bf90b07
hangilc <hangil@chang.jp>
2018-02-25 11:01:09 +0900

    temporary saving

--- practice/src/main/java/jp/chang/myclinic/practice/javafx/CashierDialog.java
+++ practice/src/main/java/jp/chang/myclinic/practice/javafx/CashierDialog.java
@@ -3,10 +3,12 @@ package jp.chang.myclinic.practice.javafx;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Region;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -28,17 +30,18 @@ public class CashierDialog extends Stage {
         root.getChildren().addAll(
                 createDisp(meisai),
                 createTotalTen(meisai.totalTen),
-                createCharge(meisai)
+                createCharge(meisai),
+                createCommands()
         );
         setScene(new Scene(root));
     }
 
-    private Node createDisp(MeisaiDTO meisai){
+    private Node createDisp(MeisaiDTO meisai) {
         MeisaiDisp disp = new MeisaiDisp(meisai);
         return disp;
     }
 
-    private Node createTotalTen(int totalTen){
+    private Node createTotalTen(int totalTen) {
         HBox hbox = new HBox(4);
         hbox.getStyleClass().add("total-ten");
         String text = String.format("総点 %,d 点", totalTen);
@@ -47,55 +50,57 @@ public class CashierDialog extends Stage {
         return hbox;
     }
 
-    private Node createCharge(MeisaiDTO meisai){
+    private Node createCharge(MeisaiDTO meisai) {
         StackPane wrapper = new StackPane();
+        wrapper.getChildren().add(createChargeDisp(wrapper, meisai.charge, meisai));
+        return wrapper;
+    }
+
+    private Node createChargeDisp(Pane wrapper, int currentCharge, MeisaiDTO meisai) {
         HBox hbox = new HBox(4);
         hbox.setAlignment(Pos.CENTER_LEFT);
-        Label label = new Label(String.format("請求額 %,d 円 (負担割 %d 割)", meisai.charge, meisai.futanWari));
+        Label label = new Label(String.format("請求額 %,d 円 (負担割 %d 割)", currentCharge, meisai.futanWari));
         Hyperlink modifyLink = new Hyperlink("変更");
-        modifyLink.setOnAction(evt -> {
-            Node form = createEditForm(meisai);
-
-            if( workarea.isVisible() ){
-                workarea.hide();
-            } else {
-                workarea.show(form);
-            }
-        });
         hbox.getChildren().addAll(
                 label,
                 modifyLink
         );
-        wrapper.getChildren().add(hbox);
-        return wrapper;
+        modifyLink.setOnAction(evt -> {
+            Node form = createEditForm(
+                    () -> {
+                    },
+                    () -> {
+                        wrapper.getChildren().setAll(hbox);
+                    }
+            );
+            wrapper.getChildren().setAll(form);
+        });
+        return hbox;
     }
 
-    private Node createChargeDisp(Region wrapper, int charge, int futanwari){
+    private Node createEditForm(Runnable applyCallback, Runnable cancelCallback) {
         HBox hbox = new HBox(4);
         hbox.setAlignment(Pos.CENTER_LEFT);
-        Label label = new Label(String.format("請求額 %,d 円 (負担割 %d 割)", charge, futanwari));
-        Hyperlink modifyLink = new Hyperlink("変更");
-        modifyLink.setOnAction(evt -> {
-            Node form = createEditForm(meisai);
-
-            if( workarea.isVisible() ){
-                workarea.hide();
-            } else {
-                workarea.show(form);
-            }
-        });
+        TextField textField = new TextField("");
+        textField.getStyleClass().add("charge-input-field");
+        Hyperlink enterLink = new Hyperlink("適用");
+        Hyperlink cancelLink = new Hyperlink("キャンセル");
+        cancelLink.setOnAction(evt -> cancelCallback.run());
         hbox.getChildren().addAll(
-                label,
-                modifyLink
+                new Label("変更額 "),
+                textField,
+                new Label("円"),
+                enterLink,
+                cancelLink
         );
         return hbox;
     }
 
-    private Node createEditForm(){
+    private Node createCommands(){
         HBox hbox = new HBox(4);
-        hbox.getChildren().addAll(
-                new Label("変更額 ")
-        );
+        Button enterButton = new Button("入力");
+        Button cancelButton = new Button("キャンセル");
+        hbox.getChildren().addAll(enterButton, cancelButton);
         return hbox;
     }
 
