b379eab223bc04f63de020c5668035158a5b7cb0
Hangil Chang <hangil@chang.jp>
2018-03-05 18:27:47 +0900

    print datasource

--- server/src/main/java/jp/chang/myclinic/server/AppServer.java
+++ server/src/main/java/jp/chang/myclinic/server/AppServer.java
@@ -4,19 +4,26 @@ import jp.chang.myclinic.drawer.JacksonOpSerializer;
 import jp.chang.myclinic.drawer.Op;
 import jp.chang.myclinic.mastermap.MasterMap;
 import jp.chang.myclinic.server.rcpt.HoukatsuKensa;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 @SpringBootApplication
-public class AppServer {
+public class AppServer implements CommandLineRunner{
+
+    @Autowired
+    private DataSource dataSource;
+
     public static void main( String[] args )
     {
         SpringApplication.run(AppServer.class, args);
@@ -49,4 +56,8 @@ public class AppServer {
         return masterMap;
     }
 
+    @Override
+    public void run(String... strings) throws Exception {
+        System.out.println("DataSource: " + dataSource);
+    }
 }
