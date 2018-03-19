850694cb787b0f62aac5f0fcf2e278b47841133f
Hangil Chang <hangil@chang.jp>
2018-03-19 15:34:28 +0900

    added context menu to patient info

--- practice/src/main/java/jp/chang/myclinic/practice/javafx/CurrentPatientInfo.java
+++ practice/src/main/java/jp/chang/myclinic/practice/javafx/CurrentPatientInfo.java
@@ -2,7 +2,13 @@ package jp.chang.myclinic.practice.javafx;
 
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.scene.Node;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.DataFormat;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -12,6 +18,8 @@ import jp.chang.myclinic.dto.PatientDTO;
 import jp.chang.myclinic.util.DateTimeUtil;
 
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CurrentPatientInfo extends VBox {
 
@@ -28,6 +36,7 @@ public class CurrentPatientInfo extends VBox {
                 TextFlow upperRow = new TextFlow(
                         new Text(makeText()), detailLink
                 );
+                setupNameContextMenu(upperRow);
                 StackPane lowerRow = new StackPane();
                 detailLink.setOnAction(event -> {
                     if( lowerRow.getChildren().size() == 0 ){
@@ -42,6 +51,58 @@ public class CurrentPatientInfo extends VBox {
                 getChildren().add(new Text("(選択患者なし)"));
             }
         });
+    }
+
+    private void setupNameContextMenu(Node node){
+        node.setOnMouseClicked(event -> {
+            MouseButton button = event.getButton();
+            if( button == MouseButton.SECONDARY ){
+                ContextMenu contextMenu = new ContextMenu();
+                {
+                    MenuItem copyNameItem = new MenuItem("[id] 名前 (よみ) をコピー");
+                    copyNameItem.setOnAction(evt -> {
+                        PatientDTO patientDTO = patient.getValue();
+                        if( patientDTO != null ) {
+                            String text = String.format("[%d] %s %s (%s %s)",
+                                    patientDTO.patientId,
+                                    patientDTO.lastName,
+                                    patientDTO.firstName,
+                                    patientDTO.lastNameYomi,
+                                    patientDTO.firstNameYomi);
+                            boolean ok = copyToClipboard(text);
+                            if( !ok ){
+                                GuiUtil.alertError("クリップボードへのコピーに失敗しました。");
+                            }
+                        }
+                    });
+                    contextMenu.getItems().add(copyNameItem);
+                }
+                {
+                    MenuItem copyNameItem = new MenuItem("名前をコピー");
+                    copyNameItem.setOnAction(evt -> {
+                        PatientDTO patientDTO = patient.getValue();
+                        if( patientDTO != null ) {
+                            String text = String.format("%s%s",
+                                    patientDTO.lastName,
+                                    patientDTO.firstName);
+                            boolean ok = copyToClipboard(text);
+                            if( !ok ){
+                                GuiUtil.alertError("クリップボードへのコピーに失敗しました。");
+                            }
+                        }
+                    });
+                    contextMenu.getItems().add(copyNameItem);
+                }
+                contextMenu.show(node, event.getScreenX(), event.getScreenY());
+            }
+        });
+    }
+
+    private boolean copyToClipboard(String text){
+        Map<DataFormat,Object> content = new HashMap<>();
+        content.put(DataFormat.PLAIN_TEXT, text);
+        Clipboard clipboard = Clipboard.getSystemClipboard();
+        return clipboard.setContent(content);
     }
 
     public PatientDTO getPatient() {
