4e9b7c8f06d940dec7a08b17d67b4037d855ca1c
Hangil Chang <hangil@chang.jp>
2018-03-23 13:34:24 +0900

    added shinryou/kizai csv row

--- master-app/src/main/java/jp/chang/myclinic/master/add/IyakuhinMasterCSV.java
+++ master-app/src/main/java/jp/chang/myclinic/master/add/IyakuhinMasterCSV.java
@@ -31,34 +31,3 @@ public class IyakuhinMasterCSV {
 
 }
 
-    /*
-	    henkou_kubun = get_csv_value(sa, 1);
-	    if( *henkou_kubun == '1'  // 抹消  
-                ||
-		    *henkou_kubun == '9'  // 廃止 
-           )
-		    return 1;
-	    master_shubetsu = get_csv_value(sa, 2);
-	    if( *master_shubetsu != 'Y' ){
-		    MessageBox(NULL, "医薬品マスターのデータではありません", "エラー", MB_ICONERROR);
-		    return 0;
-	    }
-	    iyakuhincode = get_csv_value(sa, 3);
-	    name = get_csv_value(sa, 5);
-	    yomi = get_csv_value(sa, 7);
-	    unit = get_csv_value(sa, 10);
-	    kingaku_shubetsu = get_csv_value(sa, 11);
-	    if( *kingaku_shubetsu != '1' )
-		    return 1;
-	    yakka = get_csv_value(sa, 12);
-	    madoku = get_csv_value(sa, 14);
-		    // madoku: 麻薬・毒薬など
-		    //   0:右記以外、1:麻薬、2:毒薬、3:覚せい剤原料、5:向精神薬
-	    kouhatsu = get_csv_value(sa, 17);
-		    // kouhatsu: 後発品
-		    //   0:右記以外、1:後発品
-	    zaikei = get_csv_value(sa, 28);
-		    // zaikei: 剤型
-		    //   1:内服薬、3:その他、4:注射薬, 6:外用薬、8:歯科用薬剤、9:歯科特定薬剤
-	    yakkacode = get_csv_value(sa, 32);
-    */
---
+++ master-app/src/main/java/jp/chang/myclinic/master/add/ShinryouMasterCSV.java
@@ -0,0 +1,49 @@
+package jp.chang.myclinic.master.add;
+
+public class ShinryouMasterCSV {
+    public String kubun;
+    public String masterShubetsu;
+    public String nyuuGaiTekiyou;
+    public String byouShinKubun;
+    public String tensuuShikibetsu;
+    public int shinryoucode;
+    public String name;
+    public String tensuu;
+    public String shuukeisaki;
+    public String houkatsukensa;
+    public String oushinKubun;
+    public String kensaGroup;
+    public String roujinTekiyou;
+    public String codeShou;
+    public String codeBu;
+    public String codeAlpha;
+    public String codeKubun;
+
+    public ShinryouMasterCSV(CSVRow row) {
+        kubun = row.getString(1);
+        masterShubetsu = row.getString(2);
+        nyuuGaiTekiyou = row.getString(13);
+        byouShinKubun = row.getString(19);
+        tensuuShikibetsu = row.getString(11);
+        shinryoucode = row.getInt(3);
+        name = row.getString(5);
+        tensuu = row.getString(12);
+        shuukeisaki = row.getString(15);
+        houkatsukensa = twoChars(row.getString(16));
+        oushinKubun = row.getString(17);
+        kensaGroup = twoChars(row.getString(51));
+        roujinTekiyou = row.getString(14);
+        codeShou = row.getString(90);
+        codeBu = row.getString(91);
+        codeAlpha = row.getString(85);
+        codeKubun = row.getString(92);
+    }
+
+    private String twoChars(String s){
+        if( s.length() == 1 ){
+            return "0" + s;
+        } else {
+            return s;
+        }
+    }
+}
