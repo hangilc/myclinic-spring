3faa6b2a32e9801837dd9d9fb809b69cfdb8176d
hangilc <hangil@chang.jp>
2018-03-26 00:07:07 +0900

    adding master-update

--- master-manip/update-master.ps1
+++ master-manip/update-master.ps1
@@ -2,7 +2,8 @@ param(
     [parameter(mandatory)]$IyakuhinZipfile,
     [parameter(mandatory)]$ShinryouZipfile,
     [parameter(mandatory)]$KizaiZipfile,
-    [parameter(mandatory)]$ValidFrom
+    [parameter(mandatory)]$ValidFrom,
+    [switch]$SkipDump = $false  # skip backing up master tables
 )
 
 $prevDate = (get-date $ValidFrom).AddDays(-1).toString('yyyy-MM-dd')
@@ -23,9 +24,18 @@ $dbPass = ./read-pass "mysql pass: "
 # backup master tables
 # to restore backup
 # get-content work/masters-save.sql | mysql -u hangil -p myclinic
-mysqldump --default-character-set=cp932 -u "$dbUser" -p"$dbPass" myclinic `
-    iyakuhin_master_arch shinryoukoui_master_arch `
-    tokuteikizai_master_arch | set-content work/masters-save.sql
+if( -Not $SkipDump ){
+    mysqldump --default-character-set=cp932 -u "$dbUser" -p"$dbPass" myclinic `
+        iyakuhin_master_arch shinryoukoui_master_arch `
+        tokuteikizai_master_arch | set-content work/masters-save.sql
+}
 
+# set valid_upto of current masters
+echo "update iyakuhin_master_arch set valid_upto = '$prevDate' where valid_upto = '0000-00-00';" |
+        mysql -u "$dbUser" -p"$dbPass" myclinic
+echo "update shinryoukoui_master_arch set valid_upto = '$prevDate' where valid_upto = '0000-00-00';" |
+        mysql -u "$dbUser" -p"$dbPass" myclinic
+echo "update tokuteikizai_master_arch set valid_upto = '$prevDate' where valid_upto = '0000-00-00';" |
+        mysql -u "$dbUser" -p"$dbPass" myclinic
 
 
