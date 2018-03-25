56e0a9f15e9a3e014b86ea7ffd4419268de186a8
hangilc <hangil@chang.jp>
2018-03-25 22:16:02 +0900

    adding master-update

--- master-manip/update-master.ps1
+++ master-manip/update-master.ps1
@@ -5,10 +5,13 @@ param(
     [parameter(mandatory)]$ValidFrom
 )
 
+$prevDate = (get-date $ValidFrom).AddDays(-1).toString('yyyy-MM-dd')
+
 echo "IyakuhinZipfile: $IyakuhinZipfile"
 echo "ShinryouZipfile: $ShinryouZipfile"
 echo "KizaiZipfile: $KizaiZipfile"
 echo "ValidFrom: $ValidFrom"
+echo "previous day of ValidFrom: $prevDate"
 
 # ensure working directory
 mkdir work -Force | out-null
@@ -17,11 +20,12 @@ mkdir work -Force | out-null
 $dbUser = read-host -prompt "mysql user: "
 $dbPass = ./read-pass "mysql pass: "
 
-echo $dbPass.Length
-
 # backup master tables
-mysqldump -u "$dbUser" -p"$dbPass" --default-character-set=cp932 myclinic `
+# to restore backup
+# get-content work/masters-save.sql | mysql -u hangil -p myclinic
+mysqldump --default-character-set=cp932 -u "$dbUser" -p"$dbPass" myclinic `
     iyakuhin_master_arch shinryoukoui_master_arch `
-    tokuteikizai_master_arch > work/masters-save.sql
+    tokuteikizai_master_arch | set-content work/masters-save.sql
+
 
 
