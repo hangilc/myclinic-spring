25d40bea0a7e0465a1f7bfddab37a4d623c7be20
Hangil Chang <hangil@chang.jp>
2018-04-02 15:49:38 +0900

    added deploy-current

---
+++ scripts/deploy-current.ps1
@@ -0,0 +1,18 @@
+$dstDir = "C:\Users\hangil\myclinic-current"
+
+if( test-path "$dstDir" ){
+    $dateTag = get-date -format "yyyyMMddHHmm"
+    ren $dstDir "$dstDir-$dateTag"
+}
+mkdir $dstDir
+
+function copyModule($module){
+    $src = "./$module/target/${module}*.jar"
+    $dst = "$dstDir/$module/target"
+    mkdir "$dst"
+    copy $src $dst
+}
+
+$modules = "server", "hotline", "reception", "practice", "intraclinic"
+$modules | %{ copyModule $_ }
+copy-item "./config" -destination "$dstDir/config" -recurse -exclude "*.git"
