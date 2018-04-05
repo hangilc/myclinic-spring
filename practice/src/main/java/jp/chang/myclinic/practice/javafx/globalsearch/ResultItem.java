9c7c2b6a0f11100f218ff6aceaa0a95fe35c25af
Hangil Chang <hangil@chang.jp>
2018-04-06 08:42:53 +0900

    added result-item

--- practice/src/main/java/jp/chang/myclinic/practice/javafx/globalsearch/ResultItem.java
+++ practice/src/main/java/jp/chang/myclinic/practice/javafx/globalsearch/ResultItem.java
@@ -3,7 +3,10 @@ package jp.chang.myclinic.practice.javafx.globalsearch;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import jp.chang.myclinic.dto.PatientDTO;
+import jp.chang.myclinic.dto.TextDTO;
 import jp.chang.myclinic.dto.TextVisitPatientDTO;
 import jp.chang.myclinic.dto.VisitDTO;
 import jp.chang.myclinic.util.DateTimeUtil;
@@ -16,23 +19,36 @@ class ResultItem extends VBox {
 
     ResultItem(TextVisitPatientDTO data) {
         super(4);
+        setFillWidth(true);
         getChildren().addAll(
                 createPatientRow(data.patient),
-                createVisitedAtRow(data.visit)
+                createVisitedAtRow(data.visit),
+                createContent(data.text)
         );
     }
 
     private Node createPatientRow(PatientDTO patient){
         String text = String.format("(%d) %s %s", patient.patientId, patient.lastName,
                 patient.firstName);
-        return new Label(text);
+        Label label = new Label(text);
+        label.setMaxWidth(Double.MAX_VALUE);
+        label.getStyleClass().add("result-item-patient");
+        return label;
     }
 
     private Node createVisitedAtRow(VisitDTO visit){
         String text = DateTimeUtil.sqlDateTimeToKanji(visit.visitedAt,
                 DateTimeUtil.kanjiFormatter3, DateTimeUtil.kanjiFormatter4);
-        return new Label(text);
+        Label label = new Label(text);
+        label.setMaxWidth(Double.MAX_VALUE);
+        label.getStyleClass().add("result-item-visited-at");
+        return label;
+    }
 
+    private Node createContent(TextDTO text){
+        TextFlow textFlow = new TextFlow(new Text(text.content));
+        textFlow.getStyleClass().add("result-item-content");
+        return textFlow;
     }
 
 }
