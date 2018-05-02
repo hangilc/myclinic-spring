566e5b99c38d64fe753654521cc08d13737a2ae3
hangilc <hangil@chang.jp>
2018-05-02 23:11:42 +0900

    added opt parse

--- rcpt/src/main/java/jp/chang/myclinic/rcpt/check/Check.java
+++ rcpt/src/main/java/jp/chang/myclinic/rcpt/check/Check.java
@@ -5,49 +5,95 @@ import jp.chang.myclinic.dto.DiseaseFullDTO;
 import jp.chang.myclinic.dto.PatientDTO;
 import jp.chang.myclinic.dto.VisitFull2DTO;
 import jp.chang.myclinic.rcpt.Masters;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Check {
 
-    private static Logger logger = LoggerFactory.getLogger(Check.class);
+    //private static Logger logger = LoggerFactory.getLogger(Check.class);
+    private String serverUrl;
     private boolean fixit = false;
     private int year;
     private int month;
+    private List<Integer> optPatientIds;
 
-    // check server-url [-f] year month
     public Check(String[] args) {
         parseArgs(args);
     }
 
+    private void usage(){
+        System.err.println("usage: rcpt check [options] serverUrl year month");
+        System.err.println("  options:");
+        System.err.println("    -f                 : fix problems");
+        System.err.println("    -p=1234,3211,...   : handle only specified patientIds");
+    }
+
     private void parseArgs(String[] args) {
-        Service.setServerUrl(args[1]);
-        //Service.setLogBody();
-        if (args.length == 5) {
-            if (args[2].equals("-f")) {
-                fixit = true;
+        if( args.length < 4 ){
+            usage();
+            System.exit(1);
+        }
+        int i = 1;
+        while( i < args.length ){
+            String s = args[i];
+            if( s.startsWith("-") ){
+                if( s.length() < 2 ){
+                    usage();
+                    System.exit(1);
+                }
+                char opt = s.charAt(1);
+                switch(opt){
+                    case 'f': fixit = true; break;
+                    case 'p': optPatientIds(s.substring(2)); break;
+                    default: usage(); System.exit(1); break;
+                }
+                i += 1;
             } else {
-                System.err.println("invalid arg : " + args[2]);
-                System.exit(1);
+                break;
             }
-            year = Integer.parseInt(args[3]);
-            month = Integer.parseInt(args[4]);
-        } else if (args.length == 4) {
-            year = Integer.parseInt(args[2]);
-            month = Integer.parseInt(args[3]);
-        } else {
-            System.err.println("usage: rcpt check [-f] year month");
+        }
+        if( args.length - i != 3 ){
+            usage();
+            System.exit(1);
+        }
+        serverUrl = args[i];
+        try {
+            year = Integer.parseInt(args[i+1]);
+            month = Integer.parseInt(args[i+2]);
+        } catch(NumberFormatException ex){
+            System.err.println("Invalid year or month.");
+            usage();
             System.exit(1);
         }
     }
 
+    private void optPatientIds(String arg){
+        this.optPatientIds = new ArrayList<>();
+        if( arg.startsWith("=") ){
+            arg = arg.substring(1);
+        }
+        String[] toks = arg.split(",");
+        for (String tok : toks) {
+            try {
+                int patientId = Integer.parseInt(tok);
+                optPatientIds.add(patientId);
+            } catch (NumberFormatException ex) {
+                System.err.println("Invalid patientId");
+                System.exit(1);
+            }
+        }
+    }
+
     public void run() throws Exception {
+        Service.setServerUrl(serverUrl);
         Masters masters = new Masters(year, month);
-        List<Integer> patientIds = Service.api.listVisitingPatientIdHavingHokenCall(year, month).execute().body();
-        //patientIds = patientIds.subList(0, 3); // for development
-        //patientIds = List.of(75);
+        List<Integer> patientIds;
+        if( optPatientIds != null ){
+            patientIds = optPatientIds;
+        } else {
+            patientIds = Service.api.listVisitingPatientIdHavingHokenCall(year, month).execute().body();
+        }
         for(int patientId: patientIds){
             PatientDTO patient = Service.api.getPatientCall(patientId).execute().body();
             System.out.printf("%04d %s%s%n", patient.patientId, patient.lastName, patient.firstName);
