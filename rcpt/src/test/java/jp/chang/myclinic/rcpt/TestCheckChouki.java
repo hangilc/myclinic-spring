9bd340678958f19015acf712d2343c1b7237353f
hangilc <hangil@chang.jp>
2018-05-05 09:14:22 +0900

    added junit

---
+++ rcpt/src/test/java/jp/chang/myclinic/rcpt/TestCheckChouki.java
@@ -0,0 +1,17 @@
+package jp.chang.myclinic.rcpt;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import static org.junit.Assert.*;
+
+public class TestCheckChouki {
+
+    private static Logger logger = LoggerFactory.getLogger(TestCheckChouki.class);
+
+    @Test
+    public void shouldDetectMissing(){
+        assertTrue("should be true", false);
+    }
+
+}
