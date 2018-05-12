3530b1f295e9c197ca72cfcd561fb2703377f042
Hangil Chang <hangil@chang.jp>
2018-05-12 19:21:20 +0900

    added crlf-to-lf.py

--- scripts/crlf-to-lf.py
+++ scripts/crlf-to-lf.py
@@ -1,7 +1,6 @@
 import sys
 
 file = sys.argv[1]
-contents = open(file, "r").read()
-print(contents)
-with open(file, "w", newline="\n") as f:
+contents = open(file, "r", encoding="utf-8").read()
+with open(file, "w", encoding="utf-8", newline="\n") as f:
 	f.write(contents)
