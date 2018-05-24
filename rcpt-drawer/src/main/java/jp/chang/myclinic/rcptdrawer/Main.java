961b6c4c2cd1dbe15f7fce545e840315210abe63
hangilc <hangil@chang.jp>
2018-05-24 20:09:39 +0900

    started rcpt^drawer

---
+++ rcpt-drawer/src/main/java/jp/chang/myclinic/rcptdrawer/Main.java
@@ -0,0 +1,25 @@
+package jp.chang.myclinic.rcptdrawer;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main extends Application {
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main( String[] args )
+    {
+        Application.launch(Main.class, args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        MainRoot root = new MainRoot();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+}
+
