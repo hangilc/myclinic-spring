c37151b8218b22d9d1ca4a7374bfe5d342f01500
hangilc <hangil@chang.jp>
2018-06-27 22:26:47 +0900

    added table-cleanup.sql

--- server/migrations/table-cleanup.sql
+++ server/migrations/table-cleanup.sql
@@ -1,1 +1,4 @@
-drop table if exists callback_page;
+drop table if exists callback_page, containers, hoken_other, iyakuhin_master, iyakuhin_master_200511,
+    iyakuhin_transition, kensa_master, master_symbol_map, no_roujin_techou, shinryou_group, shinryou_item,
+    shinryoukoui_master, shinryoukoui_master_200511,
+    shoubyoumei_master, temp_iyakuhin_master, tokuteikizai_master, tokuteikizai_master_200511;
