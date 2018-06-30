1bb1975cb95ab975dc2f69105b7ee785e6ef7fe4
Hangil Chang <hangil@chang.jp>
2018-06-30 16:35:21 +0900

    changed rcpt data output to utf-8

--- rcpt/src/main/java/jp/chang/myclinic/rcpt/data/Xml.java
+++ rcpt/src/main/java/jp/chang/myclinic/rcpt/data/Xml.java
@@ -1,48 +1,59 @@
 package jp.chang.myclinic.rcpt.data;
 
 import jp.chang.myclinic.util.NumberUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
 
 class Xml {
 
-    //private static Logger logger = LoggerFactory.getLogger(Xml.class);
+    private static Logger logger = LoggerFactory.getLogger(Xml.class);
 
     private String indentStr = "";
+    private PrintStream out;
 
     Xml() {
-
+        try {
+            this.out = new PrintStream(System.out, true, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            logger.error("Failed to create PrintStream. {}", e);
+            throw new RuntimeException(e);
+        }
     }
 
     void prelude(){
-        System.out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+        out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
     }
 
     void element(String tag, Runnable inner){
-        System.out.printf("%s<%s>\n", indentStr, tag);
+        out.printf("%s<%s>\n", indentStr, tag);
         indent();
         inner.run();
         unindent();
-        System.out.printf("%s</%s>\n", indentStr, tag);
+        out.printf("%s</%s>\n", indentStr, tag);
     }
 
     void element(String tag, int value){
-        System.out.printf("%s<%s>%d</%s>\n", indentStr, tag, value, tag);
+        out.printf("%s<%s>%d</%s>\n", indentStr, tag, value, tag);
     }
 
     void element(String tag, String value){
-        System.out.printf("%s<%s>%s</%s>\n", indentStr, tag, value, tag);
+        out.printf("%s<%s>%s</%s>\n", indentStr, tag, value, tag);
     }
 
     void element(String tag, char value){
-        System.out.printf("%s<%s>%c</%s>\n", indentStr, tag, value, tag);
+        out.printf("%s<%s>%c</%s>\n", indentStr, tag, value, tag);
     }
 
     void element(String tag, double value){
         String rep = NumberUtil.formatNumber(value);
-        System.out.printf("%s<%s>%s</%s>\n", indentStr, tag, rep, tag);
+        out.printf("%s<%s>%s</%s>\n", indentStr, tag, rep, tag);
     }
 
     void element(String tag, String fmt, double value){
-        System.out.printf("%s<%s>" + fmt + "</%s>\n", indentStr, tag, value, tag);
+        out.printf("%s<%s>" + fmt + "</%s>\n", indentStr, tag, value, tag);
     }
 
     void indent(){
