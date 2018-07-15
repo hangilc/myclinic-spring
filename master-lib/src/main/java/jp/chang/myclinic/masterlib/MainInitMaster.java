45c687695ad99cedc21b1cd2577f149253c96df7
hangilc <hangil@chang.jp>
2018-07-16 01:07:55 +0900

    added init shinryou master

--- master-lib/src/main/java/jp/chang/myclinic/masterlib/MainInitMaster.java
+++ master-lib/src/main/java/jp/chang/myclinic/masterlib/MainInitMaster.java
@@ -10,26 +10,84 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.function.Consumer;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 public class MainInitMaster {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(getConnectionString());
+        Statement stmt = conn.createStatement();
         Path saveDir = Paths.get("./master-files");
         Path shinryouZip = saveDir.resolve(MasterDownloader.DEFAULT_SHINRYOU_FILENAME);
+        String validFrom = LocalDate.now().toString();
         int[] counts = new int[]{0};
         parseZipFile(shinryouZip.toFile(), "s.csv", record -> {
             CSVRow csvRow = new CommonsCSVRow(record);
             ShinryouMasterCSV shinryouCSV = new ShinryouMasterCSV(csvRow);
             int kubun = shinryouCSV.kubun;
             if (kubun == 0 || kubun == 3 || kubun == 5) {
-                
+                String sql = shinryouSql(shinryouCSV, validFrom);
+                try {
+                    stmt.executeUpdate(sql);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    System.exit(1);
+                }
                 counts[0] += 1;
             }
         });
+        stmt.close();
+        conn.close();
         System.out.println(counts[0]);
+    }
+
+    private static String getConnectionString(){
+        String host = System.getenv("MYCLINIC_DB_HOST");
+        String user = System.getenv("MYCLINIC_DB_USER");
+        String pass = System.getenv("MYCLINIC_DB_PASS");
+        return String.format("jdbc:mysql://%s:3306/myclinic?user=%s&password=%s&zeroDateTimeBehavior=convertToNull&noDatetimeStringSync=true&useUnicode=true&characterEncoding=utf8&verifyServerCertificate=false&useSSL=true",
+                host, user, pass);
+    }
+
+    private static String shinryouTemplate;
+
+    static {
+        shinryouTemplate = String.join(" ",
+                "insert into shinryoukoui_master_arch set",
+                "shinryoucode=%s,",
+                "name='%s',",
+                "tensuu='%s',",
+                "tensuu_shikibetsu='%s',",
+                "shuukeisaki='%s',",
+                "houkatsukensa='%s',",
+                "oushinkubun='%s',",
+                "kensagroup='%s',",
+                "valid_from='%s',",
+                "valid_upto='%s';"
+        );
+    }
+
+    private static String shinryouSql(ShinryouMasterCSV master, String validFrom) {
+        return String.format(shinryouTemplate,
+                master.shinryoucode,
+                master.name,
+                master.tensuu,
+                master.tensuuShikibetsu,
+                master.shuukeisaki,
+                master.houkatsukensa,
+                master.oushinKubun,
+                master.kensaGroup,
+                validFrom,
+                "0000-00-00"
+        );
     }
 
     private static void parseZipFile(File zipFile, String file, Consumer<CSVRecord> consumer) throws IOException {
