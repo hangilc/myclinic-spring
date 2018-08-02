3441c582b73a4449eb748b8f51433b7739c4baa6
Hangil Chang <hangil@chang.jp>
2018-08-02 13:46:46 +0900

    added CreateShortcut

--- winutil/src/main/java/jp/chang/myclinic/winutil/main/CreateShortcut.java
+++ winutil/src/main/java/jp/chang/myclinic/winutil/main/CreateShortcut.java
@@ -4,13 +4,20 @@ import jp.chang.myclinic.winutil.ShellLink;
 
 public class CreateShortcut {
 
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
+        if (args.length != 4) {
+            System.err.println("Usage: create-shortcut TARGET ARGS WORKING-DIR SAVE-PAT");
+            System.exit(1);
+        }
+        String target = args[0];
+        String arguments = args[1];
+        String workDir = args[2];
+        String savePath = args[3];
         ShellLink shellLink = new ShellLink();
-        shellLink.setPath("C:/Program Files/erl8.3/bin/werl.exe");
-        System.out.println(shellLink.getPath());
-        shellLink.setWorkingDirectory("C:/Users/hangil/work/learn-java/myclinic-spring");
-        shellLink.save("C:/Users/hangil/shortcut.lnk");
+        shellLink.setPath(target);
+        shellLink.setArguments(arguments);
+        shellLink.setWorkingDirectory(workDir);
+        shellLink.save(savePath);
         shellLink.close();
     }
 
