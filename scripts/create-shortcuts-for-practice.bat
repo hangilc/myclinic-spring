eb0b8ae9c75b5b037c74e3174162ef0f8dee5a53
hangilc <hangil@chang.jp>
2018-08-03 23:58:21 +0900

    revised create-shortcut

--- scripts/create-shortcuts-for-practice.bat
+++ scripts/create-shortcuts-for-practice.bat
@@ -1,4 +1,4 @@
-@echo off
+rem @echo off
 
 set RELEASE_FOLDER=%~1
 set NAME_EXTRA=%~2
@@ -10,8 +10,7 @@ if "%RELEASE_FOLDER%" == "" (
 echo "%RELEASE_FOLDER%"
 echo "%NAME_EXTRA%"
 
-java -cp C:\Users\hangil\myclinic-spring\winutil\target\winutil-1.0.0-SNAPSHOT-jar-with-dependencies.jar ^
-  jp.chang.myclinic.winutil.main.CreateShortcut ^
+java -cp .\winutil.jar jp.chang.myclinic.winutil.main.CreateShortcut ^
   "%JAVA_HOME%"\bin\javaw ^
   "-jar ""%RELEASE_FOLDER%""\practice.jar http://localhost:18080/json" ^
   "%RELEASE_FOLDER%" ^
