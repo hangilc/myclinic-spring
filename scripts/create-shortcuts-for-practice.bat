65041e253c335a0a2a5a9b0955974cd10dd582b9
Hangil Chang <hangil@chang.jp>
2018-08-03 19:29:42 +0900

    revised create-shortcut

--- scripts/create-shortcuts-for-practice.bat
+++ scripts/create-shortcuts-for-practice.bat
@@ -1,14 +1,19 @@
-rem @echo off
-set RELEASE_FOLDER=%1
-set NAME_EXTRA=%2
+@echo off
 
-echo %RELEASE_FOLDER%
-echo %NAME_EXTRA%
+set RELEASE_FOLDER=%~1
+set NAME_EXTRA=%~2
+
+if "%RELEASE_FOLDER%" == "" (
+	set RELEASE_FOLDER=%cd%
+)
+
+echo "%RELEASE_FOLDER%"
+echo "%NAME_EXTRA%"
 
 java -cp C:\Users\hangil\myclinic-spring\winutil\target\winutil-1.0.0-SNAPSHOT-jar-with-dependencies.jar ^
   jp.chang.myclinic.winutil.main.CreateShortcut ^
   "%JAVA_HOME%"\bin\javaw ^
   "-jar ""%RELEASE_FOLDER%""\practice.jar http://localhost:18080/json" ^
   "%RELEASE_FOLDER%" ^
-  "%cd%"\êfé@.lnk
+  "%cd%"\êfé@"%NAME_EXTRA%".lnk
 
--- scripts/deploy-current.bat
+++ scripts/deploy-current.bat
@@ -1,5 +1,5 @@
 @echo off
-set RELEASE_FOLDER=C:/Users/hangil/current-test
+set RELEASE_FOLDER=C:\Users\hangil\current-test
 if exist %RELEASE_FOLDER% (
 	set time0=%time::=%
 	set date0=%date:/=%
@@ -15,18 +15,18 @@ mkdir "%RELEASE_FOLDER%"
 if not "%errorlevel%" == "0" (goto end)
 
 xcopy /Y /I config "%RELEASE_FOLDER%/config"
-copy /Y hotline\target\hotline-1.0.0-SNAPSHOT.jar "%RELEASE_FOLDER%/hotline.jar"
-copy /Y intraclinic\target\intraclinic-1.0.0-SNAPSHOT.jar "%RELEASE_FOLDER%/intraclinic.jar"
-copy /Y pharma\target\pharma-1.0.0-SNAPSHOT.jar "%RELEASE_FOLDER%/pharma.jar"
-copy /Y practice\target\practice-1.0.0-SNAPSHOT.jar "%RELEASE_FOLDER%/practice.jar"
-copy /Y reception\target\reception-1.0.0-SNAPSHOT.jar "%RELEASE_FOLDER%/reception.jar"
-copy /Y record-browser\target\record-browser-1.0.0-SNAPSHOT.jar "%RELEASE_FOLDER%/record-browser.jar"
-copy /Y scanner\target\scanner-1.0.0-SNAPSHOT.jar "%RELEASE_FOLDER%/scanner.jar"
-copy /Y server\target\server-1.0.0-SNAPSHOT.jar "%RELEASE_FOLDER%/server.jar"
-copy /Y rcpt-drawer\target\rcpt-drawer-1.0.0-SNAPSHOT.jar "%RELEASE_FOLDER%/rcpt-drawer.jar"
-copy /Y winutil\target\winutil-1.0.0-SNAPSHOT-jar-with-dependencies.jar "%RELEASE_FOLDER%/winutil.jar"
+copy /Y hotline\target\hotline-1.0.0-SNAPSHOT.jar "%RELEASE_FOLDER%\hotline.jar"
+copy /Y intraclinic\target\intraclinic-1.0.0-SNAPSHOT.jar "%RELEASE_FOLDER%\intraclinic.jar"
+copy /Y pharma\target\pharma-1.0.0-SNAPSHOT.jar "%RELEASE_FOLDER%\pharma.jar"
+copy /Y practice\target\practice-1.0.0-SNAPSHOT.jar "%RELEASE_FOLDER%\practice.jar"
+copy /Y reception\target\reception-1.0.0-SNAPSHOT.jar "%RELEASE_FOLDER%\reception.jar"
+copy /Y record-browser\target\record-browser-1.0.0-SNAPSHOT.jar "%RELEASE_FOLDER%\record-browser.jar"
+copy /Y scanner\target\scanner-1.0.0-SNAPSHOT.jar "%RELEASE_FOLDER%\scanner.jar"
+copy /Y server\target\server-1.0.0-SNAPSHOT.jar "%RELEASE_FOLDER%\server.jar"
+copy /Y rcpt-drawer\target\rcpt-drawer-1.0.0-SNAPSHOT.jar "%RELEASE_FOLDER%\rcpt-drawer.jar"
+copy /Y winutil\target\winutil-1.0.0-SNAPSHOT-jar-with-dependencies.jar "%RELEASE_FOLDER%\winutil.jar"
 copy /Y scripts\create-shortcuts-for-practice.bat "%RELEASE_FOLDER%\create-shortcuts-for-practice.bat"
-mkdir "%RELEASE_FOLDER%/work"
+mkdir "%RELEASE_FOLDER%\work"
 
 echo @cmd /K >"%RELEASE_FOLDER%"/console.bat
 
