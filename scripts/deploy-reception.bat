9e6ce2f7265fb277e5735c3b3ca767adc459d984
Hangil Chang <hangil@chang.jp>
2018-08-04 14:26:15 +0900

    added deploy-reception.bat

--- scripts/deploy-reception.bat
+++ scripts/deploy-reception.bat
@@ -1,14 +1,21 @@
-::
-:: Usage: deploy-current [TARGET-FOLDER]
-:: Default target folder is ~/myclinic-releases/current
-::
-@echo off
+:: 
+:: Usage: deploy-reception [TARGET-FOLDER]
+:: Default target folder is %RECEPTION_RELEASE_FOLDER%
+:: 
+rem @echo off
 
 set RELEASE_FOLDER=%~1
 
 if "%RELEASE_FOLDER%" == "" (
-	set RELEASE_FOLDER=%HOMEDRIVE%%HOMEPATH%\myclinic-releases\current
+	set RELEASE_FOLDER=%RECEPTION_RELEASE_FOLDER%
 )
+
+
+if "%RELEASE_FOLDER%" == "" (
+	set RELEASE_FOLDER=%HOMEDRIVE%%HOMEPATH%\myclinic-reception-releases\current
+)
+
+echo %RELEASE_FOLDER%
 
 if exist %RELEASE_FOLDER% (
 	set time0=%time::=%
@@ -22,18 +29,14 @@ if exist %RELEASE_FOLDER% (
 echo creating release folder^: %RELEASE_FOLDER%
 mkdir "%RELEASE_FOLDER%" || exit /B 2
 
-xcopy /Y /I config "%RELEASE_FOLDER%/config"
 copy /Y hotline\target\hotline-1.0.0-SNAPSHOT.jar "%RELEASE_FOLDER%\hotline.jar"
 copy /Y intraclinic\target\intraclinic-1.0.0-SNAPSHOT.jar "%RELEASE_FOLDER%\intraclinic.jar"
 copy /Y pharma\target\pharma-1.0.0-SNAPSHOT.jar "%RELEASE_FOLDER%\pharma.jar"
-copy /Y practice\target\practice-1.0.0-SNAPSHOT.jar "%RELEASE_FOLDER%\practice.jar"
 copy /Y reception\target\reception-1.0.0-SNAPSHOT.jar "%RELEASE_FOLDER%\reception.jar"
 copy /Y record-browser\target\record-browser-1.0.0-SNAPSHOT.jar "%RELEASE_FOLDER%\record-browser.jar"
 copy /Y scanner\target\scanner-1.0.0-SNAPSHOT.jar "%RELEASE_FOLDER%\scanner.jar"
-copy /Y server\target\server-1.0.0-SNAPSHOT.jar "%RELEASE_FOLDER%\server.jar"
-copy /Y rcpt-drawer\target\rcpt-drawer-1.0.0-SNAPSHOT.jar "%RELEASE_FOLDER%\rcpt-drawer.jar"
 copy /Y winutil\target\winutil-1.0.0-SNAPSHOT-jar-with-dependencies.jar "%RELEASE_FOLDER%\winutil.jar"
-copy /Y scripts\create-shortcuts-for-practice.bat "%RELEASE_FOLDER%\create-shortcuts-for-practice.bat"
+copy /Y scripts\create-shortcuts-for-reception.bat "%RELEASE_FOLDER%\create-shortcuts-for-practice.bat"
 mkdir "%RELEASE_FOLDER%\work"
 
 echo @cmd /K >"%RELEASE_FOLDER%"/console.bat
