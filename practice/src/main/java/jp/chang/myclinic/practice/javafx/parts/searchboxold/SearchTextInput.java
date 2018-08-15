d852d7e65d779f627bbbd1162d3611911a561834
hangilc <hangil@chang.jp>
2018-08-15 17:30:31 +0900

    removed searchboxold from practice

--- practice/src/main/java/jp/chang/myclinic/practice/javafx/parts/searchboxold/SearchTextInput.java
+++
@@ -1,9 +0,0 @@
-package jp.chang.myclinic.practice.javafx.parts.searchboxold;
-
-import java.util.function.Consumer;
-
-public interface SearchTextInput {
-
-    void setOnSearchCallback(Consumer<String> cb);
-
-}
