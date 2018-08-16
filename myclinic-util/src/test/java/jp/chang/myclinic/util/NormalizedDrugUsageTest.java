869b4175d1f322455c0b3e04a20b9b354622f503
hangilc <hangil@chang.jp>
2018-08-16 11:33:49 +0900

    added uneven

--- myclinic-util/src/test/java/jp/chang/myclinic/util/NormalizedDrugUsageTest.java
+++ myclinic-util/src/test/java/jp/chang/myclinic/util/NormalizedDrugUsageTest.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class NormalizedDrugUsageTest {
 
@@ -33,6 +32,40 @@ public class NormalizedDrugUsageTest {
         NormalizedDrugUsage nu = new NormalizedDrugUsage("分３　毎食後");
         assertEquals(3, (Object)nu.getTimes());
         assertEquals(List.of("朝食後", "昼食後", "夕食後"), nu.getParts());
+    }
+
+    @Test void parts2Test(){
+        NormalizedDrugUsage nu = new NormalizedDrugUsage("分1　寝る前");
+        assertEquals(1, (Object)nu.getTimes());
+        assertEquals(List.of("寝る前"), nu.getParts());
+    }
+
+    @Test void parts3Test(){
+        NormalizedDrugUsage nu = new NormalizedDrugUsage("分1　眠前");
+        assertEquals(1, (Object)nu.getTimes());
+        assertEquals(List.of("寝る前"), nu.getParts());
+    }
+
+    @Test void unevenTest(){
+        NormalizedDrugUsage nu = new NormalizedDrugUsage("分３ (1-1-2) 毎食後");
+        assertEquals(3, (Object)nu.getTimes());
+        assertEquals("(1-1-2)", nu.getUnevenText());
+        assertEquals("毎食後", nu.getUsageWithoutTimes());
+        assertEquals(List.of("朝食後", "昼食後", "夕食後"), nu.getParts());
+        assertEquals(List.of(1.0, 1.0, 2.0), nu.getWeights());
+    }
+
+    @Test void uneven2Test(){
+        NormalizedDrugUsage nu = new NormalizedDrugUsage("分３  毎食後(1-1-2)");
+        assertEquals(3, (Object)nu.getTimes());
+        assertEquals("(1-1-2)", nu.getUnevenText());
+        assertEquals("毎食後", nu.getUsageWithoutTimes());
+        assertEquals(List.of("朝食後", "昼食後", "夕食後"), nu.getParts());
+        assertEquals(List.of(1.0, 1.0, 2.0), nu.getWeights());
+    }
+
+    @Test void formatErrorTest(){
+        assertThrows(DrugUsageFormatException.class, () -> new NormalizedDrugUsage("分１  毎食後"));
     }
 
 }
