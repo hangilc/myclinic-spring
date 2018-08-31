7bb7a5695be15741731a343f0fdabb60fa5e6da1
Hangil Chang <hangil@chang.jp>
2018-08-31 11:47:00 +0900

    added shelllink load

--- winutil/src/main/java/jp/chang/myclinic/winutil/PersistFile.java
+++ winutil/src/main/java/jp/chang/myclinic/winutil/PersistFile.java
@@ -3,6 +3,7 @@ package jp.chang.myclinic.winutil;
 import com.sun.jna.Pointer;
 import com.sun.jna.platform.win32.COM.Unknown;
 import com.sun.jna.platform.win32.WTypes.LPOLESTR;
+import com.sun.jna.platform.win32.WinDef;
 import com.sun.jna.platform.win32.WinDef.BOOL;
 import com.sun.jna.platform.win32.WinNT.HRESULT;
 
@@ -19,8 +20,15 @@ public class PersistFile extends Unknown implements IPersistFile {
     }
 
     @Override
+    public HRESULT Load(LPOLESTR pszFileName, WinDef.DWORD dwMode) {
+        return (HRESULT)this._invokeNativeObject(5,
+                new Object[]{ this.getPointer(), pszFileName, dwMode}, HRESULT.class);
+    }
+
+    @Override
     public HRESULT Save(LPOLESTR pszFileName, BOOL fRememebr) {
         return (HRESULT)this._invokeNativeObject(6,
                 new Object[]{ this.getPointer(), pszFileName, fRememebr}, HRESULT.class);
     }
+
 }
---
+++ winutil/src/main/java/jp/chang/myclinic/winutil/WinConsts.java
@@ -0,0 +1,16 @@
+package jp.chang.myclinic.winutil;
+
+public class WinConsts {
+
+    private WinConsts() {
+
+    }
+
+    public static final int STGM_READ           = 0x00000000;
+    public static final int STGM_WRITE          = 0x00000001;
+    public static final int STGM_READWRITE      = 0x00000002;
+    public static final int STGM_CREATE         = 0x00001000;
+    public static final int STGM_CONVERT        = 0x00020000;
+    public static final int STGM_FAILIFTHERE    = 0x00000000;
+
+}
