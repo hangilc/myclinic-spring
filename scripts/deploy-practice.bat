67e330a496c02126e02c94363fb7bbd09922a510
Hangil Chang <hangil@chang.jp>
2018-09-01 08:43:24 +0900

    added deploy-practice.bat

--- scripts/deploy-practice.bat
+++ scripts/deploy-practice.bat
@@ -4,14 +4,22 @@
 :: jar files are copied from %MYCLINIC_REPOSITORY%\current
 ::
 
-:: @echo off
+@echo off
 setlocal enabledelayedexpansion
 set dst=%MYCLINIC_PRACTICE_PROG%
 if not exist "%dst%" (
     mkdir "%dst%"
 )
-set /P current=<"%MYCLINIC_REPOSITORY%\current.txt"
-set current=%current%
-copy "%current%\hotline.jar" "%dst%"
+set current=%MYCLINIC_REPOSITORY%\current
+copy /Y "%current%\hotline.jar" "%dst%"
+copy /Y "%current%\intraclinic.jar" "%dst%"
+copy /Y "%current%\pharma.jar" "%dst%"
+copy /Y "%current%\practice.jar" "%dst%"
+copy /Y "%current%\rcpt-drawer.jar" "%dst%"
+copy /Y "%current%\reception.jar" "%dst%"
+copy /Y "%current%\record-browser.jar" "%dst%"
+copy /Y "%current%\scanner.jar" "%dst%"
+copy /Y "%current%\server.jar" "%dst%"
+xcopy "%current%\config" "%dst%\config" /I /Y /S /E /Q
 endlocal
 
