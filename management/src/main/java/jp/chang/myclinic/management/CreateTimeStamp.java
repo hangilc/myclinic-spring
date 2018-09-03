a09b87f3a71418f3975fafcc68aab7292a4f9a9e
Hangil Chang <hangil@chang.jp>
2018-09-03 18:38:31 +0900

    fixed CreateTimeStamp

--- management/src/main/java/jp/chang/myclinic/management/CreateTimeStamp.java
+++ management/src/main/java/jp/chang/myclinic/management/CreateTimeStamp.java
@@ -6,7 +6,7 @@ import java.time.format.DateTimeFormatter;
 public class CreateTimeStamp {
 
     public static void main(String[] args) {
-        DateTimeFormatter format = DateTimeFormatter.ofPattern("uuuuMMdd-hhmmss");
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("uuuuMMdd-HHmmss");
         LocalDateTime t = LocalDateTime.now();
         System.out.println(t.format(format));
     }
--- scripts/release-install.bat
+++ scripts/release-install.bat
@@ -26,12 +26,14 @@ copy intraclinic\target\intraclinic-1.0.0-SNAPSHOT.jar "%folder%\intraclinic.jar
 copy management\target\management-1.0.0-SNAPSHOT-jar-with-dependencies.jar "%folder%\management.jar"
 copy pharma\target\pharma-1.0.0-SNAPSHOT.jar "%folder%\pharma.jar"
 copy practice\target\practice-1.0.0-SNAPSHOT.jar "%folder%\practice.jar"
+copy rcpt\target\rcpt-1.0.0-SNAPSHOT.jar "%folder%\rcpt.jar"
+copy rcpt-drawer\target\rcpt-drawer-1.0.0-SNAPSHOT.jar "%folder%\rcpt-drawer.jar"
 copy reception\target\reception-1.0.0-SNAPSHOT.jar "%folder%\reception.jar"
 copy record-browser\target\record-browser-1.0.0-SNAPSHOT.jar "%folder%\record-browser.jar"
 copy scanner\target\scanner-1.0.0-SNAPSHOT.jar "%folder%\scanner.jar"
 copy server\target\server-1.0.0-SNAPSHOT.jar "%folder%\server.jar"
-copy rcpt-drawer\target\rcpt-drawer-1.0.0-SNAPSHOT.jar "%folder%\rcpt-drawer.jar"
 xcopy config "%folder%\config" /I /Y /S /E /Q
+type nul >"%folder%\myclinic-release-%ts%.txt"
 set current=%repo%\current
 if exist "%current%" (
     rmdir "%current%"
