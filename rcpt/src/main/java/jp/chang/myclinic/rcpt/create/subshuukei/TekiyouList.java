6f69a892b6a340dff544b9ed82dccca4d653f467
Hangil Chang <hangil@chang.jp>
2018-09-07 08:32:17 +0900

    removed toukutei shikkan shohou abbreviation

--- rcpt/src/main/java/jp/chang/myclinic/rcpt/create/subshuukei/TekiyouList.java
+++ rcpt/src/main/java/jp/chang/myclinic/rcpt/create/subshuukei/TekiyouList.java
@@ -42,10 +42,10 @@ class TekiyouList {
         shinryouAliasMap = new HashMap<>();
         shinryouAliasMap.put(shinryouMasterMap.薬剤情報提供, "（薬情）");
         shinryouAliasMap.put(shinryouMasterMap.特定疾患管理, "（特）");
-        shinryouAliasMap.put(shinryouMasterMap.特定疾患処方, "（特処）");
-        shinryouAliasMap.put(shinryouMasterMap.特定疾患処方管理加算処方せん料, "（特処）");
-        shinryouAliasMap.put(shinryouMasterMap.長期処方, "（特処長）");
-        shinryouAliasMap.put(shinryouMasterMap.長期投薬加算処方せん料, "（特処長）");
+//        shinryouAliasMap.put(shinryouMasterMap.特定疾患処方, "（特処）");
+//        shinryouAliasMap.put(shinryouMasterMap.特定疾患処方管理加算処方せん料, "（特処）");
+//        shinryouAliasMap.put(shinryouMasterMap.長期処方, "（特処長）");
+//        shinryouAliasMap.put(shinryouMasterMap.長期投薬加算処方せん料, "（特処長）");
     }
 
     private Map<Integer, String> getShinryouAliasMap(){
