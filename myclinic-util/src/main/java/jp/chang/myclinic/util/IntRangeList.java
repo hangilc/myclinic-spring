45d4367eebf1714d7b5aebd3ba75570f059d758b
Hangil Chang <hangil@chang.jp>
2018-09-07 08:31:18 +0900

    temporary saving

---
+++ myclinic-util/src/main/java/jp/chang/myclinic/util/IntRangeList.java
@@ -0,0 +1,13 @@
+package jp.chang.myclinic.util;
+
+import java.util.ArrayList;
+
+public final class IntRangeList extends ArrayList<IntRange> {
+
+    //private static Logger logger = LoggerFactory.getLogger(IntRangeList.class);
+
+    IntRangeList() {
+
+    }
+
+}
