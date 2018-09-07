96ae62d413dfbc2871bb317a3f83ae0ec2059817
Hangil Chang <hangil@chang.jp>
2018-09-07 18:52:23 +0900

    temporary saving

--- management/src/main/java/jp/chang/myclinic/management/ReleaseDelete.java
+++ management/src/main/java/jp/chang/myclinic/management/ReleaseDelete.java
@@ -1,6 +1,7 @@
 package jp.chang.myclinic.management;
 
 import jp.chang.myclinic.management.lib.ReleaseLib;
+import jp.chang.myclinic.util.IntRange;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -12,7 +13,7 @@ public class ReleaseDelete {
 
     public static void main(String[] args) throws Exception {
         //noinspection InfiniteLoopStatement
-        while(true){
+        while (true) {
             doList();
             break;
         }
@@ -21,25 +22,61 @@ public class ReleaseDelete {
     private static void doList() throws IOException {
         String cur = ReleaseLib.getCurrentVersion();
         List<Path> paths = ReleaseLib.listReleases();
-        for(int i=0;i<paths.size();i++){
+        Integer currentPos = null;
+        for (int i = 0; i < paths.size(); i++) {
             String dir = paths.get(i).getFileName().toString();
             boolean isCurrent = dir.equals(cur);
+            if( isCurrent ){
+                currentPos = i+1;
+            }
             String pre = makePrefix(paths.size(), i, isCurrent);
             System.out.println(pre + dir);
         }
+        System.out.println();
+        System.out.println("削除するリリースを番号で指定してください。");
+        System.out.println("終了するには q と入力してください。");
+        System.out.print("> ");
+        String line = System.console().readLine();
+        line = line.trim();
+        if( "q".equals(line) ){
+
+        }
+        List<IntRange> ranges = IntRange.parseList(line, makeResolver(currentPos, paths.size()));
     }
 
-    private static String makePrefix(int total, int index, boolean isCurrent){
+    private static IntRange.Resolver makeResolver(Integer current, int size) {
+        return (s, pos) -> {
+            switch (s) {
+                case "*":
+                    if( current != null ) {
+                        return current;
+                    } else {
+                        throw new RuntimeException("Current release is not available.");
+                    }
+                case "":
+                    if (pos == IntRange.Position.Start) {
+                        return 1;
+                    } else if (pos == IntRange.Position.End) {
+                        return size;
+                    } else {
+                        throw new RuntimeException("Invalid int rage");
+                    }
+                default: throw new RuntimeException("Invalid int rage");
+            }
+        }
+    }
+
+    private static String makePrefix(int total, int index, boolean isCurrent) {
         String numFormat = "%d.";
         String curFormat = "*.";
-        if( total >= 10 ){
+        if (total >= 10) {
             numFormat = "%2d.";
             curFormat = "**.";
         }
-        if( isCurrent ){
+        if (isCurrent) {
             return curFormat;
         } else {
-            return String.format(numFormat, index+1);
+            return String.format(numFormat, index + 1);
         }
     }
 
--- myclinic-util/src/main/java/jp/chang/myclinic/util/IntRange.java
+++ myclinic-util/src/main/java/jp/chang/myclinic/util/IntRange.java
@@ -3,36 +3,41 @@ package jp.chang.myclinic.util;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
 import java.util.regex.Pattern;
 
-public class IntRange {
+import static jp.chang.myclinic.util.IntRange.Position.End;
+import static jp.chang.myclinic.util.IntRange.Position.Single;
 
-    private int startValue;
-    private int endValue;
+public final class IntRange {
+
+    public enum Position {
+        Start, End, Single
+    }
+
+    public interface Resolver {
+        int resolve(String token, Position position);
+    }
+
+    private final int startValue;
+    private final int endValue;
 
     private IntRange(int startValue, int endValue) {
         this.startValue = startValue;
         this.endValue = endValue;
     }
 
-    public void traverse(Consumer<Integer> proc){
-        if( startValue <= endValue){
-            for(int i = startValue; i<= endValue; i++){
-                proc.accept(i);
-            }
-        } else {
-            for(int i = startValue; i>= endValue; i--){
-                proc.accept(i);
-            }
-        }
+    public int getStartValue() {
+        return startValue;
+    }
+
+    public int getEndValue() {
+        return endValue;
     }
 
     private static Pattern partSepPattern = Pattern.compile("[\\s,]+");
     private static Pattern rangeSepPattern = Pattern.compile("\\s*-\\s*");
 
-    public static List<IntRange> parse(String input, Function<String, Integer> specials){
+    public static List<IntRange> parseList(String input, Resolver resolver){
         if( input == null ){
             return Collections.emptyList();
         }
@@ -42,31 +47,23 @@ public class IntRange {
         for(String part: parts){
             String[] limits = rangeSepPattern.split(part, 2);
             if( limits.length == 1 ){
-                int value = convertToInt(limits[0], specials);
+                int value = convertToInt(limits[0], resolver, Single);
                 result.add(new IntRange(value, value));
             } else {
-                int minValue = convertToInt(limits[0], specials);
-                int maxValue = convertToInt(limits[1], specials);
+                int minValue = convertToInt(limits[0], resolver, Position.Start);
+                int maxValue = convertToInt(limits[1], resolver, End);
                 result.add(new IntRange(minValue, maxValue));
             }
         }
-        return result;
+        return Collections.unmodifiableList(result);
     }
 
-    private static int convertToInt(String s, Function<String, Integer> specials){
+    private static int convertToInt(String s, Resolver resolver, Position pos){
         try {
             return Integer.parseInt(s);
         } catch(NumberFormatException ex){
-            Integer value =  specials.apply(s);
-            if( value == null ){
-                throw new RuntimeException("Invalid integer value: " + s);
-            }
-            return value;
+            return  resolver.resolve(s, pos);
         }
-    }
-
-    public static void traverse(List<IntRange> ranges, Consumer<Integer> proc){
-        ranges.forEach(range -> range.traverse(proc));
     }
 
 }
