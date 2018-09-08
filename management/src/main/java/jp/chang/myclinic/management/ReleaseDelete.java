eba28766b07d59b69109705bed8cb1551209dcab
Hangil Chang <hangil@chang.jp>
2018-09-08 15:10:23 +0900

    temporary saving

--- management/src/main/java/jp/chang/myclinic/management/ReleaseDelete.java
+++ management/src/main/java/jp/chang/myclinic/management/ReleaseDelete.java
@@ -5,51 +5,94 @@ import jp.chang.myclinic.util.IntRange;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ReleaseDelete {
 
     //private static Logger logger = LoggerFactory.getLogger(ReleaseDelete.class);
 
     public static void main(String[] args) throws Exception {
+        ReleaseDelete instance = new ReleaseDelete();
+        instance.run();
+    }
+
+    private List<Path> releasePaths;
+    private Integer currentIndex;
+
+    private ReleaseDelete(){
+
+    }
+
+    private void run() throws IOException {
         //noinspection InfiniteLoopStatement
         while (true) {
+            doUpdate();
             doList();
-            break;
+            doPrompt();
+            String line = System.console().readLine();
+            line = line.trim();
+            if( "q".equals(line) ){
+                break;
+            }
+            try {
+                List<IntRange> ranges = IntRange.parseList(line, makeResolver(currentIndex, releasePaths.size()));
+                System.out.println(ranges);
+            } catch(Exception ex){
+                System.err.println(ex);
+            }
         }
     }
 
-    private static void doList() throws IOException {
+    private void doUpdate() throws IOException {
         String cur = ReleaseLib.getCurrentVersion();
-        List<Path> paths = ReleaseLib.listReleases();
-        Integer currentPos = null;
-        for (int i = 0; i < paths.size(); i++) {
-            String dir = paths.get(i).getFileName().toString();
-            boolean isCurrent = dir.equals(cur);
-            if( isCurrent ){
-                currentPos = i+1;
+        this.releasePaths = ReleaseLib.listReleases();
+        this.currentIndex = null;
+        for (int i = 0; i < releasePaths.size(); i++) {
+            String dir = releasePaths.get(i).getFileName().toString();
+            if( dir.equals(cur) ){
+                this.currentIndex = i;
+                break;
             }
-            String pre = makePrefix(paths.size(), i, isCurrent);
-            System.out.println(pre + dir);
         }
+    }
+
+    private void doPrompt(){
         System.out.println();
         System.out.println("削除するリリースを番号で指定してください。");
         System.out.println("終了するには q と入力してください。");
         System.out.print("> ");
-        String line = System.console().readLine();
-        line = line.trim();
-        if( "q".equals(line) ){
-
-        }
-        List<IntRange> ranges = IntRange.parseList(line, makeResolver(currentPos, paths.size()));
     }
 
-    private static IntRange.Resolver makeResolver(Integer current, int size) {
+    private void doList() throws IOException {
+        for (int i = 0; i < releasePaths.size(); i++) {
+            String dir = releasePaths.get(i).getFileName().toString();
+            boolean isCurrent = currentIndex != null && currentIndex == i;
+            String pre = makePrefix(releasePaths.size(), i, isCurrent);
+            System.out.println(pre + " " + dir);
+        }
+    }
+
+    private void doListToBeDeleted(List<IntRange> deleteList) throws IOException {
+        Set<Integer> deleteSet = new HashSet<>(IntRange.toIntList(deleteList));
+        for (int i = 0; i < releasePaths.size(); i++) {
+            String dir = releasePaths.get(i).getFileName().toString();
+            boolean isCurrent = currentIndex != null && currentIndex == i;
+            if( (!isCurrent) && deleteSet.contains(i) ){
+
+            }
+            String pre = makePrefix(releasePaths.size(), i, isCurrent);
+            System.out.println(pre + " " + dir);
+        }
+    }
+
+    private IntRange.Resolver makeResolver(Integer current, int size) {
         return (s, pos) -> {
             switch (s) {
                 case "*":
                     if( current != null ) {
-                        return current;
+                        return current + 1;
                     } else {
                         throw new RuntimeException("Current release is not available.");
                     }
@@ -63,10 +106,10 @@ public class ReleaseDelete {
                     }
                 default: throw new RuntimeException("Invalid int rage");
             }
-        }
+        };
     }
 
-    private static String makePrefix(int total, int index, boolean isCurrent) {
+    private String makePrefix(int total, int index, boolean isCurrent) {
         String numFormat = "%d.";
         String curFormat = "*.";
         if (total >= 10) {
--- myclinic-util/src/main/java/jp/chang/myclinic/util/IntRange.java
+++ myclinic-util/src/main/java/jp/chang/myclinic/util/IntRange.java
@@ -3,6 +3,7 @@ package jp.chang.myclinic.util;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.IntConsumer;
 import java.util.regex.Pattern;
 
 import static jp.chang.myclinic.util.IntRange.Position.End;
@@ -34,19 +35,39 @@ public final class IntRange {
         return endValue;
     }
 
-    private static Pattern partSepPattern = Pattern.compile("[\\s,]+");
+    public void traverse(IntConsumer callback) {
+        if (startValue <= endValue) {
+            for (int i = startValue; i <= endValue; i++) {
+                callback.accept(i);
+            }
+        } else {
+            for (int i = startValue; i >= endValue; i--) {
+                callback.accept(i);
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "IntRange{" +
+                "startValue=" + startValue +
+                ", endValue=" + endValue +
+                '}';
+    }
+
+    private static Pattern partSepPattern = Pattern.compile("\\s*,\\s*");
     private static Pattern rangeSepPattern = Pattern.compile("\\s*-\\s*");
 
-    public static List<IntRange> parseList(String input, Resolver resolver){
-        if( input == null ){
+    public static List<IntRange> parseList(String input, Resolver resolver) {
+        if (input == null) {
             return Collections.emptyList();
         }
         input = input.trim();
         List<IntRange> result = new ArrayList<>();
         String[] parts = partSepPattern.split(input);
-        for(String part: parts){
+        for (String part : parts) {
             String[] limits = rangeSepPattern.split(part, 2);
-            if( limits.length == 1 ){
+            if (limits.length == 1) {
                 int value = convertToInt(limits[0], resolver, Single);
                 result.add(new IntRange(value, value));
             } else {
@@ -58,11 +79,23 @@ public final class IntRange {
         return Collections.unmodifiableList(result);
     }
 
-    private static int convertToInt(String s, Resolver resolver, Position pos){
+    public static void traverse(List<IntRange> list, IntConsumer callback) {
+        for (IntRange range : list) {
+            range.traverse(callback);
+        }
+    }
+
+    public static List<Integer> toIntList(List<IntRange> rangeList){
+        List<Integer> result = new ArrayList<>();
+        IntRange.traverse(rangeList, result::add);
+        return result;
+    }
+
+    private static int convertToInt(String s, Resolver resolver, Position pos) {
         try {
             return Integer.parseInt(s);
-        } catch(NumberFormatException ex){
-            return  resolver.resolve(s, pos);
+        } catch (NumberFormatException ex) {
+            return resolver.resolve(s, pos);
         }
     }
 
