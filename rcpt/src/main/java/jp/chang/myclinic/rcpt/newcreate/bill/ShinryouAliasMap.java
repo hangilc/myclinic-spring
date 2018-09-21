45059b8b5c5152d431811d2fb46a5a85e9762e5e
Hangil Chang <hangil@chang.jp>
2018-09-21 15:08:10 +0900

    removed shinryou alias map

--- rcpt/src/main/java/jp/chang/myclinic/rcpt/newcreate/bill/ShinryouAliasMap.java
+++ rcpt/src/main/java/jp/chang/myclinic/rcpt/newcreate/bill/ShinryouAliasMap.java
@@ -15,12 +15,12 @@ class ShinryouAliasMap {
 
     static Map<Integer, String> create(ResolvedShinryouMap resolvedShinryouMap){
         Map<Integer, String> map = new HashMap<>();
-        map.put(resolvedShinryouMap.薬剤情報提供, "（薬情）");
-        map.put(resolvedShinryouMap.特定疾患管理, "（特）");
-        map.put(resolvedShinryouMap.特定疾患処方, "（特処）");
-        map.put(resolvedShinryouMap.特定疾患処方管理加算処方せん料, "（特処）");
-        map.put(resolvedShinryouMap.長期処方, "（特処長）");
-        map.put(resolvedShinryouMap.長期投薬加算処方せん料, "（特処長）");
+//        map.put(resolvedShinryouMap.薬剤情報提供, "（薬情）");
+//        map.put(resolvedShinryouMap.特定疾患管理, "（特）");
+//        map.put(resolvedShinryouMap.特定疾患処方, "（特処）");
+//        map.put(resolvedShinryouMap.特定疾患処方管理加算処方せん料, "（特処）");
+//        map.put(resolvedShinryouMap.長期処方, "（特処長）");
+//        map.put(resolvedShinryouMap.長期投薬加算処方せん料, "（特処長）");
         return map;
     }
 
