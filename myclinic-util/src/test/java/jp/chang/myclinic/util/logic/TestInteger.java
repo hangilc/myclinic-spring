7c7f2d341973a65d93311411e2321aea6369a9b9
Hangil Chang <hangil@chang.jp>
2018-10-04 13:37:49 +0900

    added test for IntegerLogic

--- myclinic-util/src/test/java/jp/chang/myclinic/util/logic/TestInteger.java
+++ myclinic-util/src/test/java/jp/chang/myclinic/util/logic/TestInteger.java
@@ -3,6 +3,7 @@ package jp.chang.myclinic.util.logic;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 public class TestInteger extends LogicBase {
@@ -14,6 +15,32 @@ public class TestInteger extends LogicBase {
         Integer value = logic.getValue(em);
         assertTrue(em.hasNoError());
         assertEquals(12, (int)value);
+    }
+
+    @Test
+    public void testNull(){
+        IntegerLogic logic = new IntegerLogic("TEST");
+        Integer value = logic.getValue(em);
+        assertTrue(em.hasError());
+        assertNull(value);
+    }
+
+    @Test
+    public void testInput(){
+        IntegerLogic logic = new IntegerLogic("TEST");
+        logic.setStorageValue("32", em);
+        String store = logic.getStorageValue(em);
+        assertTrue(em.hasNoError());
+        assertEquals("32", store);
+    }
+
+    @Test
+    public void testInvalidInput(){
+        IntegerLogic logic = new IntegerLogic("TEST");
+        logic.setStorageValue("32a", em);
+        String store = logic.getStorageValue(em);
+        assertTrue(em.hasError());
+        assertEquals(null, store);
     }
 
 }
