a455d675b73a4f90c0e6634a1f8aa15b8fc78125
Hangil Chang <hangil@chang.jp>
2018-10-13 17:04:00 +0900

    rewrote date form logic

--- utilfx/src/main/java/jp/chang/myclinic/utilfx/dateinput/DateFormLogic.java
+++ utilfx/src/main/java/jp/chang/myclinic/utilfx/dateinput/DateFormLogic.java
@@ -1,160 +1,85 @@
 package jp.chang.myclinic.utilfx.dateinput;
 
 import jp.chang.myclinic.consts.Gengou;
-import jp.chang.myclinic.util.value.ErrorMessages;
-import jp.chang.myclinic.util.value.Logic;
-import jp.chang.myclinic.util.value.LogicValue;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import jp.chang.myclinic.util.logic.Converter;
+import jp.chang.myclinic.util.logic.ErrorMessages;
+import jp.chang.myclinic.util.logic.LogicValue;
 
 import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.chrono.JapaneseDate;
 import java.time.temporal.ChronoField;
-import java.util.function.BiConsumer;
 
-import static jp.chang.myclinic.util.value.Converters.stringToInteger;
-import static jp.chang.myclinic.util.value.Validators.*;
+import static jp.chang.myclinic.util.logic.Converters.stringToInteger;
+import static jp.chang.myclinic.util.logic.Validators.*;
 
 public class DateFormLogic {
 
-    private static Logger logger = LoggerFactory.getLogger(DateFormLogic.class);
-
-    public static LocalDate inputsToDate(DateFormInputs inputs, String name, ErrorMessages em){
-        if( inputs == null ){
-            em.add(String.format("DateFormInputs is null. (%s)", name));
-            return null;
-        }
-        if( inputs.isEmpty() ){
-            return null;
-        }
-        ErrorMessages emDate = new ErrorMessages();
-        Gengou gengou = new LogicValue<Gengou>(inputs.gengou)
-                .validate(isNotNull())
-                .getValue("元号", emDate);
-        int nen = new LogicValue<String>(inputs.nen)
-                .validate(isNotNull())
-                .validate(isNotEmpty())
-                .convert(stringToInteger())
-                .validate(inRange(1, Integer.MAX_VALUE))
-                .getValueAsInt("年", emDate);
-        int month = new LogicValue<String>(inputs.month)
-                .validate(isNotNull())
-                .validate(isNotEmpty())
-                .convert(stringToInteger())
-                .validate(inRange(1, 12))
-                .getValueAsInt("月", emDate);
-        int day = new LogicValue<String>(inputs.day)
-                .validate(isNotNull())
-                .validate(isNotEmpty())
-                .convert(stringToInteger())
-                .validate(inRange(1, 31))
-                .getValueAsInt("日", emDate);
-        if( emDate.hasError() ){
-            em.add(String.format("%sの内容が不適切です。", name));
-            em.indent();
-            em.add(emDate);
-            em.unindent();
-            return null;
-        } else {
-            try {
-                return LocalDate.ofEpochDay(JapaneseDate.of(gengou.getEra(), nen, month, day).toEpochDay());
-            } catch (DateTimeException ex) {
+    public static Converter<DateFormInputs, LocalDate> inputsToLocalDate() {
+        return (inputs, name, em) -> {
+            if (inputs == null || inputs.isEmpty()) {
+                return null;
+            }
+            ErrorMessages emDate = new ErrorMessages();
+            Gengou gengou = new LogicValue<Gengou>(inputs.gengou)
+                    .validate(isNotNull())
+                    .getValue("元号", emDate);
+            int nen = new LogicValue<String>(inputs.nen)
+                    .validate(isNotNull())
+                    .validate(isNotEmpty())
+                    .convert(stringToInteger())
+                    .validate(isInRange(1, Integer.MAX_VALUE))
+                    .getValueOrElse(0, "年", emDate);
+            int month = new LogicValue<String>(inputs.month)
+                    .validate(isNotNull())
+                    .validate(isNotEmpty())
+                    .convert(stringToInteger())
+                    .validate(isInRange(1, 12))
+                    .getValueOrElse(0, "月", emDate);
+            int day = new LogicValue<String>(inputs.day)
+                    .validate(isNotNull())
+                    .validate(isNotEmpty())
+                    .convert(stringToInteger())
+                    .validate(isInRange(1, 31))
+                    .getValueOrElse(0, "日", emDate);
+            if (emDate.hasError()) {
                 em.add(String.format("%sの内容が不適切です。", name));
+                em.indent();
+                em.add(emDate);
+                em.unindent();
+                return null;
+            } else {
+                try {
+                    return LocalDate.ofEpochDay(JapaneseDate.of(gengou.getEra(), nen, month, day).toEpochDay());
+                } catch (DateTimeException ex) {
+                    em.add(String.format("%sの内容が不適切です。", name));
+                    return null;
+                }
+            }
+        };
+    }
+
+    public static Converter<LocalDate, DateFormInputs> localDateToInputs(){
+        return (date, name, em) -> {
+            if( date == null ){
                 return null;
             }
-        }
-    }
-
-    public static void verifyValidFromAndValidUpto(LocalDate validFrom, LocalDate validUpto,
-                                                      String validFromName, String validUptoName,
-                                                      ErrorMessages em){
-        if( validFrom == null ){
-            em.add(String.format("%sが設定されていません。", validFromName));
-            return;
-        }
-        if( validUpto == null ){
-            return;
-        }
-        if (!validFrom.equals(validUpto) && !validFrom.isBefore(validUpto)) {
-            em.add(String.format("%sが%sより前の値です。", validUptoName, validFromName));
-        }
-    }
-
-    public static DateFormInputs dateToInputs(LocalDate date){
-        try {
-            JapaneseDate jd = JapaneseDate.from(date);
-            Gengou gengou = Gengou.fromEra(jd.getEra());
-            DateFormInputs inputs = new DateFormInputs(gengou);
-            int nen = jd.get(ChronoField.YEAR_OF_ERA);
-            int month = date.getMonthValue();
-            int day = date.getDayOfMonth();
-            inputs.nen = "" + nen;
-            inputs.month = "" + month;
-            inputs.day = "" + day;
-            return inputs;
-        } catch (DateTimeException ex) {
-            logger.error("Invalid date. " + date);
-            return null;
-        } catch(NullPointerException ex){
-            logger.error("Null date. ");
-            return null;
-        }
-    }
-
-    public static DateFormInputs storageValueToInputs(String store){
-        try {
-            if( "0000-00-00".equals(store) ){
+            try {
+                JapaneseDate jd = JapaneseDate.from(date);
+                Gengou gengou = Gengou.fromEra(jd.getEra());
+                DateFormInputs inputs = new DateFormInputs(gengou);
+                int nen = jd.get(ChronoField.YEAR_OF_ERA);
+                int month = date.getMonthValue();
+                int day = date.getDayOfMonth();
+                inputs.nen = "" + nen;
+                inputs.month = "" + month;
+                inputs.day = "" + day;
+                return inputs;
+            } catch (DateTimeException ex) {
+                em.add(String.format("%sが不適切は日付です。", name));
                 return null;
             }
-            LocalDate date = LocalDate.parse(store);
-            return dateToInputs(date);
-        } catch(NumberFormatException ex){
-            logger.error("Invalid date string.", ex);
-            return null;
-        } catch(NullPointerException ex){
-            logger.error("Null pointer.", ex);
-            return null;
-        }
-    }
-
-    public static String dateToStorageValue(LocalDate date, String name, ErrorMessages em){
-        if( date == null ){
-            em.add(String.format("%sが設定されていません。", name));
-            return null;
-        }
-        return date.toString();
-    }
-
-    public static String dateToValidUptoStorageValue(LocalDate date, String name, ErrorMessages em){
-        if( date == null ){
-            return "0000-00-00";
-        } else {
-            return dateToStorageValue(date, name, em);
-        }
-    }
-
-    public static void verifyValidFromAndValidUptoInputs(DateFormInputs validFromInputs,
-                                                         DateFormInputs validUptoInputs,
-                                                         String validFromName, String validUptoName,
-                                                         ErrorMessages em,
-                                                         BiConsumer<String, String> handler){
-        int ne = em.getNumberOfErrors();
-        LocalDate validFrom = inputsToDate(validFromInputs, validFromName, em);
-        LocalDate validUpto = inputsToDate(validUptoInputs, validUptoName, em);
-        if( em.hasErrorSince(ne) ){
-            return;
-        }
-        verifyValidFromAndValidUpto(validFrom, validUpto, validFromName, validUptoName, em);
-        if( em.hasErrorSince(ne) ){
-            return;
-        }
-        String validFromStorage = dateToStorageValue(validFrom, validFromName, em);
-        String validUptoStorage = dateToValidUptoStorageValue(validUpto, validUptoName, em);
-        if( em.hasErrorSince(ne) ){
-            return;
-        }
-        handler.accept(validFromStorage, validUptoStorage);
+        };
     }
 
 }
---
+++ utilfx/src/test/java/jp/chang/myclinic/utilfx/dateinput/TestDateInput.java
@@ -0,0 +1,166 @@
+package jp.chang.myclinic.utilfx.dateinput;
+
+import jp.chang.myclinic.consts.Gengou;
+import jp.chang.myclinic.util.logic.LogicValue;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static jp.chang.myclinic.utilfx.dateinput.DateFormLogic.inputsToLocalDate;
+import static jp.chang.myclinic.utilfx.dateinput.DateFormLogic.localDateToInputs;
+import static org.junit.Assert.*;
+
+public class TestDateInput extends LogicTestBase {
+
+    @Test
+    public void testSimple(){
+        DateFormInputs inputs = new DateFormInputs(Gengou.Heisei, "30", "10", "13");
+        LocalDate date = new LogicValue<DateFormInputs>(inputs)
+                .convert(inputsToLocalDate())
+                .getValue("TEST", em);
+        assertTrue(em.hasNoError());
+        assertEquals(LocalDate.of(2018, 10, 13), date);
+    }
+
+    @Test
+    public void testNull(){
+        DateFormInputs inputs = new DateFormInputs(Gengou.Heisei, null, null, null);
+        LocalDate date = new LogicValue<DateFormInputs>(inputs)
+                .convert(inputsToLocalDate())
+                .getValue("TEST", em);
+        assertTrue(em.hasNoError());
+        assertNull(date);
+    }
+
+    @Test
+    public void testEmpty(){
+        DateFormInputs inputs = new DateFormInputs(Gengou.Heisei, "", "", "");
+        LocalDate date = new LogicValue<DateFormInputs>(inputs)
+                .convert(inputsToLocalDate())
+                .getValue("TEST", em);
+        assertTrue(em.hasNoError());
+        assertNull(date);
+    }
+
+    @Test
+    public void testNullGengou(){
+        DateFormInputs inputs = new DateFormInputs(null, "30", "10", "13");
+        LocalDate date = new LogicValue<DateFormInputs>(inputs)
+                .convert(inputsToLocalDate())
+                .getValue("TEST", em);
+        assertTrue(em.hasError());
+        assertNull(date);
+    }
+
+    @Test
+    public void testNullNen(){
+        DateFormInputs inputs = new DateFormInputs(Gengou.Heisei, null, "10", "13");
+        LocalDate date = new LogicValue<DateFormInputs>(inputs)
+                .convert(inputsToLocalDate())
+                .getValue("TEST", em);
+        assertTrue(em.hasError());
+        assertNull(date);
+    }
+
+    @Test
+    public void testEmptyNen(){
+        DateFormInputs inputs = new DateFormInputs(Gengou.Heisei, "", "10", "13");
+        LocalDate date = new LogicValue<DateFormInputs>(inputs)
+                .convert(inputsToLocalDate())
+                .getValue("TEST", em);
+        assertTrue(em.hasError());
+        assertNull(date);
+    }
+
+    @Test
+    public void testInvalidNen(){
+        DateFormInputs inputs = new DateFormInputs(Gengou.Heisei, "0", "10", "13");
+        LocalDate date = new LogicValue<DateFormInputs>(inputs)
+                .convert(inputsToLocalDate())
+                .getValue("TEST", em);
+        assertTrue(em.hasError());
+        assertNull(date);
+    }
+
+    @Test
+    public void testNullMonth(){
+        DateFormInputs inputs = new DateFormInputs(Gengou.Heisei, "30", null, "13");
+        LocalDate date = new LogicValue<DateFormInputs>(inputs)
+                .convert(inputsToLocalDate())
+                .getValue("TEST", em);
+        assertTrue(em.hasError());
+        assertNull(date);
+    }
+
+    @Test
+    public void testEmptyMonth(){
+        DateFormInputs inputs = new DateFormInputs(Gengou.Heisei, "30", "", "13");
+        LocalDate date = new LogicValue<DateFormInputs>(inputs)
+                .convert(inputsToLocalDate())
+                .getValue("TEST", em);
+        assertTrue(em.hasError());
+        assertNull(date);
+    }
+
+    @Test
+    public void testInvalidMonth(){
+        DateFormInputs inputs = new DateFormInputs(Gengou.Heisei, "30", "13", "13");
+        LocalDate date = new LogicValue<DateFormInputs>(inputs)
+                .convert(inputsToLocalDate())
+                .getValue("TEST", em);
+        assertTrue(em.hasError());
+        assertNull(date);
+    }
+
+    @Test
+    public void testNullDay(){
+        DateFormInputs inputs = new DateFormInputs(Gengou.Heisei, "30", "10", null);
+        LocalDate date = new LogicValue<DateFormInputs>(inputs)
+                .convert(inputsToLocalDate())
+                .getValue("TEST", em);
+        assertTrue(em.hasError());
+        assertNull(date);
+    }
+
+    @Test
+    public void testEmptyDay(){
+        DateFormInputs inputs = new DateFormInputs(Gengou.Heisei, "30", "10", "");
+        LocalDate date = new LogicValue<DateFormInputs>(inputs)
+                .convert(inputsToLocalDate())
+                .getValue("TEST", em);
+        assertTrue(em.hasError());
+        assertNull(date);
+    }
+
+    @Test
+    public void testInvalidDay(){
+        DateFormInputs inputs = new DateFormInputs(Gengou.Heisei, "30", "10", "32");
+        LocalDate date = new LogicValue<DateFormInputs>(inputs)
+                .convert(inputsToLocalDate())
+                .getValue("TEST", em);
+        assertTrue(em.hasError());
+        assertNull(date);
+    }
+
+    @Test
+    public void testDateToInputs(){
+        LocalDate date = LocalDate.of(2018, 10, 13);
+        DateFormInputs inputs = new LogicValue<>(date)
+                .convert(localDateToInputs())
+                .getValue("TEST", em);
+        assertTrue(em.hasNoError());
+        assertEquals(Gengou.Heisei, inputs.gengou);
+        assertEquals("30", inputs.nen);
+        assertEquals("10", inputs.month);
+        assertEquals("13", inputs.day);
+    }
+
+    @Test
+    public void testNullDateToInputs(){
+        DateFormInputs inputs = new LogicValue<LocalDate>(null)
+                .convert(localDateToInputs())
+                .getValue("TEST", em);
+        assertTrue(em.hasNoError());
+        assertNull(inputs);
+    }
+}
