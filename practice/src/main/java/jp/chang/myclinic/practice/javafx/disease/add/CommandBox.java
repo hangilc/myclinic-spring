f13cc83e1bbf536309fd6a891ef2712833cf5dc9
hangilc <hangil@chang.jp>
2018-10-24 22:00:17 +0900

    revised enter disease command hbox

--- practice/src/main/java/jp/chang/myclinic/practice/javafx/disease/add/CommandBox.java
+++ practice/src/main/java/jp/chang/myclinic/practice/javafx/disease/add/CommandBox.java
@@ -1,5 +1,6 @@
 package jp.chang.myclinic.practice.javafx.disease.add;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.layout.HBox;
@@ -16,6 +17,7 @@ public class CommandBox extends HBox {
 
     public CommandBox() {
         super(4);
+        setAlignment(Pos.CENTER_LEFT);
         Button enterButton = new Button("入力");
         Hyperlink suspLink = new Hyperlink("の疑い");
         Hyperlink deleteAdjLink = new Hyperlink("修飾語削除");
