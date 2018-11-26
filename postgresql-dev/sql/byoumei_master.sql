96bb3680a0b9f4cf90880aa7682654f9743d7641
Hangil Chang <hangil@chang.jp>
2018-11-26 19:03:38 +0900

    modified schema sql

--- postgresql-dev/sql/byoumei_master.sql
+++ postgresql-dev/sql/byoumei_master.sql
@@ -1,27 +1,27 @@
-create table byoumei_master (
-	shoubyoumeicode integer not null check (shoubyoumeicode > 0),
-	name varchar(60) not null check(char_length(name) > 0),
-	valid_from date not null,
-	valid_upto date,
-	primary key (shoubyoumeicode, valid_from)
+CREATE TABLE byoumei_master (
+	shoubyoumeicode INTEGER NOT NULL CHECK (shoubyoumeicode > 0),
+	name VARCHAR(60) NOT NULL CHECK (char_length(name) > 0),
+	valid_from DATE NOT NULL,
+	valid_upto DATE,
+	PRIMARY KEY (shoubyoumeicode, valid_from)
 );
 
-create or replace function check_byoumei_master_fun() returns trigger as $$
-	declare
-		count integer;
-	begin
-		select count(*) into count from byoumei_master where shoubyoumeicode = new.shoubyoumeicode 
-			and valid_from <> new.valid_from
-			and not (
-				(valid_upto is not null and valid_upto < new.valid_from) or
-				(new.valid_upto is not null and new.valid_upto < valid_from)
+CREATE OR REPLACE FUNCTION check_byoumei_master_fun() returns trigger AS $$
+	DECLARE
+		count INTEGER;
+	BEGIN
+		SELECT count(*) INTO count FROM public.byoumei_master WHERE shoubyoumeicode = new.shoubyoumeicode 
+			AND valid_from <> new.valid_from
+			AND not (
+				(valid_upto IS NOT NULL AND valid_upto < new.valid_from) OR
+				(new.valid_upto IS NOT NULL AND new.valid_upto < valid_from)
 			);
-		if count > 0 then
-			raise exception 'conflicting byoumei_master row exists';
-		end if;
-		return new;
-	end;
-$$ language plpgsql;
+		IF count > 0 THEN
+			RAISE EXCEPTION 'conflicting byoumei_master row exists';
+		END IF;
+		RETURN new;
+	END;
+$$ LANGUAGE PLPGSQL;
 
 create trigger check_byoumei_master before insert or update on byoumei_master
 	for each row execute procedure check_byoumei_master_fun();
