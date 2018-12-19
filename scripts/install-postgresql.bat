ab3ee67533077c6e1f5f40a26664a92f70092753
hangilc <hangil@chang.jp>
2018-12-19 22:52:46 +0900

    added open firewall port to install postgresql

--- scripts/install-postgresql.bat
+++ scripts/install-postgresql.bat
@@ -1,8 +1,15 @@
 @echo off
 rem Usage: install-postgresql installer.exe
-%1 --datadir C:\pgdata\main\cluster --enable-components server,commandlinetools --locale C ^
-	--mode unattended --servicename postgresql
+rem %1 --datadir C:\pgdata\main\cluster --enable-components server,commandlinetools --locale C ^
+rem 	--mode unattended --servicename postgresql
 echo "PostgreSQL has been installed."
+set /P yesno="Open firewall port 5432? (Y/N): "
+if "%yesno%" == "Y" (
+	powershell -Command "start-process netsh -Verb runas -Argument 'advfirewall firewall add rule action=allow dir=in protocol=tcp name=PostgreSQL profile=private localport=5432'"
+	echo "    Port 5432 has been opend."
+)
 echo "Add bin path to PATH environment variable."
+
+
 
 
