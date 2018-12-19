b5fabe54f91d19ca314c92720e66e37047e38a6b
hangilc <hangil@chang.jp>
2018-12-19 17:02:01 +0900

    fixed migrate from mysql

--- scripts/migrate-from-mysql.ps1
+++ scripts/migrate-from-mysql.ps1
@@ -1,6 +1,6 @@
 my-pyenv\Scripts\Activate.ps1
 $env:PYTHONPATH="db\scripts"
-# scripts\setup-new-postgresql-repository
+.\scripts\setup-new-postgresql-repository
 $loc = get-location
 set-location "postgresql-dev\sql"
 psql -f create-master-tables.sql myclinic "$env:MYCLINIC_DB_ADMIN_USER"
