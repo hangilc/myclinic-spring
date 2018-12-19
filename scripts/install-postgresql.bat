c55f1b50159d4f6fd898791e993bcc64221b3a07
hangilc <hangil@chang.jp>
2018-12-19 19:08:30 +0900

    added message to install postgresql

--- scripts/install-postgresql.bat
+++ scripts/install-postgresql.bat
@@ -2,4 +2,7 @@
 rem Usage: install-postgresql installer.exe
 %1 --datadir C:\pgdata\main\cluster --enable-components server,commandlinetools --locale C ^
 	--mode unattended --servicename postgresql
+echo "PostgreSQL has been installed."
+echo "Add bin path to PATH environment variable."
+
 
