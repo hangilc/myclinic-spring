92c84e89cc3e845596c483eb6df25cf24ae399d1
hangilc <hangil@chang.jp>
2018-12-31 18:51:45 +0900

    rewrote point in time recovery

--- scripts/postgresql-point-in-time-recover.ps1
+++ scripts/postgresql-point-in-time-recover.ps1
@@ -1,35 +1,73 @@
+[CmdletBinding(PositionalBinding=$false)]
 Param (
+    [string][alias('Host')]$DbHost = "localhost",
     [ValidatePattern('\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}')]
-    [string]$at = $null
+    [string]$At = "",
+    [ValidateSet("pause", "promote", "shutdown")]
+    [string]$TargetAction = "promote"
 )
 
 $ErrorActionPreference = "Stop"
+& "$PSScriptRoot\use-local-psmodules"
 
+$repo = Get-PostgreSQLRepository
 $targetTime = ''
 if( $at ){
     Write-Host "recovering to $at"
     $targetTime = "recovery_target_time = '$at'"
 }
 
-$isRunning = "Running" -eq (Get-Service -Name 'PostgreSQL' `
-    | Select-Object -ExpandProperty Status)
+$isRunning = Test-PostgreSQLServiceIsRunning $DbHost
 if( $isRunning ){
-    Stop-Service 'PostgreSQL'
+    Stop-PostgreSQLService $DbHost
 }
 
-$repo = 'C:\pgdata\main'
-$backup = "$repo\backup"
-$cluster = "$repo\cluster"
-$timestamp = Get-Date -Format "yyyy-MM-dd-HHmmss"
-$clusterSave = "$cluster-$timestamp"
-Rename-Item -Path $cluster -NewName $clusterSave
-Copy-Item $backup $cluster -recurse
-Remove-Item "$cluster\pg_wal" -recurse -force
-Copy-Item "$clusterSave\pg_wal" "$cluster\pg_wal" -recurse
-$recovery = @"
-restore_command = 'copy "C:\\pgdata\\main\\walarchive\\%f" "%p"'
+$block = {
+    Param($repo, $targetAction, $targetTime)
+    $backup = "$repo\backup"
+    $cluster = "$repo\cluster"
+    $timestamp = Get-Date -Format "yyyy-MM-dd-HHmmss"
+    $clusterSave = "$cluster-$timestamp"
+    Rename-Item -Path $cluster -NewName $clusterSave
+    Copy-Item $backup $cluster -recurse
+    Remove-Item "$cluster\pg_wal" -recurse -force
+    Copy-Item "$clusterSave\pg_wal" "$cluster\pg_wal" -recurse
+    $walarch = "$repo\walarchive" -replace "\\", "\\"
+    $recovery = @"
+restore_command = 'copy "$walarch\\%f" "%p"'
+recovery_target_action = '$targetAction'
 $targetTime
 "@
-$recovery | Out-File -FilePath "$cluster\recovery.conf" -Encoding ASCII
+    Write-Host $recovery
+    $recovery | Out-File -FilePath "$cluster\recovery.conf" -Encoding ASCII
+}
 
-Start-Service 'PostgreSQL'
+if( $DbHost in ".", "localhost", "127.0.0.1" ){
+    Invoke-Command -ScriptBlock $block -ArgumentList $repo, $targetAction, $targetTime
+} else {
+    Invoke-Command -ComputerName $DbHost `
+                   -ScriptBlock $block -ArgumentList $repo, $targetAction, $targetTime 
+}
+
+Start-PostgreSQLService $DbHost
+
+# if( $isRunning ){
+#     Stop-Service 'PostgreSQL'
+# }
+
+# $repo = 'C:\pgdata\main'
+# $backup = "$repo\backup"
+# $cluster = "$repo\cluster"
+# $timestamp = Get-Date -Format "yyyy-MM-dd-HHmmss"
+# $clusterSave = "$cluster-$timestamp"
+# Rename-Item -Path $cluster -NewName $clusterSave
+# Copy-Item $backup $cluster -recurse
+# Remove-Item "$cluster\pg_wal" -recurse -force
+# Copy-Item "$clusterSave\pg_wal" "$cluster\pg_wal" -recurse
+# $recovery = @"
+# restore_command = 'copy "C:\\pgdata\\main\\walarchive\\%f" "%p"'
+# $targetTime
+# "@
+# $recovery | Out-File -FilePath "$cluster\recovery.conf" -Encoding ASCII
+
+# Start-Service 'PostgreSQL'
