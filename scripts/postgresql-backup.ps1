564c25bc6643ef5aba42c41ead8dc87c65c1e116
hangilc <hangil@chang.jp>
2019-01-03 18:12:23 +0900

    added remote backup

--- scripts/postgresql-backup.ps1
+++ scripts/postgresql-backup.ps1
@@ -1,11 +1,22 @@
 Param (
-    [string]$saveDir = "$env:userprofile\db-backup"
+    [string]$SaveDir = "$env:userprofile\db-backup",
+    [string][alias('Host')]$DbHost = "localhost"
 )
 
-if( -not (Test-Path -Path $saveDir) ){
-    New-Item -ItemType directory -Path $saveDir
+$block = {
+    Param($saveDir, $backup)
+    if( -not (Test-Path -Path $SaveDir) ){
+        New-Item -ItemType directory -Path $saveDir
+    }
+    cmd.exe /c "pg_dump -U postgres -f `"$backup`" -E UTF8 -Fc --verbose myclinic 2>&1"
 }
+
 $timestamp = Get-Date -Format "yyyy-MM-dd-HHmmss"
 $backup = "$saveDir\myclinic-postgresql-backup-$timestamp.dump"
-pg_dump -U postgres -f $backup -E UTF8 -Fc --verbose myclinic
-Write-Host "Backup has been created at: $backup"
+
+if( $DbHost -in ".", "localhost", "127.0.0.1" ){
+    Invoke-Command -ScriptBlock $block -ArgumentList $SaveDir, $backup
+} else {
+    Invoke-Command -ScriptBlock $block -ArgumentList $SaveDir, $backup -ComputerName $DbHost
+}
+Write-Host "Backup has been created at: $backup (Host $DbHost)"
