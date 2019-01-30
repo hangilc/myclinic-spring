7ac9a803b97fc3f3e2b41f8de302cf8a2800bb96
hangilc <hangil@chang.jp>
2019-01-30 16:19:16 +0900

    added pick names

--- integral-test/src/main/java/jp/chang/myclinic/integraltest/SampleData.java
+++ integral-test/src/main/java/jp/chang/myclinic/integraltest/SampleData.java
@@ -1,38 +1,60 @@
 package jp.chang.myclinic.integraltest;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.*;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.Scanner;
 
 public class SampleData {
 
     //private static Logger logger = LoggerFactory.getLogger(SampleData.class);
-    List<String> firstNames;
-    Random random = new Random();
+    private static class NameEntry {
+        public String kanji;
+        public String yomi;
+        public int freq;
 
-    public SampleData(){
-        this.firstNames = loadFirstNames();
+        private NameEntry(String kanji, String yomi, int freq){
+            this.kanji = kanji;
+            this.yomi = yomi;
+            this.freq = freq;
+        }
+
+        @Override
+        public String toString() {
+            return "NameEntry{" +
+                    "kanji='" + kanji + '\'' +
+                    ", yomi='" + yomi + '\'' +
+                    ", freq=" + freq +
+                    '}';
+        }
     }
 
-    private List<String> loadFirstNames(){
-        List<String> lines = new ArrayList<>();
+    private List<NameEntry> lastNames;
+    private List<NameEntry> maleFirstNames;
+    private List<NameEntry> femaleFirstNames;
+    private Random random = new Random();
+
+    public SampleData(){
+        this.lastNames = loadNames("/last-names.txt");
+        this.maleFirstNames = loadNames("/male-first-names.txt");
+        this.femaleFirstNames = loadNames("/female-first-names.txt");
+    }
+
+    private List<NameEntry> loadNames(String name){
+        List<NameEntry> lines = new ArrayList<>();
         try {
-            InputStream ins = getClass().getResourceAsStream("/first-names.txt");
-            if( ins == null ){
-                throw new RuntimeException("Cannot read first-names.txt");
-            }
+            Path path = Paths.get("config", name);
+            InputStream ins = new FileInputStream(path.toFile());
             BufferedReader reader = new BufferedReader(new InputStreamReader(ins, StandardCharsets.UTF_8));
-            reader.lines().forEach(lines::add);
+            reader.lines().forEach(line -> {
+                String[] parts = line.split("\\s*\\|\\s*");
+                if( parts.length == 3 ){
+                    lines.add(new NameEntry(parts[0], parts[1], Integer.parseInt(parts[2])));
+                }
+            });
             reader.close();
             return lines;
         } catch (Exception e) {
@@ -41,9 +63,28 @@ public class SampleData {
         }
     }
 
-    public String pickFirstName(){
-        int i = random.nextInt(firstNames.size());
-        return firstNames.get(i);
+    public NameEntry pickLastName(){
+        int i = random.nextInt(lastNames.size());
+        return lastNames.get(i);
+    }
+
+    public String pickSex(){
+        if( random.nextDouble() < 0.5 ){
+            return "M";
+        } else {
+            return "F";
+        }
+    }
+
+    public NameEntry pickFirstName(String sex){
+        List<NameEntry> entries;
+        if( sex.equals("M") ){
+            entries = maleFirstNames;
+        } else {
+            entries = femaleFirstNames;
+        }
+        int i = random.nextInt(entries.size());
+        return entries.get(i);
     }
 
 }
