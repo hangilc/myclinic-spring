c6f67d22e9e8c1cad9c05eb355c8212b198f2e48
hangilc <hangil@chang.jp>
2019-01-30 22:40:24 +0900

    started integral-test

---
+++ integral-test/scripts/integral-test.jsh
@@ -0,0 +1,19 @@
+/env --class-path reception-grpc/target/reception-grpc-1.0.0-SNAPSHOT-jar-with-dependencies.jar;integral-test\target\integral-test-1.0.0-SNAPSHOT-jar-with-dependencies.jar
+import io.grpc.*;
+import jp.chang.myclinic.reception.grpc.generated.*;
+import static jp.chang.myclinic.reception.grpc.generated.ReceptionMgmtOuterClass.*;
+import jp.chang.myclinic.integraltest.*;
+
+var channel = ManagedChannelBuilder.forAddress("localhost", 18084).usePlaintext().build();
+var stub = ReceptionMgmtGrpc.newBlockingStub(channel);
+var sample = new SampleData();
+
+void start() throws Exception {
+    stub.clickMainPaneNewPatientButton(null);
+    Thread.sleep(1);
+    var win = stub.findCreatedNewPatientWindow(null);
+    var patientInputs = sample.pickPatientInputs();
+    var req = SetNewPatientWindowInputsRequest.newBuilder().setWindow(win).setInputs(patientInputs).build();
+    stub.setNewPatientWindowInputs(req);
+    stub.clickNewPatientWindowEnterButton(win);
+}
--- integral-test/src/main/java/jp/chang/myclinic/integraltest/SampleData.java
+++ integral-test/src/main/java/jp/chang/myclinic/integraltest/SampleData.java
@@ -1,9 +1,15 @@
 package jp.chang.myclinic.integraltest;
 
+import jp.chang.myclinic.reception.grpc.generated.ReceptionMgmtOuterClass;
+import jp.chang.myclinic.util.kanjidate.GengouNenPair;
+import jp.chang.myclinic.util.kanjidate.KanjiDate;
+
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -85,6 +91,54 @@ public class SampleData {
         }
         int i = random.nextInt(entries.size());
         return entries.get(i);
+    }
+
+    private int pickBirthdayYear(){
+        int age = random.nextInt(100);
+        return LocalDate.now().getYear() - 1 - age;
+    }
+
+    public LocalDate pickBirthday(){
+        int year = pickBirthdayYear();
+        int month = 1 + random.nextInt(12);
+        int lastDay = LocalDate.of(year, month, 1).plus(1, ChronoUnit.MONTHS).minus(1, ChronoUnit.DAYS)
+                .getDayOfMonth();
+        int day = 1 + random.nextInt(lastDay);
+        return LocalDate.of(year, month, day);
+    }
+
+    public String pickAddress(){
+        int choume = 1 + random.nextInt(9);
+        int ban = 1 + random.nextInt(18);
+        int chi = 1 + random.nextInt(28);
+        return String.format("杉並区参考地%d-%d-%d", choume, ban, chi);
+    }
+
+    public String pickPhone(){
+        List<String> ds = List.of("03", "070", "080", "090");
+        String d = ds.get(random.nextInt(ds.size()));
+        return String.format("%s-%04d-%04d", d, random.nextInt(10000), random.nextInt(10000));
+    }
+
+    public ReceptionMgmtOuterClass.PatientInputs pickPatientInputs(){
+        NameEntry lastName = pickLastName();
+        String sex = pickSex();
+        NameEntry firstName = pickFirstName(sex);
+        LocalDate birthday = pickBirthday();
+        GengouNenPair gn = KanjiDate.yearToGengou(birthday);
+        return ReceptionMgmtOuterClass.PatientInputs.newBuilder()
+                .setLastName(lastName.kanji)
+                .setFirstName(firstName.kanji)
+                .setLastNameYomi(lastName.yomi)
+                .setFirstNameYomi(firstName.yomi)
+                .setBirthdayGengou(gn.gengou.getKanjiRep())
+                .setBirthdayNen(gn.nen + "")
+                .setBirthdayMonth(birthday.getMonthValue() + "")
+                .setBirthdayDay(birthday.getDayOfMonth() + "")
+                .setSex(sex)
+                .setAddress(pickAddress())
+                .setPhone(pickPhone())
+                .build();
     }
 
 }
