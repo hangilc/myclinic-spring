2bdbb5152b55ad9780d3ec93bfd2e4ab93e9b02f
Hangil Chang <hangil@chang.jp>
2019-02-05 18:51:20 +0900

    improved start mock server

--- integral-test/scripts/start-mock-server.ps1
+++ integral-test/scripts/start-mock-server.ps1
@@ -1,3 +1,8 @@
 $env:MYCLINIC_DB_HOST="192.168.33.10"
-java -jar server\target\server-1.0.0-SNAPSHOT.jar --server.port=18084
-$env:MYCLINIC_MOCK_SERVER="http://localhost:18084"
+Start-Process -FilePath powershell -ArgumentList '-Command', {
+    java -jar server\target\server-1.0.0-SNAPSHOT.jar --server.port=18084
+}, '-noexit'
+Write-Host "mock server started at port 18084"
+# $env:MYCLINIC_DB_HOST="192.168.33.10"
+# java -jar server\target\server-1.0.0-SNAPSHOT.jar --server.port=18084
+# $env:MYCLINIC_MOCK_SERVER="http://localhost:18084"
