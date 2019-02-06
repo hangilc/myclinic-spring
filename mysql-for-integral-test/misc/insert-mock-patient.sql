d463cfdb127596b83e57c7adf2c0d6e277b19787
hangilc <hangil@chang.jp>
2019-02-06 15:18:34 +0900

    revised mysql-for-integral-test

---
+++ mysql-for-integral-test/misc/insert-mock-patient.sql
@@ -0,0 +1,3 @@
+insert into patient (patient_id, first_name, last_name, first_name_yomi, last_name_yomi, sex, birth_day, address, phone) 
+	values (1, 'データ', '試験', 'でーた', 'しけん', 'F', '1957-01-23', '', '');
+	
