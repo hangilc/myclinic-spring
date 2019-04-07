802bc885e6f7844a4f00f7fdabede6c384dc7615
hangilc <hangil@chang.jp>
2019-04-07 18:55:38 +0900

    added test for RecordHoken

--- practice/src/main/java/jp/chang/myclinic/practice/componenttest/hoken/RecordHokenTest.java
+++ practice/src/main/java/jp/chang/myclinic/practice/componenttest/hoken/RecordHokenTest.java
@@ -18,7 +18,11 @@ import jp.chang.myclinic.util.DateTimeUtil;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class RecordHokenTest extends ComponentTestBase {
 
@@ -142,6 +146,153 @@ public class RecordHokenTest extends ComponentTestBase {
         }
     }
 
+    @CompTest
+    public void editKouhi1() {
+        MockData mock = new MockData();
+        int patientId = 1;
+        KouhiDTO kouhi1 = mock.pickKouhiWithKouhiId(patientId);
+        HokenDTO hoken = new HokenDTO();
+        hoken.kouhi1 = kouhi1;
+        VisitDTO visit = new VisitDTO();
+        visit.visitId = 1;
+        visit.patientId = patientId;
+        visit.kouhi1Id = kouhi1.kouhiId;
+        visit.visitedAt = DateTimeUtil.toSqlDateTime(LocalDateTime.now());
+        Context.frontend = createFrontend(hoken, hoken);
+        RecordHoken record = createRecord(hoken, visit);
+        {
+            HokenDisp disp = waitFor(record::findDisp);
+            gui(() -> disp.fireEvent(createMouseClickedEvent(disp)));
+            HokenSelectForm form = waitFor(record::findForm);
+            gui(() -> {
+                form.simulateKouhiSelect(kouhi1.kouhiId, false);
+                form.simulateEnterButtonClick();
+            });
+            HokenDisp disp2 = waitFor(record::findDisp);
+            HokenDTO updated = disp2.getHoken();
+            confirm(updated.shahokokuho == null && updated.koukikourei == null &&
+                    updated.roujin == null && updated.kouhi1 == null &&
+                    updated.kouhi2 == null && updated.kouhi3 == null);
+        }
+        {
+            HokenDisp disp = waitFor(record::findDisp);
+            gui(() -> disp.fireEvent(createMouseClickedEvent(disp)));
+            HokenSelectForm form = waitFor(record::findForm);
+            gui(() -> {
+                form.simulateKouhiSelect(kouhi1.kouhiId, true);
+                form.simulateEnterButtonClick();
+            });
+            HokenDisp disp2 = waitFor(record::findDisp);
+            HokenDTO updated = disp2.getHoken();
+            confirm(updated.shahokokuho == null && updated.koukikourei == null &&
+                    updated.roujin == null && kouhi1.equals(updated.kouhi1) &&
+                    updated.kouhi2 == null && updated.kouhi3 == null);
+        }
+    }
+
+    @CompTest
+    public void editKouhi2() {
+        MockData mock = new MockData();
+        int patientId = 1;
+        KouhiDTO kouhi1 = mock.pickKouhiWithKouhiId(patientId);
+        KouhiDTO kouhi2 = mock.pickKouhiWithKouhiId(patientId);
+        HokenDTO hoken = new HokenDTO();
+        hoken.kouhi1 = kouhi1;
+        hoken.kouhi2 = kouhi2;
+        VisitDTO visit = new VisitDTO();
+        visit.visitId = 1;
+        visit.patientId = patientId;
+        visit.kouhi1Id = kouhi1.kouhiId;
+        visit.kouhi2Id = kouhi2.kouhiId;
+        visit.visitedAt = DateTimeUtil.toSqlDateTime(LocalDateTime.now());
+        Context.frontend = createFrontend(hoken, hoken);
+        RecordHoken record = createRecord(hoken, visit);
+        for (boolean selKouhi1 : List.of(false, true)) {
+            for (boolean selKouhi2 : List.of(false, true)) {
+                HokenDisp disp = waitFor(record::findDisp);
+                gui(() -> disp.fireEvent(createMouseClickedEvent(disp)));
+                HokenSelectForm form = waitFor(record::findForm);
+                gui(() -> {
+                    form.simulateKouhiSelect(kouhi1.kouhiId, selKouhi1);
+                    form.simulateKouhiSelect(kouhi2.kouhiId, selKouhi2);
+                    form.simulateEnterButtonClick();
+                });
+                HokenDisp disp2 = waitFor(record::findDisp);
+                HokenDTO updated = disp2.getHoken();
+                KouhiDTO[] resultKouhi = new KouhiDTO[]{null, null};
+                int resultIndex = 0;
+                if (selKouhi1) {
+                    resultKouhi[resultIndex++] = kouhi1;
+                }
+                if (selKouhi2) {
+                    resultKouhi[resultIndex] = kouhi2;
+                }
+                confirm(isHoken(updated, null, null, resultKouhi[0], resultKouhi[1], null));
+            }
+        }
+    }
+
+    @CompTest
+    public void editKouhi3() {
+        MockData mock = new MockData();
+        int patientId = 1;
+        KouhiDTO kouhi1 = mock.pickKouhiWithKouhiId(patientId);
+        KouhiDTO kouhi2 = mock.pickKouhiWithKouhiId(patientId);
+        KouhiDTO kouhi3 = mock.pickKouhiWithKouhiId(patientId);
+        HokenDTO hoken = new HokenDTO();
+        hoken.kouhi1 = kouhi1;
+        hoken.kouhi2 = kouhi2;
+        hoken.kouhi3 = kouhi3;
+        VisitDTO visit = new VisitDTO();
+        visit.visitId = 1;
+        visit.patientId = patientId;
+        visit.kouhi1Id = kouhi1.kouhiId;
+        visit.kouhi2Id = kouhi2.kouhiId;
+        visit.kouhi3Id = kouhi3.kouhiId;
+        visit.visitedAt = DateTimeUtil.toSqlDateTime(LocalDateTime.now());
+        Context.frontend = createFrontend(hoken, hoken);
+        RecordHoken record = createRecord(hoken, visit);
+        for (boolean selKouhi1 : List.of(false, true)) {
+            for (boolean selKouhi2 : List.of(false, true)) {
+                for (boolean selKouhi3 : List.of(false, true)) {
+                    HokenDisp disp = waitFor(record::findDisp);
+                    gui(() -> disp.fireEvent(createMouseClickedEvent(disp)));
+                    HokenSelectForm form = waitFor(record::findForm);
+                    gui(() -> {
+                        form.simulateKouhiSelect(kouhi1.kouhiId, selKouhi1);
+                        form.simulateKouhiSelect(kouhi2.kouhiId, selKouhi2);
+                        form.simulateKouhiSelect(kouhi3.kouhiId, selKouhi3);
+                        form.simulateEnterButtonClick();
+                    });
+                    HokenDisp disp2 = waitFor(record::findDisp);
+                    HokenDTO updated = disp2.getHoken();
+                    KouhiDTO[] resultKouhi = new KouhiDTO[]{null, null, null};
+                    int resultIndex = 0;
+                    if (selKouhi1) {
+                        resultKouhi[resultIndex++] = kouhi1;
+                    }
+                    if (selKouhi2) {
+                        resultKouhi[resultIndex++] = kouhi2;
+                    }
+                    if (selKouhi3) {
+                        resultKouhi[resultIndex] = kouhi3;
+                    }
+                    confirm(isHoken(updated, null, null, resultKouhi[0], resultKouhi[1], resultKouhi[2]));
+                }
+            }
+        }
+    }
+
+    private boolean isHoken(HokenDTO hoken, ShahokokuhoDTO shahokokuho, KoukikoureiDTO koukikourei,
+                            KouhiDTO kouhi1, KouhiDTO kouhi2, KouhiDTO kouhi3) {
+        return Objects.equals(hoken.shahokokuho, shahokokuho) &&
+                Objects.equals(hoken.koukikourei, koukikourei) &&
+                hoken.roujin == null &&
+                Objects.equals(hoken.kouhi1, kouhi1) &&
+                Objects.equals(hoken.kouhi2, kouhi2) &&
+                Objects.equals(hoken.kouhi3, kouhi3);
+    }
+
     private Frontend createFrontend(HokenDTO available, HokenDTO current) {
         HokenDTO currentHoken = HokenDTO.copy(available);
         return new FrontendAdapter() {
@@ -199,7 +350,10 @@ public class RecordHokenTest extends ComponentTestBase {
     }
 
     private KouhiDTO findKouhi(HokenDTO hoken, int kouhiId) {
-        for (KouhiDTO kouhi : List.of(hoken.kouhi1, hoken.kouhi2, hoken.kouhi3)) {
+        List<KouhiDTO> kouhiList = Stream.of(hoken.kouhi1, hoken.kouhi2, hoken.kouhi3)
+                .filter(Objects::nonNull)
+                .collect(toList());
+        for (KouhiDTO kouhi : kouhiList) {
             if (kouhi != null && kouhi.kouhiId == kouhiId) {
                 return kouhi;
             }
