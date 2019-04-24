d407c0c0ac37c522d88029217266c2a66f23ab63
hangilc <hangil@chang.jp>
2019-04-24 15:19:20 +0900

    added primitive client get in FrontendRest

--- frontend/src/main/java/jp/chang/myclinic/frontend/FrontendRest.java
+++ frontend/src/main/java/jp/chang/myclinic/frontend/FrontendRest.java
@@ -2,6 +2,7 @@ package jp.chang.myclinic.frontend;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jp.chang.myclinic.dto.PatientDTO;
+import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.jackson.internal.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,23 +16,45 @@ import javax.ws.rs.core.MediaType;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.function.Consumer;
 
 public class FrontendRest {
     static ExecutorService executorService = Executors.newFixedThreadPool(10);
-    static Client client = ClientBuilder.newBuilder()
-            .executorService(executorService).build();
+    static ObjectMapper mapper = new ObjectMapper();
+    static ClientConfig clientConfig = new ClientConfig();
 
-    CompletableFuture<PatientDTO> getPatient(int patientId){
-        ObjectMapper mapper = new ObjectMapper();
-        client.register(new JacksonJaxbJsonProvider(mapper,
+    static {
+        clientConfig.executorService(executorService);
+        clientConfig.register(new JacksonJaxbJsonProvider(mapper,
                 JacksonJaxbJsonProvider.DEFAULT_ANNOTATIONS));
-        return client.target("http://localhost:38080/api")
-                .path("get-patient")
-                .queryParam("patient-id", patientId)
-                .request(MediaType.APPLICATION_JSON)
+    }
+
+    interface ParamSetter {
+        void set(String name, Object value);
+    }
+
+    private <T> CompletableFuture<T> get(String path, Consumer<ParamSetter> paramSetter,
+                                         Class<T> returnType) {
+        class Local {
+            private WebTarget target;
+        }
+        Local local = new Local();
+        local.target = ClientBuilder.newClient(clientConfig)
+                .target("http://localhost:38080/api")
+                .path(path);
+        paramSetter.accept((name, value) -> {
+            local.target = local.target.queryParam(name, value);
+        });
+        return local.target.request(MediaType.APPLICATION_JSON)
                 .rx()
-                .get(new GenericType<PatientDTO>(){})
+                //.post(null, new GenericType<T>(returnType){})
+                .get(new GenericType<T>(returnType) {})
                 .toCompletableFuture();
+    }
+
+    public CompletableFuture<PatientDTO> getPatient(int patientId) {
+        return get("get-patient", setter -> setter.set("patient-id", patientId),
+                PatientDTO.class);
     }
 
 }
