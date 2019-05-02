28b10a2f5799941648f544d43b4f9749cfeefdca
hangilc <hangil@chang.jp>
2019-05-02 21:18:36 +0900

    rewrote update patient test

--- backend-db/src/main/java/jp/chang/myclinic/backenddb/test/PatientTester.java
+++ backend-db/src/main/java/jp/chang/myclinic/backenddb/test/PatientTester.java
@@ -1,10 +1,8 @@
 package jp.chang.myclinic.backenddb.test;
 
-import jp.chang.myclinic.backenddb.Backend;
 import jp.chang.myclinic.backenddb.DbBackend;
 import jp.chang.myclinic.backenddb.test.annotation.DbTest;
 import jp.chang.myclinic.dto.PatientDTO;
-import jp.chang.myclinic.dto.PracticeConfigDTO;
 import jp.chang.myclinic.logdto.practicelog.PatientCreated;
 import jp.chang.myclinic.logdto.practicelog.PracticeLogDTO;
 
@@ -36,24 +34,39 @@ class PatientTester extends TesterBase {
         confirm(logs.size() == 1 && logs.get(0).asPatientCreated().created.equals(p));
     }
 
-//    @DbTest
-//    public void testUpdate(Backend backend){
-//        PatientDTO p = mock.pickPatient();
-//        p.sex = "M";
-//        backend.enterPatient(p);
-//        p.lastName += "変更";
-//        p.firstName += "変更";
-//        p.lastNameYomi += "変更";
-//        p.firstNameYomi += "変更";
-//        p.birthday = LocalDate.parse(p.birthday).minus(1, ChronoUnit.YEARS).toString();
-//        p.sex = "F";
-//        p.address += "変更";
-//        p.phone += "変更";
-//        backend.updatePatient(p);
-//        PatientDTO pp = backend.getPatient(p.patientId);
-//        confirm(p.equals(pp));
-//    }
-//
+    @DbTest
+    public void testUpdate(){
+        int logIndex = getCurrentPracticeLogIndex();
+        PatientDTO p = mock.pickPatient();
+        p.sex = "M";
+        dbBackend.txProc(backend -> backend.enterPatient(p));
+        PatientDTO pp = PatientDTO.copy(p);
+        pp.lastName += "変更";
+        pp.firstName += "変更";
+        pp.lastNameYomi += "変更";
+        pp.firstNameYomi += "変更";
+        pp.birthday = LocalDate.parse(p.birthday).minus(1, ChronoUnit.YEARS).toString();
+        pp.sex = "F";
+        pp.address += "変更";
+        pp.phone += "変更";
+        dbBackend.txProc(backend -> backend.updatePatient(pp));
+        PatientDTO ppp = dbBackend.query(backend -> backend.getPatient(p.patientId));
+        confirm(ppp.equals(pp));
+        List<PracticeLogDTO> logs = getPracticeLogList(logIndex, log -> {
+            if( log.isPatientCreated() ){
+                return log.asPatientCreated().created.patientId == p.patientId;
+            }
+            if( log.isPatientUpdated() ){
+                return log.asPatientUpdated().updated.patientId == p.patientId;
+            }
+            return false;
+        });
+        confirm(logs.size() == 2);
+        confirm(logs.get(0).asPatientCreated().created.equals(p));
+        confirm(logs.get(1).isPatientUpdated() && logs.get(1).asPatientUpdated().prev.equals(p) &&
+                logs.get(1).asPatientUpdated().updated.equals(pp));
+    }
+
 //    @DbTest
 //    public void testSeachPatient(Backend backend){
 //        List<PatientDTO> patients;
