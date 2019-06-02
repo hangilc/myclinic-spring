e7ff28832dd3d7cf484b78910c8a07346f75cca2
hangilc <hangil@chang.jp>
2019-06-02 09:29:29 +0900

    temporary saving

--- api-tool/src/main/java/jp/chang/myclinic/apitool/CSharpClient.java
+++ api-tool/src/main/java/jp/chang/myclinic/apitool/CSharpClient.java
@@ -6,6 +6,10 @@ import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.body.Parameter;
+import com.github.javaparser.ast.expr.AnnotationExpr;
+import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.expr.SingleMemberAnnotationExpr;
+import com.github.javaparser.ast.expr.StringLiteralExpr;
 import com.github.javaparser.ast.type.Type;
 import jp.chang.myclinic.apitool.lib.Helper;
 import org.apache.velocity.Template;
@@ -19,6 +23,9 @@ import java.io.StringWriter;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static com.github.javaparser.ast.NodeList.*;
 
 @Command(name = "csharp-client")
@@ -56,12 +63,66 @@ class CSharpClient implements Runnable {
         }
     }
 
+    private String extractAnnotationValue(AnnotationExpr annotExpr) {
+        if (annotExpr == null) {
+            return null;
+        }
+        if (annotExpr instanceof SingleMemberAnnotationExpr) {
+            SingleMemberAnnotationExpr annot = (SingleMemberAnnotationExpr) annotExpr;
+            Expression value = annot.getMemberValue();
+            if (value instanceof StringLiteralExpr) {
+                return ((StringLiteralExpr) value).getValue();
+            }
+        }
+        return null;
+    }
+
+    private String extractAnnotationValue(MethodDeclaration method, String name) {
+        return extractAnnotationValue(method.getAnnotationByName(name).orElse(null));
+    }
+
+    public static class QueryParam {
+        private String key;
+        private String value;
+
+        public QueryParam(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+
+    private List<QueryParam> createQueryParams(NodeList<Parameter> params){
+        List<QueryParam> queryParams = new ArrayList<>();
+        for(Parameter param: params){
+            String name = param.getNameAsString();
+            String key = helper.toHyphenChain(name);
+            queryParams.add(new QueryParam(key, name));
+        }
+        return queryParams;
+    }
+
     private void handleServerMethod(MethodDeclaration serverMethod) {
         String clientMethodName = helper.capitalize(serverMethod.getNameAsString());
         VelocityContext context = new VelocityContext();
         context.put("retType", getRetType(serverMethod.getType()));
         context.put("methodName", clientMethodName);
         context.put("params", getParams(serverMethod.getParameters()));
+        String path = extractAnnotationValue(serverMethod, "Path");
+        if (path == null) {
+            System.err.println("Cannot find @Path annotation: " + serverMethod);
+            System.exit(1);
+            return;
+        }
+        context.put("path", path);
+        context.put("queryParams", createQueryParams(serverMethod.getParameters()));
         StringWriter writer = new StringWriter();
         if (serverMethod.isAnnotationPresent("GET")) {
             tmplGet.merge(context, writer);
@@ -81,16 +142,24 @@ class CSharpClient implements Runnable {
         return String.join(", ", parts);
     }
 
+    private static Pattern genericTypePattern = Pattern.compile("(\\w+)<(.+)>");
+
     private String toCSharpType(String javaType){
-        switch(javaType){
-            case "Integer": return "int";
-            case "Double": return "double";
-            case "Character": return "char";
-            case "String": return "string";
-            case "Void": return "void";
-            case "LocalDate": return "DateTimeAsDate";
-            case "LocalDateTime": return "DateTimeAsDateTime";
-            default: return javaType;
+        Matcher matcher = genericTypePattern.matcher(javaType);
+        if( matcher.matches() ){
+            String outer = matcher.group(1);
+            return String.format("%s<%s>", outer, toCSharpType(matcher.group(2)));
+        } else {
+            switch(javaType){
+                case "Integer": return "int";
+                case "Double": return "double";
+                case "Character": return "char";
+                case "String": return "string";
+                case "Void": return "void";
+                case "LocalDate": return "DateTimeAsDate";
+                case "LocalDateTime": return "DateTimeAsDateTime";
+                default: return javaType;
+            }
         }
     }
 
