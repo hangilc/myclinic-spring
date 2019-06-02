f95d3e52fd1ce6d5916e16bdc5e94ce12e2e4220
hangilc <hangil@chang.jp>
2019-06-02 11:39:00 +0900

    added csharp-client to api-tool

--- api-tool/src/main/java/jp/chang/myclinic/apitool/CSharpClient.java
+++ api-tool/src/main/java/jp/chang/myclinic/apitool/CSharpClient.java
@@ -20,13 +20,18 @@ import org.slf4j.LoggerFactory;
 import picocli.CommandLine.*;
 
 import java.io.StringWriter;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static com.github.javaparser.ast.NodeList.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
 @Command(name = "csharp-client")
 class CSharpClient implements Runnable {
@@ -37,7 +42,9 @@ class CSharpClient implements Runnable {
     private Helper helper = Helper.getInstance();
     private StringBuffer sb = new StringBuffer();
     private Template tmplGet;
+    private Template tmplVoidGet;
     private Template tmplPost;
+    private Template tmplVoidPost;
 
     CSharpClient() {
 
@@ -48,7 +55,9 @@ class CSharpClient implements Runnable {
         try {
             Velocity.init();
             this.tmplGet = Velocity.getTemplate("api-tool/template/CSharpClientGet.vm");
+            this.tmplVoidGet = Velocity.getTemplate("api-tool/template/CSharpClientVoidGet.vm");
             this.tmplPost = Velocity.getTemplate("api-tool/template/CSharpClientPost.vm");
+            this.tmplVoidPost = Velocity.getTemplate("api-tool/template/CSharpClientVoidPost.vm");
             Path serverSource = helper.getBackendServerSourcePath();
             CompilationUnit backendUnit = StaticJavaParser.parse(serverSource);
             ClassOrInterfaceDeclaration serverDecl = backendUnit.getClassByName("RestServer")
@@ -56,10 +65,94 @@ class CSharpClient implements Runnable {
             for (MethodDeclaration backendMethod : serverDecl.getMethods()) {
                 handleServerMethod(backendMethod);
             }
-            System.out.println(sb.toString());
+            if( outputFile == null ) {
+                System.out.println(sb.toString());
+            } else {
+                Files.write(Path.of(outputFile), sb.toString().getBytes());
+            }
         } catch (Exception ex) {
             ex.printStackTrace();
             System.exit(1);
+        }
+    }
+
+    private void handleServerMethod(MethodDeclaration serverMethod) {
+        String clientMethodName = helper.capitalize(serverMethod.getNameAsString());
+        VelocityContext context = new VelocityContext();
+        context.put("methodRetType", calcMethodRetType(serverMethod.getType()));
+        context.put("retType", calcRetType(serverMethod.getType()));
+        context.put("methodName", clientMethodName);
+        context.put("params", getParams(serverMethod.getParameters()));
+        String path = extractAnnotationValue(serverMethod, "Path");
+        if (path == null) {
+            System.err.println("Cannot find @Path annotation: " + serverMethod);
+            System.exit(1);
+            return;
+        }
+        context.put("path", path);
+        StringWriter writer = new StringWriter();
+        if (serverMethod.isAnnotationPresent("GET")) {
+            context.put("queryParams", createQueryParams(serverMethod.getParameters()));
+            if( Objects.equals(context.get("methodRetType"), "Task") ){
+                tmplVoidGet.merge(context, writer);
+            } else {
+                tmplGet.merge(context, writer);
+            }
+        } else if (serverMethod.isAnnotationPresent("POST")) {
+            PostMethodParams postParams = new PostMethodParams(serverMethod.getParameters());
+            context.put("queryParams", createQueryParams(postParams.getRegularParameters()));
+            context.put("bodyParam", postParams.getBodyParameter());
+            context.put("bodyParamType", postParams.getBodyParameterType());
+            if( Objects.equals(context.get("methodRetType"), "Task") ){
+                tmplVoidPost.merge(context, writer);
+            } else {
+                tmplPost.merge(context, writer);
+            }
+        }
+        sb.append(writer.toString());
+    }
+
+    private String calcMethodRetType(Type type){
+        String cType = toCSharpType(type.toString());
+        if( cType.equals("void") ){
+            return "Task";
+        } else {
+            return String.format("Task<%s>", cType);
+        }
+    }
+
+    private String calcRetType(Type type){
+        String cType = toCSharpType(type.toString());
+        if( cType.equals("void") ){
+            return "object";
+        } else {
+            return cType;
+        }
+    }
+
+    private static Pattern genericTypePattern = Pattern.compile("(\\w+)<(.+)>");
+
+    private static String toCSharpType(String javaType){
+        Matcher matcher = genericTypePattern.matcher(javaType);
+        if( matcher.matches() ){
+            String outer = matcher.group(1);
+            String cOuter = toCSharpType(outer);
+            String inner = matcher.group(2);
+            String[] innerParts = inner.split("\\s*,\\s+");
+            String cInner = Arrays.stream(innerParts).map(CSharpClient::toCSharpType).collect(joining(", "));
+            return String.format("%s<%s>", cOuter, cInner);
+        } else {
+            switch(javaType){
+                case "Integer": return "int";
+                case "Double": return "double";
+                case "Character": return "char";
+                case "String": return "string";
+                case "Void": return "void";
+                case "LocalDate": return "DateTimeAsDate";
+                case "LocalDateTime": return "DateTimeAsDateTime";
+                case "Map": return "Dictionary";
+                default: return javaType;
+            }
         }
     }
 
@@ -99,7 +192,7 @@ class CSharpClient implements Runnable {
         }
     }
 
-    private List<QueryParam> createQueryParams(NodeList<Parameter> params){
+    private List<QueryParam> createQueryParams(List<Parameter> params){
         List<QueryParam> queryParams = new ArrayList<>();
         for(Parameter param: params){
             String name = param.getNameAsString();
@@ -109,27 +202,55 @@ class CSharpClient implements Runnable {
         return queryParams;
     }
 
-    private void handleServerMethod(MethodDeclaration serverMethod) {
-        String clientMethodName = helper.capitalize(serverMethod.getNameAsString());
-        VelocityContext context = new VelocityContext();
-        context.put("retType", getRetType(serverMethod.getType()));
-        context.put("methodName", clientMethodName);
-        context.put("params", getParams(serverMethod.getParameters()));
-        String path = extractAnnotationValue(serverMethod, "Path");
-        if (path == null) {
-            System.err.println("Cannot find @Path annotation: " + serverMethod);
-            System.exit(1);
-            return;
+    private static boolean isBodyParam(Parameter param){
+        Type type = param.getType();
+        if( type.isPrimitiveType() ){
+            return false;
+        } else {
+            switch(type.toString()){
+                case "String": case "Character": case "Integer": case "Boolean": case "Double":
+                case "LocalDate": case "LocalDateTime":
+                    return false;
+                default:
+                    return true;
+            }
         }
-        context.put("path", path);
-        context.put("queryParams", createQueryParams(serverMethod.getParameters()));
-        StringWriter writer = new StringWriter();
-        if (serverMethod.isAnnotationPresent("GET")) {
-            tmplGet.merge(context, writer);
-        } else if (serverMethod.isAnnotationPresent("POST")) {
-            tmplPost.merge(context, writer);
+    }
+
+    private static class PostMethodParams {
+        private List<Parameter> regularParameters = new ArrayList<>();
+        private String bodyParameter;
+        private String bodyParameterType;
+
+        PostMethodParams(List<Parameter> params){
+            for(Parameter param: params){
+                if( isBodyParam(param) ){
+                    if( bodyParameter != null ){
+                        throw new RuntimeException("Too many body params: " + param.getNameAsString());
+                    }
+                    this.bodyParameter = param.getNameAsString();
+                    this.bodyParameterType = toCSharpType(param.getType().toString());
+                } else {
+                    regularParameters.add(param);
+                }
+            }
+            if( bodyParameter == null ){
+                this.bodyParameter = "null";
+                this.bodyParameterType = "object";
+            }
         }
-        sb.append(writer.toString());
+
+        List<Parameter> getRegularParameters() {
+            return regularParameters;
+        }
+
+        String getBodyParameter() {
+            return bodyParameter;
+        }
+
+        String getBodyParameterType(){
+            return bodyParameterType;
+        }
     }
 
     public String getParams(NodeList<Parameter> serverParams){
@@ -140,27 +261,6 @@ class CSharpClient implements Runnable {
             parts.add(cType + " " + name);
         }
         return String.join(", ", parts);
-    }
-
-    private static Pattern genericTypePattern = Pattern.compile("(\\w+)<(.+)>");
-
-    private String toCSharpType(String javaType){
-        Matcher matcher = genericTypePattern.matcher(javaType);
-        if( matcher.matches() ){
-            String outer = matcher.group(1);
-            return String.format("%s<%s>", outer, toCSharpType(matcher.group(2)));
-        } else {
-            switch(javaType){
-                case "Integer": return "int";
-                case "Double": return "double";
-                case "Character": return "char";
-                case "String": return "string";
-                case "Void": return "void";
-                case "LocalDate": return "DateTimeAsDate";
-                case "LocalDateTime": return "DateTimeAsDateTime";
-                default: return javaType;
-            }
-        }
     }
 
     private String getRetType(Type serverRetType){
