1feb3dd0ea73ca6dc6b7f6b2d9719e4970164f7e
Hangil Chang <hangil@chang.jp>
2019-12-02 18:27:50 +0900

    added start-mysql

--- server-docker/dump-mysql.ps1
+++ server-docker/dump-mysql.ps1
@@ -1,5 +1,10 @@
-$SavePath = "./data/dump-sql/dump.sql"
+Param(
+    [string]$DbHost = "localhost",
+    [string]$DbUser = "$env:MYCLINIC_DB_ADMIN_USER",
+    [string]$DbPass = "$env:MYCLINIC_DB_ADMIN_PASS",
+    [string]$SavePath = "./data/production-sql/dump.sql"
+)
 
-mysqldump -u "$env:MYCLINIC_DB_ADMIN_USER" -p"$env:MYCLINIC_DB_ADMIN_PASS" `
+mysqldump -h "$DbHost" -u "$env:MYCLINIC_DB_ADMIN_USER" -p"$env:MYCLINIC_DB_ADMIN_PASS" `
     --result-file "$SavePath" `
     --single-transaction --master-data myclinic 
--- server-docker/start-mysql-production.ps1
+++ server-docker/start-mysql-production.ps1
@@ -1,7 +1,8 @@
 Param(
     [int]$Port = 3306,
-    [string]$SqlDir = "dump-sql"
-    )
+    [string]$SqlDir = "data/production-sql",
+    [string]$ConfFile = "myclinic-mysql-production.cnf"
+)
 
 $MYSQLROOTPASS="$env:MYCLINIC_DB_ROOT_PASS"
 if( $MYSQLROOTPASS -eq "" ){
@@ -10,17 +11,18 @@ if( $MYSQLROOTPASS -eq "" ){
 $DOCKER_HOST = [System.Uri]$env:DOCKER_HOST
 $HostAddr = $DOCKER_HOST.Host
 $Cert = "$env:DOCKER_CERT_PATH\id_rsa"
-ssh -i "$Cert" "docker@$HostAddr" "mkdir -p ~/myclinic-server-data"
-ssh -i "$Cert" "docker@$HostAddr" "rm -rf ~/myclinic-server-data/*-sql"
-scp -i "$Cert" -r "./data/$SqlDir" "docker@${HostAddr}:~/myclinic-server-data/"
-scp -i "$Cert" ./data/myclinic-mysql-production.cnf "docker@${HostAddr}:~/myclinic-server-data/myclinic-mysql-production.cnf"
+ssh -i "$Cert" "docker@$HostAddr" "rm -rf ~/myclinic-server-data"
+ssh -i "$Cert" "docker@$HostAddr" "mkdir ~/myclinic-server-data"
+ssh -i "$Cert" "docker@$HostAddr" "mkdir ~/myclinic-server-data/sql"
+scp -i "$Cert" "./$SqlDir/*.sql" "docker@${HostAddr}:~/myclinic-server-data/sql"
+scp -i "$Cert" "$ConfFile" "docker@${HostAddr}:~/myclinic-server-data/myclinic-mysql.cnf"
 docker run -e MYSQL_ROOT_PASSWORD="$MYSQLROOTPASS" `
     -d `
     -e MYSQL_USER="$env:MYCLINIC_DB_USER" `
     -e MYSQL_PASSWORD="$env:MYCLINIC_DB_PASS" `
     -e MYSQL_DATABASE="myclinic" `
     -p "${Port}:3306" `
-    -v "/home/docker/myclinic-server-data/myclinic-mysql-production.cnf:/etc/mysql/conf.d/myclinic-mysql-production.cnf" `
-    -v "/home/docker/myclinic-server-data/${SqlDir}:/docker-entrypoint-initdb.d" `
+    -v "/home/docker/myclinic-server-data/myclinic-mysql.cnf:/etc/mysql/conf.d/myclinic-mysql.cnf" `
+    -v "/home/docker/myclinic-server-data/sql:/docker-entrypoint-initdb.d" `
     mysql:5.7
 
