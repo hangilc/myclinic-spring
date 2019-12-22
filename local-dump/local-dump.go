e72faf97dc7fe21341ef90ce3624ac699801d46e
hangilc <hangil@chang.jp>
2019-12-22 16:16:55 +0900

    added timestamp to local-dump

--- local-dump/local-dump.go
+++ local-dump/local-dump.go
@@ -1,20 +1,33 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"os"
 	"os/exec"
+	"strconv"
+	"time"
 )
 
-func main() {
+func getTimestamp() string {
+	t := time.Now()
+	return t.Format("20060102150405")
+}
+
+func dump(host string, port int, fname string) {
 	user := "root"
 	os.Setenv("MYSQL_PWD", os.Getenv("MYCLINIC_DB_ROOT_PASS"))
-	cmd := exec.Command("mysqldump", "-h", "127.0.0.1", "-u", user, "myclinic",
-		"--result-file=data/dump.sql")
+	cmd := exec.Command("mysqldump", "-h", host, "-P", strconv.Itoa(port), "-u", user, "myclinic",
+		"--result-file="+fname)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
 	if err != nil {
-		log.Printf("%v", err)
+		fmt.Printf("%v", err)
 	}
+}
+
+func main() {
+	stamp := getTimestamp()
+	fname := "data/dump-" + stamp + ".sql"
+	dump("127.0.0.1", 3306, fname)
 }
