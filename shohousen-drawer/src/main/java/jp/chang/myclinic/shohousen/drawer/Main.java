98663fead1ab93f8501e1fc8c20822b21608e34d
hangilc <hangil@chang.jp>
2020-04-16 22:48:26 +0900

    added input/output to shohousen-drawer

--- shohousen-drawer/src/main/java/jp/chang/myclinic/shohousen/drawer/Main.java
+++ shohousen-drawer/src/main/java/jp/chang/myclinic/shohousen/drawer/Main.java
@@ -3,53 +3,99 @@ package jp.chang.myclinic.shohousen.drawer;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jp.chang.myclinic.drawer.Op;
-import jp.chang.myclinic.drawer.printer.PrinterEnv;
 import jp.chang.myclinic.shohousen.ShohousenData;
 import jp.chang.myclinic.shohousen.ShohousenDrawer;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Path;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        File inputFile = null;
-        String printerSetting = null;
-        if( args.length == 1 || args.length == 2 ){
-            inputFile = new File(args[0]);
-            if( args.length >= 2 ){
-                printerSetting = args[1];
+    private static class CmdArgs {
+        public String input;
+        public String output;
+        public boolean help;
+
+        public static CmdArgs parse(String[] args) {
+            CmdArgs result = new CmdArgs();
+            for (int i = 0; i < args.length; i++) {
+                String arg = args[i];
+                switch (arg) {
+                    case "-i":
+                    case "--in": {
+                        result.input = args[i + 1];
+                        i += 1;
+                        break;
+                    }
+                    case "-o":
+                    case "--out": {
+                        result.output = args[i + 1];
+                        i += 1;
+                        break;
+                    }
+                    case "-h": {
+                        result.help = true;
+                        break;
+                    }
+                    default: {
+                        usage();
+                        System.exit(1);
+                    }
+                }
             }
+            return result;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        CmdArgs cmdArgs = CmdArgs.parse(args);
+        InputStreamReader in;
+        if (cmdArgs.input == null) {
+            in = new InputStreamReader(System.in);
         } else {
-            usage();
-            System.exit(1);
+            FileInputStream fs = new FileInputStream(cmdArgs.input);
+            in = new InputStreamReader(fs, StandardCharsets.UTF_8);
         }
         ObjectMapper mapper = new ObjectMapper();
-        List<ShohousenInput> inputs = mapper.readValue(inputFile, new TypeReference<List<ShohousenInput>>(){});
-        for(ShohousenInput input: inputs){
+        List<ShohousenInput> inputs = mapper.readValue(in, new TypeReference<List<ShohousenInput>>() {
+        });
+        List<List<Op>> result = new ArrayList<>();
+        for (ShohousenInput input : inputs) {
             ShohousenData data = toData(input);
             ShohousenDrawer drawer = new ShohousenDrawer();
             data.applyTo(drawer);
             List<Op> ops = drawer.getOps();
-            PrinterEnv penv = new PrinterEnv();
-            penv.print(List.of(ops), printerSetting);
+            result.add(ops);
+        }
+        String json = mapper.writeValueAsString(result);
+        if (cmdArgs.output == null) {
+            System.out.println(json);
+        } else {
+            FileOutputStream fout = new FileOutputStream(cmdArgs.output);
+            OutputStreamWriter out = new OutputStreamWriter(fout, StandardCharsets.UTF_8);
+            out.write(json);
+            out.write("\n");
         }
     }
 
-    private static void usage(){
-        System.err.println("Usage: shohousen-drawer INPUT.JSON [PRINTER-SETTING]");
+    private static void usage() {
+        System.err.println("Usage: shohousen-drawer [options");
+        System.err.println("  options:");
+        System.err.println("    -i, --in INPUT : read from file");
+        System.err.println("    -o, --out OUTPUT : write to file");
+        System.err.println("    -h : print help");
     }
 
-    private static ShohousenData toData(ShohousenInput input){
+    private static ShohousenData toData(ShohousenInput input) {
         ShohousenData data = new ShohousenData();
         data.clinicAddress = input.clinicAddress;
         data.setDrugs(input.content);
         return data;
     }
 
-    private static String getPrinterSettingsDir(){
-        return System.getenv("MYCLINIC_PRINTER_SETTINGS_DIR");
-    }
+//    private static String getPrinterSettingsDir(){
+//        return System.getenv("MYCLINIC_PRINTER_SETTINGS_DIR");
+//    }
 }
--- shohousen-drawer/src/main/java/jp/chang/myclinic/shohousen/drawer/ShohousenInput.java
+++ shohousen-drawer/src/main/java/jp/chang/myclinic/shohousen/drawer/ShohousenInput.java
@@ -1,28 +1,30 @@
 package jp.chang.myclinic.shohousen.drawer;
 
-import java.time.LocalDate;
-import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.time.LocalDate;
+
+//@JsonIgnoreProperties(ignoreUnknown=true)
 public class ShohousenInput {
 
     public String clinicAddress = "";
-//    public String clinicName = "";
-//    public String clinicPhone = "";
-//    public String kikancode = "";
-//    public String doctorName = "";
-//    public String hokenshaBangou = "";
-//    public String hihokensha = "";
-//    public String futansha = "";
-//    public String jukyuusha = "";
-//    public String futansha2 = "";
-//    public String jukyuusha2 = "";
-//    public String shimei = "";
-//    public LocalDate birthday;
-//    public String sex;
-//    public Boolean honnin = null;
-//    public Integer futanWari = null;
-//    public LocalDate koufuDate;
-//    public LocalDate validUptoDate;
+    public String clinicName = "";
+    public String clinicPhone = "";
+    public String kikancode = "";
+    public String doctorName = "";
+    public String hokenshaBangou = "";
+    public String hihokensha = "";
+    public String futansha = "";
+    public String jukyuusha = "";
+    public String futansha2 = "";
+    public String jukyuusha2 = "";
+    public String shimei = "";
+    public String birthday = "";
+    public String sex;
+    public Boolean honnin = null;
+    public Integer futanWari = null;
+    public String koufuDate = "";
+    public String validUptoDate = "";
     public String content;
 
 }
