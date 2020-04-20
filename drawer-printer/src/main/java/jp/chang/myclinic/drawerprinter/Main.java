2de380ffa00d0d06984f5a80a1afd62fb86f7f2f
hangilc <hangil@chang.jp>
2020-04-20 23:02:58 +0900

    added pdf paper size to drawer-printer

--- drawer-printer/src/main/java/jp/chang/myclinic/drawerprinter/Main.java
+++ drawer-printer/src/main/java/jp/chang/myclinic/drawerprinter/Main.java
@@ -5,24 +5,26 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import jp.chang.myclinic.drawer.JacksonOpDeserializer;
 import jp.chang.myclinic.drawer.Op;
+import jp.chang.myclinic.drawer.PaperSize;
 import jp.chang.myclinic.drawer.pdf.PdfPrinter;
 import jp.chang.myclinic.drawer.printer.PrinterEnv;
 
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
     private static class CmdArgs {
         public String input;
         public String pdf;
+        public String pdfPageSize;
 
         public CmdArgs(String[] args) {
             for (int i = 0; i < args.length; i++) {
@@ -38,6 +40,11 @@ public class Main {
                         this.pdf = args[++i];
                         break;
                     }
+                    case "--pdf-page-size":
+                    case "--pdf-paper-size": {
+                        this.pdfPageSize = args[++i];
+                        break;
+                    }
                     default: {
                         usage();
                         System.exit(1);
@@ -51,6 +58,7 @@ public class Main {
             System.err.println("  options:");
             System.err.println("    -i, --in INPUT: read from file");
             System.err.println("    --pdf OUTPUT: write as PDF file to OUTPUT");
+            System.err.println("    --pdf-page-size SIZE: A5, 148x210, ...");
         }
     }
 
@@ -69,10 +77,14 @@ public class Main {
             InputStream fin = new FileInputStream(cmdArgs.input);
             reader = new InputStreamReader(fin, StandardCharsets.UTF_8);
         }
-        pages = mapper.readValue(reader, new TypeReference<List<List<Op>>>(){});
+        pages = mapper.readValue(reader, new TypeReference<List<List<Op>>>() {
+        });
         reader.close();
-        if( cmdArgs.pdf != null ){
-            PdfPrinter printer = new PdfPrinter();
+        if (cmdArgs.pdf != null) {
+            PaperSize paperSize = getPaperSize(cmdArgs.pdfPageSize);
+            System.out.println(paperSize.getWidth());
+            System.out.println(paperSize.getHeight());
+            PdfPrinter printer = new PdfPrinter(paperSize);
             printer.print(pages, cmdArgs.pdf);
         } else {
             PrinterEnv penv = new PrinterEnv(Paths.get(getPrinterSettingsDir()));
@@ -82,6 +94,48 @@ public class Main {
 
     private static String getPrinterSettingsDir() {
         return System.getenv("MYCLINIC_PRINTER_SETTINGS_DIR");
+    }
+
+    private final static Pattern patPaperSize = Pattern.compile("([\\d.]+)[Xx]([\\d.]+)");
+
+    private static PaperSize getPaperSize(String opt) {
+        if (opt == null || opt.isEmpty()) {
+            return PaperSize.A4;
+        }
+        Matcher m = patPaperSize.matcher(opt);
+        if (m.matches()) {
+            double w = Double.parseDouble(m.group(1));
+            double h = Double.parseDouble(m.group(2));
+            return new PaperSize(w, h);
+        }
+        switch (opt) {
+            case "A4":
+                return PaperSize.A4;
+            case "A4-landscape":
+                return PaperSize.A4_Landscape;
+            case "A5":
+                return PaperSize.A5;
+            case "A5-landscape":
+                return PaperSize.A5_Landscape;
+            case "A6":
+                return PaperSize.A6;
+            case "A6-landscape":
+                return PaperSize.A6_Landscape;
+            case "B4":
+                return PaperSize.B4;
+            case "B4-landscape":
+                return PaperSize.B4_Landscape;
+            case "B5":
+                return PaperSize.B5;
+            case "B5-landscape":
+                return PaperSize.B5_Landscape;
+            case "B6":
+                return PaperSize.B6;
+            case "B6-landscape":
+                return PaperSize.B6_Landscape;
+            default:
+                throw new RuntimeException("Unknown paper size: " + opt);
+        }
     }
 }
 
